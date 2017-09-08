832295d9f079d02d4eae0f33c1b1f27684e7a716
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-09-08 14:12:38 +0700

    add function to handle errors

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ const web3 = new Web3(new Web3.providers.HttpProvider(process.env.NODE_ADDRESS))
 const Store = require('./store')(web3)
 const DB = require('./db')
 
-const handleEvent = (err, { event, trigger }) => err
-  ? console.log(err) // TODO need to handle this error
-  : DB.writeEvent(event, trigger)
+const handleEvent = ({ event, trigger }) => DB.writeEvent(event, trigger)
+
+const executeIfNoError = callback => (err, data) => err
+  ? console.error(err) // TODO need to handle error
+  : callback(data)
 
 DB.fetchAll()
   .then(triggers => triggers
-    .map(trigger => Store.add(trigger, handleEvent))
+    .map(trigger => Store.add(trigger, executeIfNoError(handleEvent)))
   )
 
-DB.onDataUpdated((err, trigger) => trigger.enable
-  ? Store.add(trigger, handleEvent)
-  : Store.remove(trigger.id))
+DB.onDataUpdated(executeIfNoError(trigger => trigger.enable
+  ? Store.add(trigger, executeIfNoError(handleEvent))
+  : Store.remove(trigger.id)))
 
-DB.onDataDeleted((err, triggerId) => Store.remove(triggerId))
+DB.onDataDeleted(executeIfNoError(triggerId => Store.remove(triggerId)))
