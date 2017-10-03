14332f4338b8fcab0327e04945a1012347db7cc1
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-03 16:38:26 +0700

    add missing chain data while fetching db

--- src/queries/fetchAll.js
+++ src/queries/fetchAll.js
@@ -14,6 +14,7 @@ module.exports = gql`
           contract {
             abi
             address
+            chain
           }
         }
         ethereumTransaction {
--- src/queries/updated.js
+++ src/queries/updated.js
@@ -17,6 +17,7 @@ module.exports = gql`
             contract {
               abi
               address
+              chain
             }
           },
           ethereumTransaction {
