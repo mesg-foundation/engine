b918ee668d758bfff71d05419c4a6e57d0a236e5
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-03 17:28:38 +0700

    add customizable and optional retry options

--- src/utils.js
+++ src/utils.js
@@ -3,14 +3,15 @@ const { JsonRPCConnectionError } = require('./errors')
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const testConnection = async (check, endpoint) => {
-  let retry = process.env.RETRY_COUNT
+  let retry = process.env.RETRY_COUNT || 10
+  const delay = process.env.RETRY_DELAY || 1000
   const time = new Date()
   while (!check()) {
     if (retry <= 0) {
       throw new JsonRPCConnectionError(endpoint)
     }
-    await sleep(1000)
-    console.log(`Connection to ${endpoint} invalid... retrying in 1s`)
+    await sleep(delay)
+    console.log(`Connection to ${endpoint} invalid... retrying in ${delay}ms`)
     retry = retry - 1
   }
   return Promise.resolve({
