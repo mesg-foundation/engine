9f374be2d8b4fe3d389469c8d88a058c6e7542f7
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-04 16:31:24 +0700

    handle subscriptions to amount change

--- src/listeners/ethereum/transaction.js
+++ src/listeners/ethereum/transaction.js
@@ -1,20 +1,88 @@
 const createClient = require('./client')
 
-const match = trigger => trigger.connector.connectorType === 'ETHEREUM_TRANSACTION'
+const listeners = {
+  // chain: {
+  //   listener: ...,
+  //   addresses: {
+  //     '0x...': {
+  //       address: '0x...',
+  //       balance: xxx,
+  //       callbacks: [
+  //         callback1,
+  //         callback2
+  //       ]
+  //     }
+  //   }
+  // }
+}
 
-const createListener = async trigger => {
-  const { address, chain } = trigger.connector.ethereumTransaction
-  const client = await createClient(chain)
-  const listener = client
-    .filter({
-      fromBlock: 'latest',
-      toBlock: 'latest',
-      address: [address]
-    })
-  return {
-    watch: callback => listener.watch(client.handleEvent(callback)),
-    stopWatching: listener.stopWatching
+const ensureChain = chain => {
+  if (listeners[chain]) { return }
+  listeners[chain] = {
+    client: null,
+    addresses: {}
   }
 }
 
-module.exports = { match, createListener }
+const checkAddressBalance = (client, transaction, chain, address) => {
+  const { balance, callbacks } = listeners[chain].addresses[address]
+  const newBalance = client.eth.getBalance(address)
+  if (newBalance.equals(balance)) { return }
+  listeners[chain].addresses[address].balance = newBalance
+  callbacks.forEach(callback => {
+    client.handleEvent(callback)(null, {
+      ...transaction,
+      args: {
+        balance: {
+          wei: newBalance,
+          ether: client.fromWei(newBalance, 'ether')
+        }
+      }
+    })
+  })
+}
+
+const startListener = async (chain, client) => {
+  if (listeners[chain].listener) { return }
+  listeners[chain].listener = client.eth.filter({
+    fromBlock: 'latest',
+    toBlock: 'latest',
+  })
+  listeners[chain].listener.watch((error, transaction) => {
+    if (error) { throw new Error("TODO") }
+    Object.keys(listeners[chain].addresses)
+      .forEach(address => checkAddressBalance(client, transaction, chain, address))
+  })
+}
+
+const subscribeToBalanceChange = (address, chain) => async callback => {
+  const client = await createClient(chain)
+  ensureChain(chain)
+  if (!listeners[chain].addresses[address]) {
+    listeners[chain].addresses[address] = {
+      address,
+      balance: client.eth.getBalance(address),
+      callbacks: []
+    }
+  }
+  listeners[chain].addresses[address].callbacks.push(callback)
+  await startListener(chain, client)
+}
+
+const unsubscribeToBalanceChange = (address, chain) => {
+  // TODO
+}
+
+const createListener = async trigger => {
+  const { address, chain } = trigger.connector.ethereumTransaction
+
+  return {
+    watch: subscribeToBalanceChange(address, chain),
+    stopWatching: unsubscribeToBalanceChange(address, chain)
+  }
+}
+
+module.exports = {
+  match: trigger => trigger.connector.connectorType === 'ETHEREUM_TRANSACTION',
+  createListener
+}
