86dced50a7b073be8a6067c8dec91551c1243616
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-11 12:23:11 +0700

    add contract event listener

--- src/triggers/ethereum/contract.js
+++ src/triggers/ethereum/contract.js
@@ -1,14 +1,41 @@
+const SolidityEvent = require('web3/lib/web3/event')
+
+const matchLogFromTopics = topics => log => (log.topics || [])
+  .some(topic => topics.indexOf(topic) >= 0)
+
 module.exports = trigger => {
   const { eventName, contract } = trigger.connector.ethereumContract
   const { chain, address } = contract
+
+  const eventAbi = contract.abi
+    .filter(x => x.type === 'event')
+    .filter(x => x.name === eventName)[0]
+
+  const solidityEvent = new SolidityEvent(null, eventAbi, address)
+  const matchLog = matchLogFromTopics(solidityEvent.encode().topics)
 
   return {
     match: ({ type, network, transaction, block }) => {
       if (type !== 'ETHEREUM') { return false }
       if (network !== chain) { return false }
       if (address.toLowerCase() !== (transaction.to || '').toLowerCase()) { return false }
-      return false
-      // TODO return event from the transaction
+
+      return transaction.logs.some(matchLog)
+    },
+    normalizeEvent: ({ transaction, block }) => {
+      const normalizedTransaction = {
+        blockId: block.number.toString(),
+        fees: transaction.gasUsed.toString(),
+        from: transaction.from,
+        to: transaction.to,
+        transactionId: transaction.hash,
+        value: transaction.value
+      }
+      return transaction.logs
+        .filter(matchLog)
+        .map(log => Object.assign({}, normalizedTransaction, {
+          payload: solidityEvent.decode(log).args
+        }))
     }
   }
 }
