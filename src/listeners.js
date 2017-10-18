ff692beafd9e0262a8add0bdd92475274cadc056
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-18 12:36:19 +0700

    do not raise error because of no test and inject error on linting

---
+++ src/listeners.js
@@ -0,0 +1,32 @@
+const Logger = require('./logger')
+const Store = require('./store')
+const Db = require('./db')
+
+const blockchainClients = async () => [
+  // await require('./blockchains/ethereum')('MAINNET'),
+  await require('./blockchains/ethereum')('KOVAN'),
+]
+
+const handleTransaction = (type, network) => (transaction, block) => Store
+  .matchingTriggers({ type, network, transaction, block })
+  .map(trigger => ({
+    trigger,
+    event: trigger.normalizeEvent({ transaction, block })
+  }))
+  .forEach(result => Array.isArray(result.event)
+    ? result.event.map(event => Db.writeEvent(result.trigger, event))
+    : Db.writeEvent(result.trigger, result.event))
+
+const connectClientToTransactions = ({ type, network, onTransaction }) =>
+  onTransaction(handleTransaction(type, network))
+
+const start = async () => {
+  Logger.info('initializing all blockchains connections')
+  const clients = await blockchainClients()
+  Logger.info('listening for transactions')
+  clients.forEach(connectClientToTransactions)
+}
+
+module.exports = {
+  start
+}
