95c82e51ddb9ec003f3b7f228c0e4cdeb8b69fe0
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-10-30 17:05:37 +0630

    handle new blocks with web3 subscriptions

--- src/blockchains/ethereum.js
+++ src/blockchains/ethereum.js
@@ -7,6 +7,30 @@ const { emitRawBlock, emitRawTransaction } = require('../eventEmitter')
 const type = 'ETHEREUM'
 const endpoint = blockchain => process.env[`${type}_${blockchain.toUpperCase()}`]
 
+const handlerNewBlock = (client, blockchain) => async result => {
+  const block = await client.eth.getBlock(result.number, true)
+  emitRawBlock({ type, blockchain, block })
+  const receiptsBatch = new client.BatchRequest()
+  Promise.all(block.transactions
+    .map((transaction, i) => new Promise((resolve, reject) => {
+      receiptsBatch.add(client.eth.getTransactionReceipt.request(
+        transaction.hash,
+        (err, receipt) => err
+          ? reject(err)
+          : resolve({ ...transaction, ...receipt })
+      ))
+    })
+  ))
+    .then(transactions => transactions.forEach(transaction => {
+      emitRawTransaction({ type, blockchain, block, transaction })
+    }))
+    .catch(e => {
+      Logger.error(e)
+      throw new Error('Receipt transaction fetching failed')
+    })
+  receiptsBatch.execute()
+}
+
 module.exports = async ({ blockchain }) => {
   if (endpoint(blockchain) === undefined) {
     // We disable this blockchain throw the env variables but
@@ -15,37 +39,12 @@ module.exports = async ({ blockchain }) => {
   }
   if (!endpoint(blockchain)) throw new InvalidBlockchainError(blockchain)
 
-  const client = new Web3(new Web3.providers.HttpProvider(endpoint(blockchain)))
-
-  await testConnection(() => client.isConnected(), `${type}/${blockchain}`)
-  setInterval(() => {
-    if (!client.isConnected()) {
-      Logger.error(`${blockchain} disconected`)
-    }
-  }, 1000)
-
-  client.eth.filter('latest', (error, result) => {
-    if (error) throw new Error('Error on watcher', error)
-    const block = client.eth.getBlock(result, true)
-    emitRawBlock({ type, blockchain, block })
-    const receiptsBatch = client.createBatch()
-    Promise.all(block.transactions
-      .map((transaction, i) => new Promise((resolve, reject) => {
-        receiptsBatch.add(client.eth.getTransactionReceipt.request(
-          transaction.hash,
-          (err, receipt) => err
-            ? reject(err)
-            : resolve({ ...transaction, ...receipt })
-        ))
-      })
-    ))
-      .then(transactions => transactions.forEach(transaction => {
-        emitRawTransaction({ type, blockchain, block, transaction })
-      }))
-      .catch(e => {
-        Logger.error(e)
-        throw new Error('Receipt transaction fetching failed')
-      })
-    receiptsBatch.execute()
-  })
+  const client = new Web3(endpoint(blockchain))
+  
+  // client.eth.defaultBlock = 'latest'
+  const subscription = await client.eth.subscribe('newBlockHeaders')
+  subscription
+    .on('changed', () => Logger.info(`Websocket ${blockchain} changed`))
+    .on('error', error => Logger.error(`error ${error}`))
+    .on('data', handlerNewBlock(client, blockchain))
 }
