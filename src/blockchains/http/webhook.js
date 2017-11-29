41212ff807b6857d1e570d41c7290022db4b1ebd
Anthony ESTEBE <anthony.estebe@gmail.com>
2017-11-29 12:06:37 +0700

    add rate limite to the api and emit the event for propagation

--- src/blockchains/http/webhook.js
+++ src/blockchains/http/webhook.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const requestId = require('express-request-id')
+const rateLimit = require('express-redis-rate-limit')
+const redis = require('../../db/redis')
+const { emitRawTransaction } = require('../../eventEmitter')
 const Logger = require('../../logger')
 
-const handleRequest = (req, res) => {
+const handleRequest = app => (req, res) => {
   const body = req.body
-  Logger.info(`request with ${JSON.stringify(req.body)}`)
-  Logger.info(`request with ${JSON.stringify(req.params)}`) 
-  Logger.info(`request with ${JSON.stringify(req.data)}`) 
+  emitRawTransaction({ type: 'HTTP', request: req, app })
   res.json({
     body: req.body,
     params: req.params,
@@ -16,10 +18,15 @@ const handleRequest = (req, res) => {
 
 module.exports = async () => {
   const app = express()
+  app.use(requestId())
+  app.use(rateLimit(redis, {
+    requestLimit: 10,
+    timeWindow: 30
+  }))
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
   
-  app.post('/triggers/:id', handleRequest)
+  app.post('/triggers/:key', handleRequest(app))
   return new Promise((resolve, reject) => app
     .listen(3000, (err, server) => err
       ? reject(err)
