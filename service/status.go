ce82aec923f302303ec64dcbe67b0793049cd9f8
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-19 14:58:34 +0700

    add status fuctions

---
+++ service/status.go
@@ -0,0 +1,84 @@
+package service
+
+import (
+	"context"
+	"strings"
+
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+// StatusType of the service
+type StatusType uint
+
+// status for services
+const (
+	STOPPED StatusType = 0
+	RUNNING StatusType = 1
+	PARTIAL StatusType = 2
+)
+
+func dependencyStatus(dependency *Dependency, namespace string, dependencyName string) (status StatusType) {
+	ctx := context.Background()
+	dockerServices, err := dockerCli.ListServices(docker.ListServicesOptions{
+		Filters: map[string][]string{
+			"name": []string{strings.Join([]string{namespace, dependencyName}, "_")},
+		},
+		Context: ctx,
+	})
+	status = STOPPED
+	if err == nil && len(dockerServices) > 0 {
+		status = RUNNING
+	}
+	return
+}
+
+func serviceStatus(service *Service) (status StatusType) {
+	status = STOPPED
+	allRunning := true
+	for name, dependency := range service.Dependencies {
+		if dependency.IsRunning(service.namespace(), name) {
+			status = RUNNING
+		} else {
+			allRunning = false
+		}
+	}
+	if status == RUNNING && !allRunning {
+		status = PARTIAL
+	}
+	return
+}
+
+// IsRunning returns true if the service is running, false otherwise
+func (service *Service) IsRunning() (running bool) {
+	status := serviceStatus(service)
+	running = status == RUNNING
+	return
+}
+
+// IsPartiallyRunning returns true if the service is running, false otherwise
+func (service *Service) IsPartiallyRunning() (running bool) {
+	status := serviceStatus(service)
+	running = status == PARTIAL
+	return
+}
+
+// IsStopped returns true if the service is stopped, false otherwise
+func (service *Service) IsStopped() (running bool) {
+	status := serviceStatus(service)
+	running = status == STOPPED
+	return
+}
+
+// IsRunning returns true if the dependency is running, false otherwise
+func (dependency *Dependency) IsRunning(namespace string, name string) (running bool) {
+	status := dependencyStatus(dependency, namespace, name)
+	running = status == RUNNING
+	return
+}
+
+// IsStopped returns true if the dependency is stopped, false otherwise
+func (dependency *Dependency) IsStopped(namespace string, name string) (running bool) {
+	status := dependencyStatus(dependency, namespace, name)
+	running = status == STOPPED
+	return
+}
