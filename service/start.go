ae8ac5e246e55da796bee48acbc2cae10ede402a
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-19 14:58:08 +0700

    better managment of service starting

--- service/start.go
+++ service/start.go
@@ -2,8 +2,18 @@ package service
 
 // Start a service
 func (service *Service) Start() (err error) {
+	if service.IsRunning() {
+		return
+	}
+	// If there is one but not all services running stop to restart all
+	if service.IsPartiallyRunning() {
+		service.Stop()
+	}
 	for name, dependency := range service.Dependencies {
 		err = dependency.Start(name, service.namespace())
+		if err != nil {
+			break
+		}
 	}
 	// Disgrasfully close the service because there is an error
 	if err != nil {
