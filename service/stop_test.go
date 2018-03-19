dbeb55598c2eb776257429183f69f23b7edf325c
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-19 14:58:48 +0700

    add way to stop a service

---
+++ service/stop_test.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/stvp/assert"
+)
+
+func TestStopRunningService(t *testing.T) {
+	service := &Service{
+		Name: "TestStopRunningService",
+		Dependencies: map[string]*Dependency{
+			"test": &Dependency{
+				Image: "nginx",
+			},
+		},
+	}
+	service.Start()
+	err := service.Stop()
+	assert.Nil(t, err)
+	assert.Equal(t, service.IsStopped(), true)
+}
+
+func TestStopNonRunningService(t *testing.T) {
+	service := &Service{
+		Name: "TestStopNonRunningService",
+		Dependencies: map[string]*Dependency{
+			"test": &Dependency{
+				Image: "nginx",
+			},
+		},
+	}
+	err := service.Stop()
+	assert.Nil(t, err)
+	assert.Equal(t, service.IsStopped(), true)
+}
