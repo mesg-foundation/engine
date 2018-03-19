5449f2ae12af4d73eeaea733ba608bb8cbc39ec5
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-19 14:58:48 +0700

    add way to stop a service

--- service/stop.go
+++ service/stop.go
@@ -1,6 +1,15 @@
 package service
 
 // Stop a service
-func (service *Service) Stop() {
-
+func (service *Service) Stop() (err error) {
+	if service.IsStopped() {
+		return
+	}
+	for name, dependency := range service.Dependencies {
+		err = dependency.Stop(name, service.namespace())
+		if err != nil {
+			break
+		}
+	}
+	return
 }
