062080093e4075322dcc8be219db3bf339d3bb54
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-21 15:37:41 +0200

    add json for service type

--- service/type.go
+++ service/type.go
@@ -22,54 +22,46 @@ const (
 	Executor  FeeActor = "executor"
 )
 
-// Publication let you configure the part of your service you want to publish
-type Publication string
+// Publish let you configure the part of your service you want to publish
+type Publish string
 
-// List of all publications flags
+// List of all publishs flags
 const (
-	AllPublication       Publication = "ALL"
-	SourcePublication    Publication = "SOURCE"
-	ContainerPublication Publication = "CONTAINER"
-	NonePublication      Publication = "NONE"
+	PublishAll       Publish = "ALL"
+	PublishSource    Publish = "SOURCE"
+	PublishContainer Publish = "CONTAINER"
+	PublishNone      Publish = "NONE"
 )
 
 // Service is a definition for a service to run
 type Service struct {
-	Version      string       `yaml:"version"`
-	Name         string       `yaml:"name"`
-	Description  string       `yaml:"description"`
-	Requirements Requirements `yaml:"requirements"`
-	Visibility   Visibility   `yaml:"visibility"`
-	Publication  Publication  `yaml:"publication"`
-	Tasks        Tasks        `yaml:"tasks"`
-	Events       Events       `yaml:"events"`
-	Dependencies Dependencies `yaml:"dependencies"`
-}
-
-// Requirements is a list of requirements to run the service
-type Requirements struct {
-	memory uint `yaml:"memory"`
-	cpu    uint `yaml:"cpu"`
-	disk   uint `yaml:"disk"`
+	Version      string       `yaml:"version" json:"version"`
+	Name         string       `yaml:"name" json:"name"`
+	Description  string       `yaml:"description" json:"description"`
+	Visibility   Visibility   `yaml:"visibility" json:"visibility"`
+	Publish      Publish      `yaml:"publish" json:"publish"`
+	Tasks        Tasks        `yaml:"tasks" json:"tasks"`
+	Events       Events       `yaml:"events" json:"events"`
+	Dependencies Dependencies `yaml:"dependencies" json:"dependencies"`
 }
 
 // Task is a definition of a Task from a service
 type Task struct {
-	Name        string     `yaml:"name"`
-	Description string     `yaml:"description"`
-	Verifiable  bool       `yaml:"verifiable"`
-	Payable     bool       `yaml:"payable"`
-	Fees        Fees       `yaml:"fees"`
-	Inputs      Parameters `yaml:"inputs"`
-	Outputs     Events     `yaml:"outputs"`
+	Name        string     `yaml:"name" json:"name"`
+	Description string     `yaml:"description" json:"description"`
+	Verifiable  bool       `yaml:"verifiable" json:"verifiable"`
+	Payable     bool       `yaml:"payable" json:"payable"`
+	Fees        Fees       `yaml:"fees" json:"fees"`
+	Inputs      Parameters `yaml:"inputs" json:"inputs"`
+	Outputs     Events     `yaml:"outputs" json:"outputs"`
 }
 
 // Fees is the different fees to apply
 type Fees struct {
-	developer FeeActor `yaml:"developer"`
-	validator FeeActor `yaml:"validator"`
-	executor  FeeActor `yaml:"executor"`
-	emitters  FeeActor `yaml:"emitters"`
+	Developer FeeActor `yaml:"developer" json:"developer"`
+	Validator FeeActor `yaml:"validator" json:"validator"`
+	Executor  FeeActor `yaml:"executor" json:"executor"`
+	Emitters  FeeActor `yaml:"emitters" json:"emitters"`
 }
 
 // Tasks is a list of Tasks
@@ -77,9 +69,9 @@ type Tasks map[string]Task
 
 // Event is the definition of an event emitted from a service
 type Event struct {
-	Name        string     `yaml:"name"`
-	Description string     `yaml:"description"`
-	Data        Parameters `yaml:"data"`
+	Name        string     `yaml:"name" json:"name"`
+	Description string     `yaml:"description" json:"description"`
+	Data        Parameters `yaml:"data" json:"data"`
 }
 
 // Events is a list of Events
@@ -87,10 +79,10 @@ type Events map[string]Event
 
 // Parameter is the definition of a parameter for a Task
 type Parameter struct {
-	Name        string `yaml:"name"`
-	Description string `yaml:"description"`
-	Type        string `yaml:"type"`
-	Optional    bool   `yaml:"optional"`
+	Name        string `yaml:"name" json:"name"`
+	Description string `yaml:"description" json:"description"`
+	Type        string `yaml:"type" json:"type"`
+	Optional    bool   `yaml:"optional" json:"optional"`
 }
 
 // Parameters is a list of Parameters
@@ -98,10 +90,10 @@ type Parameters map[string]Parameter
 
 // Dependency is the docker informations about the Dependency
 type Dependency struct {
-	Image   string   `yaml:"image"`
-	Volumes []string `yaml:"volumes"`
-	Ports   []string `yaml:"ports"`
-	Command string   `yaml:"command"`
+	Image   string   `yaml:"image" json:"image"`
+	Volumes []string `yaml:"volumes" json:"volumes"`
+	Ports   []string `yaml:"ports" json:"ports"`
+	Command string   `yaml:"command" json:"command"`
 }
 
 // Dependencies is a list of Dependencies
