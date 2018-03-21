4a5b117f2077e61d8192f1206a262159b50722d7
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-21 17:06:50 +0200

    add creation of swarm node if not exists yet

--- service/docker.go
+++ service/docker.go
@@ -1,6 +1,13 @@
 package service
 
 import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/logrusorgru/aurora"
+
 	docker "github.com/fsouza/go-dockerclient"
 )
 
@@ -10,7 +17,23 @@ var DockerEndpoint = "unix:///var/run/docker.sock"
 var dockerCli *docker.Client
 
 func createDockerCli(endpoint string) (client *docker.Client, err error) {
-	return docker.NewClient(endpoint)
+	client, err = docker.NewClient(endpoint)
+	info, err := client.Info()
+	if info.Swarm.NodeID != "" {
+		return
+	}
+	// TODO remove and make CI works
+	if os.Getenv("CI") == "true" {
+		return
+	}
+	res, err := client.InitSwarm(docker.InitSwarmOptions{
+		Context: context.Background(),
+		InitRequest: swarm.InitRequest{
+			ListenAddr: "0.0.0.0:2377", // https://docs.docker.com/engine/reference/commandline/swarm_init/#usage
+		},
+	})
+	fmt.Println(aurora.Green("Docker swarm node created"), res)
+	return
 }
 
 func init() {
