9a0671e31148a3d37ed3ccf521bffffa09feb606
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-24 19:39:17 +0200

    add delete command

--- cmd/account/delete.go
+++ cmd/account/delete.go
@@ -3,6 +3,7 @@ package cmdAccount
 import (
 	"fmt"
 
+	"github.com/mesg-foundation/application/account"
 	"github.com/mesg-foundation/application/cmd/utils"
 	"github.com/spf13/cobra"
 )
@@ -20,8 +21,10 @@ mesg-cli service delete`,
 func deleteHandler(cmd *cobra.Command, args []string) {
 	acc := cmdUtils.AccountFromFlagOrAsk(cmd, "Choose the account you want to delete")
 	if cmdUtils.Confirm(cmd, "The account "+acc.Address.String()+" will be deleted. Are you sure ?") {
-		// TODO add real deletion
-		fmt.Println("account deleted", acc)
+		if err := account.Destroy(acc); err != nil {
+			panic(err)
+		}
+		fmt.Println("Account deleted")
 	}
 }
 
