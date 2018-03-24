13eeff455e5de7df3323e78b9126f7e0cc003516
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-24 20:19:59 +0200

    add serilaization tests

--- account/serialization_test.go
+++ account/serialization_test.go
@@ -1,17 +1,38 @@
 package account
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stvp/assert"
 )
 
 func TestImport(t *testing.T) {
-	account, _ := Import("/...", "test")
-	assert.NotEqual(t, account.Address, "")
+	acc, _ := Generate("test")
+	account, err := Import(acc.URL.Path, "test", "test")
+	assert.Nil(t, err)
+	assert.NotEqual(t, account.Address.String(), "")
+	Destroy(acc)
 }
 
-func TestImportWithDefaultName(t *testing.T) {
-	account, _ := Import("/...", "")
-	assert.NotEqual(t, account.Address, "")
+func TestImportWrongPassword(t *testing.T) {
+	acc, _ := Generate("test")
+	_, err := Import(acc.URL.Path, "wrongpassword", "test")
+	assert.NotNil(t, err)
+	Destroy(acc)
+}
+
+func TestExport(t *testing.T) {
+	acc, _ := Generate("test")
+	err := Export(acc, "test", "test", "./TestExport")
+	assert.Nil(t, err)
+	Destroy(acc)
+	os.Remove("./TestExport")
+}
+
+func TestExportWrongPath(t *testing.T) {
+	acc, _ := Generate("test")
+	err := Export(acc, "test", "test", "./")
+	assert.NotNil(t, err)
+	Destroy(acc)
 }
