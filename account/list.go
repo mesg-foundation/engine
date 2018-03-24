61b37f038ad73eebc7d3603ce07980685da00241
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-24 19:14:06 +0200

    refactore account list

--- account/list.go
+++ account/list.go
@@ -1,19 +1,15 @@
 package account
 
 import (
-	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/mesg-foundation/application/config"
 )
 
 // List all available accounts on this computer
-func List() (accountList []*Account) {
-	store := keystore.NewKeyStore(config.AccountDirectory, keystore.StandardScryptN, keystore.StandardScryptP)
-	for _, wallet := range store.Wallets() {
-		for _, account := range wallet.Accounts() {
-			accountList = append(accountList, &Account{
-				Address: account.Address,
-				URL:     account.URL.String(),
-			})
+func List() (accountList []accounts.Account) {
+	for _, wallet := range config.Store.Wallets() {
+		for _, a := range wallet.Accounts() {
+			accountList = append(accountList, a)
 		}
 	}
 	return accountList
