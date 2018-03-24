3db5d4858956b9d2b3cb20400db74c22b20b98e8
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-24 19:13:54 +0200

    refactor account generate

--- account/generate.go
+++ account/generate.go
@@ -3,36 +3,18 @@ package account
 import (
 	"errors"
 
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/mesg-foundation/application/config"
 )
 
-// TODO add real account creation
-func generate(password string, name string) (address common.Address, err error) {
+// Generate create a new account based on a password
+func Generate(password string) (acc accounts.Account, err error) {
 	if password == "" {
 		err = errors.New("Password is missing")
 		return
 	}
-	if name == "" {
-		err = errors.New("Name is missing")
-		return
-	}
 
-	scryptN := keystore.StandardScryptN
-	scryptP := keystore.StandardScryptP
+	acc, err = config.Store.NewAccount(password)
 
-	address, err = keystore.StoreKey(config.AccountDirectory, password, scryptN, scryptP)
-
-	return
-}
-
-// Generate an account based on some predefined data
-func (account *Account) Generate() (err error) {
-	addr, err := generate(account.Password, account.Name)
-	if err != nil {
-		return
-	}
-	account.Address = addr
 	return
 }
