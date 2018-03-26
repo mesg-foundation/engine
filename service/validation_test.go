2bcad9385bf2cf623e3a76ed6b6aa8e201beff36
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-26 15:58:48 +0300

    add validation of the path of a service

--- service/validation_test.go
+++ service/validation_test.go
@@ -38,3 +38,27 @@ func TestInvalidFile(t *testing.T) {
 	assert.Equal(t, valid, false)
 	assert.Equal(t, len(warnings), 1)
 }
+
+func TestValidPath(t *testing.T) {
+	valid, warnings, err := ValidService("./tests/service-valid")
+	assert.Nil(t, err)
+	assert.Equal(t, valid, true)
+	assert.Equal(t, len(warnings), 0)
+}
+
+func TestValidPathInvalidFile(t *testing.T) {
+	valid, warnings, err := ValidService("./tests/service-file-invalid")
+	assert.Nil(t, err)
+	assert.Equal(t, valid, false)
+	assert.Equal(t, len(warnings), 1)
+}
+
+func TestValidPathMissingYml(t *testing.T) {
+	_, _, err := ValidService("./tests/service-file-missing")
+	assert.NotNil(t, err)
+}
+
+func TestValidPathMissingDocker(t *testing.T) {
+	_, _, err := ValidService("./tests/service-docker-missing")
+	assert.NotNil(t, err)
+}
