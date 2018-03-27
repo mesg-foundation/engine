a232f2646da21dc68ee4bbeb3d8d55500a2b1511
Nicolas Mahe <nicolas@mahe.me>
2018-03-27 16:10:58 +0700

    Improve doc and output of service.init command

--- cmd/service/init.go
+++ cmd/service/init.go
@@ -7,20 +7,22 @@ import (
 	"path/filepath"
 	"strings"
 
-	"gopkg.in/yaml.v2"
-
-	"gopkg.in/AlecAivazis/survey.v1"
-
+	"github.com/logrusorgru/aurora"
 	"github.com/mesg-foundation/application/service"
-
 	"github.com/spf13/cobra"
+	"gopkg.in/AlecAivazis/survey.v1"
+	"gopkg.in/yaml.v2"
 )
 
 // Init run the Init command for a service
 var Init = &cobra.Command{
-	Use:               "init NAME",
-	Short:             "Initialize a service",
-	Example:           "mesg-cli service init",
+	Use:   "init",
+	Short: "Initialize a service",
+	Long: `Initialize a service by creating a mesg.yml and Dockerfile in a dedicated folder.
+	
+To get more information, see the page [service file from the documentation](https://docs.mesg.tech/service/service-file.html)`,
+	Example: `mesg-cli service init
+mesg-cli service init --name NAME --description DESCRIPTION --visibility ALL --publish ALL`,
 	Run:               initHandler,
 	DisableAutoGenTag: true,
 }
@@ -30,20 +32,22 @@ func initHandler(cmd *cobra.Command, args []string) {
 	res.Publish = service.PublishAll
 	res.Visibility = service.VisibilityAll
 
+	fmt.Printf("%s\n", aurora.Bold("Initialization of a new service"))
+
 	res.Name = cmd.Flag("name").Value.String()
-	if res.Name == "" && survey.AskOne(&survey.Input{Message: "Name of your service"}, &res.Name, nil) != nil {
+	if res.Name == "" && survey.AskOne(&survey.Input{Message: "Name:"}, &res.Name, nil) != nil {
 		os.Exit(0)
 	}
 	key := strings.Replace(strings.ToLower(res.Name), " ", "-", -1)
 
 	res.Description = cmd.Flag("description").Value.String()
-	if res.Description == "" && survey.AskOne(&survey.Input{Message: "Description of your service"}, &res.Description, nil) != nil {
+	if res.Description == "" && survey.AskOne(&survey.Input{Message: "Description:"}, &res.Description, nil) != nil {
 		os.Exit(0)
 	}
 
 	publishStr := cmd.Flag("publish").Value.String()
 	if publishStr == "" && survey.AskOne(&survey.Select{
-		Message: "Publish details of the service (ALL)",
+		Message: "Publish (ALL):",
 		Options: []string{
 			string(service.PublishAll),
 			string(service.PublishSource),
@@ -57,7 +61,7 @@ func initHandler(cmd *cobra.Command, args []string) {
 
 	visibilityStr := cmd.Flag("visibility").Value.String()
 	if visibilityStr == "" && survey.AskOne(&survey.Select{
-		Message: "Visibility of the service (ALL)",
+		Message: "Visibility (ALL):",
 		Options: []string{
 			string(service.VisibilityAll),
 			string(service.VisibilityUsers),
@@ -80,9 +84,17 @@ func initHandler(cmd *cobra.Command, args []string) {
 		panic(err)
 	}
 	fmt.Println()
-	fmt.Println(string(out))
+	fmt.Printf("%s\n", aurora.Brown("Summary:").Bold())
+	fmt.Printf("%s\n", string(out))
+	// fmt.Print(string(out))
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s %s\n", aurora.Bold("Service will be created in:"), aurora.Brown(filepath.Join(dir, key)))
+
 	ok := false
-	if survey.AskOne(&survey.Confirm{Message: "Is this ok ?", Default: true}, &ok, nil) != nil {
+	if survey.AskOne(&survey.Confirm{Message: "Is this correct?", Default: true}, &ok, nil) != nil {
 		os.Exit(0)
 	}
 	if !ok {
@@ -100,11 +112,13 @@ func initHandler(cmd *cobra.Command, args []string) {
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Printf("%s\n", aurora.Green("Service created with success").Bold())
 }
 
 func init() {
-	Init.Flags().StringP("name", "n", "", "Name of the service")
-	Init.Flags().StringP("description", "d", "", "Description of the service")
-	Init.Flags().StringP("visibility", "v", "", "Visibility of the service")
-	Init.Flags().StringP("publish", "p", "", "Publish details of the service")
+	Init.Flags().StringP("name", "n", "", "Name")
+	Init.Flags().StringP("description", "d", "", "Description")
+	Init.Flags().StringP("visibility", "v", "", "Visibility")
+	Init.Flags().StringP("publish", "p", "", "Publish")
 }
