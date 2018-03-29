cf129c668f73efadeeb6a93aea7dff326f490ced
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-29 17:37:50 +0700

    add sign method

---
+++ account/sign_test.go
@@ -0,0 +1,20 @@
+package account
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/stvp/assert"
+)
+
+func TestSign(t *testing.T) {
+	account, _ := Generate("TestSign")
+	amount := big.NewInt(0)
+	gasPrice := big.NewInt(0)
+	tx := types.NewTransaction(0, account.Address, amount, 21000, gasPrice, []byte(""))
+	res, err := Sign(account, "TestSign", tx)
+	assert.Nil(t, err)
+	assert.NotNil(t, res)
+	Destroy(account)
+}
