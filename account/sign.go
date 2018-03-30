0c98e058b1e4df86c28ab3808097564426ee1c51
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-03-30 12:18:43 +0700

    configure network id

--- account/sign.go
+++ account/sign.go
@@ -1,16 +1,14 @@
 package account
 
 import (
-	"math/big"
-
 	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/mesg-foundation/application/config"
 )
 
-// Sign some data based on an account
-func Sign(account accounts.Account, password string, tx *types.Transaction) (transaction *types.Transaction, err error) {
+// sign some data based on an account
+func signTx(account accounts.Account, password string, tx *types.Transaction) (transaction *types.Transaction, err error) {
 	// TODO check the last parameter
-	transaction, err = config.Store.SignTxWithPassphrase(account, password, tx, big.NewInt(0))
+	transaction, err = config.Store.SignTxWithPassphrase(account, password, tx, config.NetworkID)
 	return
 }
---
+++ config/networkId.go
@@ -0,0 +1,6 @@
+package config
+
+import "math/big"
+
+// NetworkID is the ID to identify the network
+var NetworkID = big.NewInt(1309467321975)
