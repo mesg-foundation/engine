75835d1c242ac549ec97645a4307f215936f4fb4
Nicolas Mahe <nicolas@mahe.me>
2018-04-17 12:14:37 +0700

    Refacto service config in config package with viper

--- cmd/root.go
+++ cmd/root.go
@@ -46,7 +46,8 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
+	err := viper.ReadInConfig()
+	if err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
