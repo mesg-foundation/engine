2a22b461d167e834659027bcfc4b930946272e71
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-04-18 11:19:46 +0700

    WIP add client api

---
+++ api/client/event/type.go
@@ -0,0 +1,4 @@
+package event
+
+// Server is the type used to create all apis for this package
+type Server int
---
+++ api/client/service/type.go
@@ -0,0 +1,4 @@
+package service
+
+// Server is the type used to create all apis for this package
+type Server int
