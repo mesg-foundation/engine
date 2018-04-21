4e88b4640f4ac8facd7278378f2d474501db58ea
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-04-21 17:41:26 +0700

    add service api

--- api/service/stop.go
+++ api/service/stop.go
@@ -1,13 +1,15 @@
 package service
 
 import (
-	"fmt"
-
-	types "github.com/mesg-foundation/application/types"
+	"github.com/mesg-foundation/application/service"
+	"github.com/mesg-foundation/application/types"
 	"golang.org/x/net/context"
 )
 
+// Stop a service
 func (s *Server) Stop(ctx context.Context, request *types.StopServiceRequest) (reply *types.ServiceReply, err error) {
-	fmt.Println("stop")
+	service := service.New(request.Service)
+	err = service.Stop()
+	reply = &types.ServiceReply{}
 	return
 }
