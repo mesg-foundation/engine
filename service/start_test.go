bd7b5bf649f919155bd23f5a0b0695c27d008815
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-04-21 12:51:02 +0700

    use basic types no protobuff generation

--- service/start_test.go
+++ service/start_test.go
@@ -15,15 +15,15 @@ func TestStartService(t *testing.T) {
 	}
 	service := &Service{
 		Name: "TestStartService",
-		Dependencies: map[string]*Dependency{
-			"test": &Dependency{
+		Dependencies: map[string]Dependency{
+			"test": Dependency{
 				Image: "nginx",
 			},
 		},
 	}
 	dockerServices, err := service.Start()
 	assert.Nil(t, err)
-	assert.Equal(t, len(dockerServices), len(service.GetDependencies()))
+	assert.Equal(t, len(dockerServices), len(service.Dependencies))
 	assert.Equal(t, service.IsRunning(), true)
 	service.Stop()
 }
@@ -35,8 +35,8 @@ func TestStartAgainService(t *testing.T) {
 	}
 	service := &Service{
 		Name: "TestStartAgainService",
-		Dependencies: map[string]*Dependency{
-			"test": &Dependency{
+		Dependencies: map[string]Dependency{
+			"test": Dependency{
 				Image: "nginx",
 			},
 		},
@@ -56,21 +56,21 @@ func TestPartiallyRunningService(t *testing.T) {
 	}
 	service := &Service{
 		Name: "TestPartiallyRunningService",
-		Dependencies: map[string]*Dependency{
-			"test": &Dependency{
+		Dependencies: map[string]Dependency{
+			"test": Dependency{
 				Image: "nginx",
 			},
-			"test2": &Dependency{
+			"test2": Dependency{
 				Image: "nginx",
 			},
 		},
 	}
 	service.Start()
-	service.GetDependencies()["test"].Stop(service.namespace(), "test")
+	service.Dependencies["test"].Stop(service.namespace(), "test")
 	assert.Equal(t, service.IsPartiallyRunning(), true)
 	dockerServices, err := service.Start()
 	assert.Nil(t, err)
-	assert.Equal(t, len(dockerServices), len(service.GetDependencies()))
+	assert.Equal(t, len(dockerServices), len(service.Dependencies))
 	assert.Equal(t, service.IsRunning(), true)
 	service.Stop()
 }
@@ -100,8 +100,8 @@ func TestNetworkCreated(t *testing.T) {
 	}
 	service := &Service{
 		Name: "TestNetworkCreated",
-		Dependencies: map[string]*Dependency{
-			"test": &Dependency{
+		Dependencies: map[string]Dependency{
+			"test": Dependency{
 				Image: "nginx",
 			},
 		},
