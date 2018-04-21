b0a654c9fcf8e1ebbab85b1a1a58d162c2e01d46
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-04-21 12:29:03 +0700

    remove useless files

--- api/client/event/type.go
+++
@@ -1,4 +0,0 @@
-package event
-
-// Server is the type used to create all apis for this package
-type Server int
--- api/client/task/type.go
+++
@@ -1,4 +0,0 @@
-package task
-
-// Server is the type used to create all apis for this package
-type Server int
