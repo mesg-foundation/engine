e0d186155499a284123e7e3e646b771cc378082a
Nicolas Mahe <nicolas@mahe.me>
2018-04-23 21:27:17 +0700

    Rename pubsub key to channel, taskKey and eventKey to taskChannel and eventChannel

--- api/task/listen.go
+++ api/task/listen.go
@@ -10,7 +10,7 @@ import (
 func (s *Server) Listen(request *types.ListenTaskRequest, stream types.Task_ListenServer) (err error) {
 	service := service.New(request.Service)
 
-	subscription := pubsub.Subscribe(service.TaskSubscriptionKey())
+	subscription := pubsub.Subscribe(service.TaskSubscriptionChannel())
 
 	for data := range subscription {
 		taskReply := data.(*types.TaskReply)
