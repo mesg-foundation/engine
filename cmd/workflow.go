e1e3690a62dc1808c8b13d3747081a998a736955
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-04-24 11:50:25 +0700

    remove some not implemented commands from cli

--- cmd/workflow.go
+++ cmd/workflow.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"github.com/mesg-foundation/core/cmd/workflow"
-
 	"github.com/spf13/cobra"
 )
 
@@ -13,18 +11,19 @@ var Workflow = &cobra.Command{
 	DisableAutoGenTag: true,
 }
 
+// TODO this command is disabled for now waiting for the Workflow feature to be implemented
 func init() {
 
-	Workflow.AddCommand(cmdWorkflow.Pause)
-	Workflow.AddCommand(cmdWorkflow.Resume)
-	Workflow.AddCommand(cmdWorkflow.Deploy)
-	Workflow.AddCommand(cmdWorkflow.Test)
-	Workflow.AddCommand(cmdWorkflow.Validate)
-	Workflow.AddCommand(cmdWorkflow.List)
-	Workflow.AddCommand(cmdWorkflow.Kill)
-	Workflow.AddCommand(cmdWorkflow.Topup)
-	Workflow.AddCommand(cmdWorkflow.Log)
-	Workflow.AddCommand(cmdWorkflow.Detail)
+	// Workflow.AddCommand(cmdWorkflow.Pause)
+	// Workflow.AddCommand(cmdWorkflow.Resume)
+	// Workflow.AddCommand(cmdWorkflow.Deploy)
+	// Workflow.AddCommand(cmdWorkflow.Test)
+	// Workflow.AddCommand(cmdWorkflow.Validate)
+	// Workflow.AddCommand(cmdWorkflow.List)
+	// Workflow.AddCommand(cmdWorkflow.Kill)
+	// Workflow.AddCommand(cmdWorkflow.Topup)
+	// Workflow.AddCommand(cmdWorkflow.Log)
+	// Workflow.AddCommand(cmdWorkflow.Detail)
 
-	RootCmd.AddCommand(Workflow)
+	// RootCmd.AddCommand(Workflow)
 }
