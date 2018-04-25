e6e691cb0a33f78b6b0537cbd6ef57811d92ef4a
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-04-25 10:24:09 +0700

    update service status command

--- cmd/service/status.go
+++ cmd/service/status.go
@@ -25,8 +25,9 @@ func statusHandler(cmd *cobra.Command, args []string) {
 		fmt.Println(aurora.Red(err))
 		os.Exit(0)
 	}
+	fmt.Println("Services running :")
 	for _, service := range services {
-		fmt.Println(aurora.Green(service + " running"))
+		fmt.Println(aurora.Bold(" - " + service))
 	}
 	if len(services) == 0 {
 		fmt.Println("No services running")
