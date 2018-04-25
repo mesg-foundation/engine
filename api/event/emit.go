fdcc69d0b1988d961b22f60d1616a1e15be63ad3
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-04-25 12:54:39 +0700

    update logging for event receive

--- api/event/emit.go
+++ api/event/emit.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/logrusorgru/aurora"
 	"github.com/mesg-foundation/core/pubsub"
 	"github.com/mesg-foundation/core/service"
 	"github.com/mesg-foundation/core/types"
@@ -11,7 +12,7 @@ import (
 
 // Emit a new event
 func (s *Server) Emit(context context.Context, request *types.EmitEventRequest) (reply *types.EventReply, err error) {
-	log.Println("Receive emit request", request)
+	log.Println("Receive event", aurora.Green(request.Event), "from", aurora.Green(request.Service.Name), ":", aurora.Bold(request.Data))
 
 	service := service.New(request.Service)
 
