d02cfc1d9df2c052cb78a844a28f0fcaf1da610a
Nicolas Mahe <nicolas@mahe.me>
2018-04-27 14:35:02 +0200

    test build command

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+pwd
+
 go get -u github.com/karalabe/xgo
 
 mkdir -p bin
 cd bin
 
 xgo \
+ -x -v
   --targets=linux/amd64,linux/386,darwin/amd64,windows/amd64,windows/386 \
   ../cli
