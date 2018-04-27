ecef56a71ae12f36708b37cd27381f644b42e61f
Nicolas Mahe <nicolas@mahe.me>
2018-04-27 15:28:10 +0200

    test build command

--- build/build.sh
+++ build/build.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
-go get -u github.com/karalabe/xgo
-
-pwd
-
-cd /go/src/github.com/mesg-foundation/core
+# go get -u github.com/karalabe/xgo
 
 mkdir -p bin
 cd bin
 
-pwd
+go build ../cli
 
-echo $GOPATH
-GOPATH="/go"
-export GOPATH
-
-xgo \
- -x -v \
-  --targets=linux/amd64,linux/386,darwin/amd64,windows/amd64,windows/386 \
-  /go/src/github.com/mesg-foundation/core/cli
+# xgo \
+#  -x -v \
+#   --targets=linux/amd64,linux/386,darwin/amd64,windows/amd64,windows/386 \
+#   /go/src/github.com/mesg-foundation/core/cli
