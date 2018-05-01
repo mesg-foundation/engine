06b6d6f74672a65849b1f2b5989bc9ba25e18c0d
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-01 12:36:46 +0700

    add execution package

---
+++ execution/create.go
@@ -0,0 +1,48 @@
+package execution
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/mesg-foundation/core/service"
+	"github.com/mesg-foundation/core/utils/hash"
+)
+
+func Create(service *service.Service, task string, inputs interface{}) (execution *Execution, err error) {
+	if !taskExists(service, task) {
+		err = errors.New("Task " + task + " doesn't exists in service " + service.Name)
+		return
+	}
+	execution = &Execution{
+		Service:   service,
+		Inputs:    inputs,
+		Task:      task,
+		CreatedAt: time.Now(),
+	}
+	execution.ID = generateID(execution)
+	mu.Lock()
+	defer mu.Unlock()
+	pendingExecutions[execution.ID] = execution
+	log.Println("[PENDING]", task)
+	return
+}
+
+func taskExists(service *service.Service, name string) (exists bool) {
+	exists = false
+	for taskName := range service.Tasks {
+		if taskName == name {
+			exists = true
+			break
+		}
+	}
+	return
+}
+
+func generateID(execution *Execution) (id string) {
+	return hash.Calculate([]string{
+		execution.CreatedAt.UTC().String(),
+		execution.Service.Name,
+		execution.Task,
+	})
+}
---
+++ execution/state_test.go
@@ -0,0 +1,61 @@
+package execution
+
+import (
+	"testing"
+
+	"github.com/mesg-foundation/core/service"
+	"github.com/mesg-foundation/core/types"
+	"github.com/stvp/assert"
+)
+
+func TestMoveFromPendingToInProgress(t *testing.T) {
+	s := service.Service{
+		Name: "TestMoveFromPendingToInProgress",
+		Tasks: map[string]*types.ProtoTask{
+			"test": &types.ProtoTask{},
+		},
+	}
+	var inputs interface{}
+	exec, _ := Create(&s, "test", inputs)
+	err := exec.moveFromPendingToInProgress()
+	assert.Equal(t, inProgressExecutions[exec.ID], exec)
+	assert.Nil(t, pendingExecutions[exec.ID])
+	assert.Nil(t, err)
+}
+
+func TestMoveFromPendingToInProgressNonExistingTask(t *testing.T) {
+	exec := Execution{ID: "test"}
+	err := exec.moveFromPendingToInProgress()
+	assert.NotNil(t, err)
+	assert.Nil(t, pendingExecutions[exec.ID])
+}
+
+func TestMoveFromInProgressToCompleted(t *testing.T) {
+	s := service.Service{
+		Name: "TestMoveFromInProgressToCompleted",
+		Tasks: map[string]*types.ProtoTask{
+			"test": &types.ProtoTask{},
+		},
+	}
+	var inputs interface{}
+	exec, _ := Create(&s, "test", inputs)
+	exec.moveFromPendingToInProgress()
+	err := exec.moveFromInProgressToProcessed()
+	assert.Equal(t, processedExecutions[exec.ID], exec)
+	assert.Nil(t, inProgressExecutions[exec.ID])
+	assert.Nil(t, err)
+}
+
+func TestMoveFromInProgressToCompletedNonExistingTask(t *testing.T) {
+	s := service.Service{
+		Name: "TestMoveFromInProgressToCompletedNonExistingTask",
+		Tasks: map[string]*types.ProtoTask{
+			"test": &types.ProtoTask{},
+		},
+	}
+	var inputs interface{}
+	exec, _ := Create(&s, "test", inputs)
+	err := exec.moveFromInProgressToProcessed()
+	assert.NotNil(t, err)
+	assert.Nil(t, inProgressExecutions[exec.ID])
+}
