598326dbdeb159ed29a119cd5a1dbfb00a991324
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-01 12:36:46 +0700

    add execution package

---
+++ execution/state.go
@@ -0,0 +1,37 @@
+package execution
+
+import (
+	"errors"
+	"sync"
+)
+
+var mu sync.Mutex
+var pendingExecutions = make(map[string]*Execution)
+var inProgressExecutions = make(map[string]*Execution)
+var processedExecutions = make(map[string]*Execution)
+
+func (execution *Execution) moveFromPendingToInProgress() (err error) {
+	mu.Lock()
+	defer mu.Unlock()
+	e, ok := pendingExecutions[execution.ID]
+	if !ok {
+		err = errors.New("Execution " + execution.ID + " not in the pending")
+		return
+	}
+	inProgressExecutions[execution.ID] = e
+	delete(pendingExecutions, execution.ID)
+	return
+}
+
+func (execution *Execution) moveFromInProgressToProcessed() (err error) {
+	mu.Lock()
+	defer mu.Unlock()
+	e, ok := inProgressExecutions[execution.ID]
+	if !ok {
+		err = errors.New("Execution " + execution.ID + " not in progress")
+		return
+	}
+	processedExecutions[execution.ID] = e
+	delete(inProgressExecutions, execution.ID)
+	return
+}
