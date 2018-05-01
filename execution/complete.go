17f0d5aebfc22cc73a3598c7e5f2a9dcbb00098e
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-01 16:04:24 +0700

    publish result on execution completion

--- execution/complete.go
+++ execution/complete.go
@@ -1,12 +1,16 @@
 package execution
 
 import (
+	"encoding/json"
 	"log"
 	"time"
+
+	"github.com/mesg-foundation/core/pubsub"
+	"github.com/mesg-foundation/core/types"
 )
 
 // Complete mark an execution as complete and put it in the list of processed tasks
-func (execution *Execution) Complete(output string, data interface{}) (err error) {
+func (execution *Execution) Complete(output string, data interface{}) (reply *types.ResultReply, err error) {
 	err = execution.moveFromInProgressToProcessed()
 	if err != nil {
 		return
@@ -14,6 +18,21 @@ func (execution *Execution) Complete(output string, data interface{}) (err error
 	execution.ExecutionDuration = time.Since(execution.ExecutedAt)
 	execution.Output = output
 	execution.OutputData = data
+
+	outputJSON, err := json.Marshal(data)
+	if err != nil {
+		return
+	}
+
+	reply = &types.ResultReply{
+		Task:   execution.Task,
+		Output: output,
+		Data:   string(outputJSON),
+	}
+
 	log.Println("[COMPLETED]", execution.Task)
+
+	go pubsub.Publish(execution.Service.ResultSubscriptionChannel(), reply)
+
 	return
 }
