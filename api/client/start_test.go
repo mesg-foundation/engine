9d5649c8cc80b48ddd21c714c42325a2619c158a
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-07 16:09:30 +0700

    remove todos and hack in tests

--- api/client/start_test.go
+++ api/client/start_test.go
@@ -3,7 +3,6 @@ package client
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/mesg-foundation/core/service"
 	"github.com/stvp/assert"
@@ -23,11 +22,6 @@ func TestStartService(t *testing.T) {
 	reply, err := serverstart.StartService(context.Background(), &StartServiceRequest{
 		Service: &service,
 	})
-	// TODO: Remove this sleep
-	// Sleep added because the test are failling, probably a concurrence issue between
-	// the test and the docker api. On a local machine it works fine but maybe the CI
-	// have some delay when starting docker
-	time.Sleep(2 * time.Second)
 	assert.Nil(t, err)
 	assert.True(t, service.IsRunning())
 	assert.NotNil(t, reply)
