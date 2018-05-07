35e583e3e891c1ee35552ae26586a79b6a59eb99
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-07 13:04:24 +0700

    move configs in config path

--- cmd/root.go
+++ cmd/root.go
@@ -1,20 +1,15 @@
 package cmd
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/mesg-foundation/core/cmd/utils"
 	"github.com/mesg-foundation/core/config"
 
 	"github.com/mesg-foundation/core/api"
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
@@ -22,37 +17,6 @@ var RootCmd = &cobra.Command{
 	Short:             "MESG CLI",
 	Run:               rootHandler,
 	DisableAutoGenTag: true,
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".mesg" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".mesg")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	err := viper.ReadInConfig()
-	if err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
 
 func rootHandler(cmd *cobra.Command, args []string) {
