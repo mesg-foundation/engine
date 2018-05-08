8d816823b4e152fdd6cd671f266cc586351a8d63
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-08 12:06:20 +0700

    add daemon commands in cli

---
+++ cmd/daemon/container.go
@@ -0,0 +1,50 @@
+package daemon
+
+import (
+	"context"
+
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/fsouza/go-dockerclient"
+)
+
+func container() (*docker.APIContainers, error) {
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		return nil, nil
+	}
+	res, err := client.ListContainers(docker.ListContainersOptions{
+		Context: context.Background(),
+		Limit:   1,
+		Filters: map[string][]string{
+			"ancestor": []string{image},
+			"status":   []string{"running"},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(res) == 0 {
+		return nil, nil
+	}
+	return &res[0], nil
+}
+
+func service() (*swarm.Service, error) {
+	client, err := docker.NewClientFromEnv()
+	if err != nil {
+		return nil, nil
+	}
+	res, err := client.ListServices(docker.ListServicesOptions{
+		Context: context.Background(),
+		Filters: map[string][]string{
+			"name": []string{name},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(res) == 0 {
+		return nil, nil
+	}
+	return &res[0], nil
+}
---
+++ cmd/daemon/start.go
@@ -0,0 +1,94 @@
+package daemon
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/mesg-foundation/core/cmd/utils"
+
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/mesg-foundation/core/config"
+	"github.com/spf13/viper"
+
+	docker "github.com/fsouza/go-dockerclient"
+	"github.com/logrusorgru/aurora"
+	"github.com/spf13/cobra"
+)
+
+// Start the daemon
+var Start = &cobra.Command{
+	Use:               "start",
+	Short:             "Start the daemon",
+	Run:               startHandler,
+	DisableAutoGenTag: true,
+}
+
+func startHandler(cmd *cobra.Command, args []string) {
+	running, err := isRunning()
+	if err != nil {
+		fmt.Println(aurora.Red(err))
+	}
+	if !running {
+		client, err := docker.NewClientFromEnv()
+		if err != nil {
+			fmt.Println(aurora.Red(err))
+		}
+
+		_, err = client.CreateService(serviceConfig())
+		if err != nil {
+			fmt.Println(aurora.Red(err))
+			return
+		}
+
+		spinner := cmdUtils.StartSpinner(cmdUtils.SpinnerOptions{})
+		for {
+			time.Sleep(500 * time.Millisecond)
+			container, _ := container()
+			if container != nil {
+				break
+			}
+		}
+		spinner.Stop()
+	}
+
+	fmt.Println(aurora.Green("Daemon running"))
+}
+
+func serviceConfig() docker.CreateServiceOptions {
+	return docker.CreateServiceOptions{
+		ServiceSpec: swarm.ServiceSpec{
+			Annotations: swarm.Annotations{
+				Name: name,
+			},
+			TaskTemplate: swarm.TaskSpec{
+				ContainerSpec: &swarm.ContainerSpec{
+					Image: image,
+					Env: []string{
+						"MESG.PATH=" + viper.GetString(config.MESGPath),
+					},
+					Mounts: []mount.Mount{
+						mount.Mount{
+							Source: socketPath,
+							Target: socketPath,
+						},
+						mount.Mount{
+							Source: viper.GetString(config.MESGPath),
+							Target: "/mesg",
+						},
+					},
+				},
+			},
+			EndpointSpec: &swarm.EndpointSpec{
+				Ports: []swarm.PortConfig{
+					swarm.PortConfig{
+						Protocol:      swarm.PortConfigProtocolTCP,
+						PublishMode:   swarm.PortConfigPublishModeIngress,
+						TargetPort:    50052,
+						PublishedPort: 50052,
+					},
+				},
+			},
+		},
+	}
+}
