89acd20f3011fe2ff4b9d3f3dc4a2f9898c12791
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-20 20:39:24 -0400

    update commands with new API

--- cmd/service/start.go
+++ cmd/service/start.go
@@ -1,7 +1,10 @@
 package cmdService
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/mesg-foundation/core/api/core"
 
 	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
@@ -12,16 +15,19 @@ var duration int
 
 // Start run the start command for a service
 var Start = &cobra.Command{
-	Use:               "start SERVICE_FOLDER",
+	Use:               "start SERVICE_ID",
 	Short:             "Start a service",
 	Long:              "Start a service from the published available services. You have to provide a stake value and duration.",
-	Example:           `mesg-core service start SERVICE_FOLDER`,
+	Example:           `mesg-core service start SERVICE_ID`,
+	Args:              cobra.MinimumNArgs(1),
 	Run:               startHandler,
 	DisableAutoGenTag: true,
 }
 
 func startHandler(cmd *cobra.Command, args []string) {
-	service := loadService(defaultPath(args))
-	startService(service)
+	_, err := cli.StartService(context.Background(), &core.StartServiceRequest{
+		ServiceID: args[0],
+	})
+	handleError(err)
 	fmt.Println(aurora.Green("Service started"))
 }
