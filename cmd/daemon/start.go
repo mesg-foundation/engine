42a299d1369be8c2384768c60f1af81a3dd2b9dc
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-26 13:47:05 -0700

    debug tests

--- cmd/daemon/start.go
+++ cmd/daemon/start.go
@@ -1,11 +1,14 @@
 package daemon
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	godocker "github.com/fsouza/go-dockerclient"
 	"github.com/logrusorgru/aurora"
-	"github.com/mesg-foundation/core/cmd/utils"
 	"github.com/mesg-foundation/core/daemon"
+	"github.com/mesg-foundation/core/docker"
 	"github.com/spf13/cobra"
 )
 
@@ -30,9 +33,21 @@ func startHandler(cmd *cobra.Command, args []string) {
 			return
 		}
 
-		spinner := cmdUtils.StartSpinner(cmdUtils.SpinnerOptions{Text: "Starting the daemon"})
+		// spinner := cmdUtils.StartSpinner(cmdUtils.SpinnerOptions{Text: "Starting the daemon"})
+		go func() {
+			client, _ := docker.Client()
+			for {
+				containers, _ := client.ListContainers(godocker.ListContainersOptions{
+					Context: context.Background(),
+				})
+				for _, c := range containers {
+					fmt.Println("id:", c.ID, "image:", c.Image, "name:", c.Names, "state", c.State, "status", c.Status)
+				}
+				time.Sleep(1 * time.Second)
+			}
+		}()
 		err = <-daemon.WaitForContainerToRun()
-		spinner.Stop()
+		// spinner.Stop()
 		if err != nil {
 			fmt.Println(aurora.Red(err))
 			return
