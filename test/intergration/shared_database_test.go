f2f85388999dc694058478952077444c9c59e639
Nicolas Mahe <nicolas@mahe.me>
2018-05-27 13:35:54 +0700

    Fix integration TestSharedDatabase

--- test/intergration/shared_database_test.go
+++ test/intergration/shared_database_test.go
@@ -3,6 +3,7 @@ package intergration_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/mesg-foundation/core/daemon"
 	"github.com/mesg-foundation/core/database/services"
@@ -15,17 +16,17 @@ import (
 	"google.golang.org/grpc"
 )
 
-func TestSharedDatabse(t *testing.T) {
+func TestSharedDatabase(t *testing.T) {
 	daemon.Start()
-	defer daemon.Stop()
 	err := <-daemon.WaitForContainerToRun()
 	assert.Nil(t, err)
+	time.Sleep(2 * time.Second)
 	connection, err := grpc.Dial(viper.GetString(config.APIClientTarget), grpc.WithInsecure())
 	assert.Nil(t, err)
 	cli := core.NewCoreClient(connection)
 	reply, err := cli.DeployService(context.Background(), &core.DeployServiceRequest{
 		Service: &service.Service{
-			Name: "TestSharedDatabse",
+			Name: "TestSharedDatabase",
 			Dependencies: map[string]*service.Dependency{
 				"test": &service.Dependency{
 					Image: "nginx",
@@ -34,8 +35,8 @@ func TestSharedDatabse(t *testing.T) {
 		},
 	})
 	assert.Nil(t, err)
-	service, err := services.Get(reply.ServiceID)
 	defer services.Delete(reply.ServiceID)
+	service, err := services.Get(reply.ServiceID)
 	assert.Nil(t, err)
-	assert.Equal(t, service.Name, "TestSharedDatabse")
+	assert.Equal(t, service.Name, "TestSharedDatabase")
 }
