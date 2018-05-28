02d410699529510aca73508bf1377a0c04e3935e
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-28 16:27:01 -0700

    make database accessible from different goroutine

--- database/services/db_test.go
+++ database/services/db_test.go
@@ -1,7 +1,10 @@
 package services
 
 import (
+	"sync"
 	"testing"
+
+	"github.com/mesg-foundation/core/service"
 
 	"github.com/stvp/assert"
 )
@@ -11,4 +14,25 @@ func TestDb(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
 	close()
+}
+
+// Test to stress the database with concurrency access
+// BUG: https://github.com/mesg-foundation/core/issues/163
+func TestConcurrency(t *testing.T) {
+	var wg sync.WaitGroup
+	service := &service.Service{
+		Name: "TestConcurrency",
+	}
+	hash, _ := Save(service)
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func() {
+			s, err := Get(hash)
+			assert.Nil(t, err)
+			assert.Equal(t, s.Name, service.Name)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	Delete(hash)
 }
