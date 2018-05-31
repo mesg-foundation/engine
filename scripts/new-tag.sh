3c82fb99942576d8d28bc1765829be270a4bc503
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-05-30 17:27:01 -0700

    update tag script

--- scripts/new-tag.sh
+++ scripts/new-tag.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
 const fs = require("fs")
-const exec = require('child_process').execFileSync
+const exec = require('child_process').execSync
 
 const version = process.argv[2]
 
 const changelog = fs.readFileSync("./CHANGELOG.md").toString()
 const date = (new Date()).toJSON().slice(0, 10)
+
+
+exec(`git tag ${version}`)
 
 const template = `## [Unreleased]
 
@@ -19,7 +22,6 @@ const template = `## [Unreleased]
 
 fs.writeFileSync("./CHANGELOG.md", changelog.replace("## [Unreleased]", template))
 
-exec(`git tag ${version}`)
 exec(`git add CHANGELOG.md`)
 exec(`git commit -m "Update version ${version}"`)
-exec(`git push && git push --tags`)
+exec(`git push && git push origin refs/tags/${version}`)
