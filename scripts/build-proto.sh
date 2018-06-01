2e8d9a67e6131b4535ebf713609004794184d4a7
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-01 16:43:13 -0700

    add custom configuration to services

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -6,9 +6,6 @@ PROJECT=github.com/mesg-foundation/core
 
 protoc --go_out=./ $PROJECT/service/service.proto
 
-for x in $PROJECT/api/*/; do
-  protoc \
-    --go_out=plugins=grpc:./ \
-    --proto_path=./ \
-    $x*.proto
-done
+# build Proto API
+protoc --go_out=plugins=grpc:./ --proto_path=./ $PROJECT/api/core/api.proto
+protoc --go_out=plugins=grpc:./ --proto_path=./ $PROJECT/api/service/api.proto
