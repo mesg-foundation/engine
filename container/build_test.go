9fce1781b7b2e26cc5bfaa21e25d69e5151eefcd
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-01 23:01:14 -0700

    add more tests for build

--- container/build_test.go
+++ container/build_test.go
@@ -11,3 +11,13 @@ func TestBuild(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotEqual(t, "", tag)
 }
+
+func TestBuildWrongPath(t *testing.T) {
+	_, err := Build("testss/")
+	assert.NotNil(t, err)
+}
+
+func TestBuildHash(t *testing.T) {
+	hash, _ := Build("test/")
+	assert.Equal(t, hash, "sha256:acf8f4e61c5bea3743c50f3ac96526d75518bd3290b7a8291d32ece917fd82a5")
+}
