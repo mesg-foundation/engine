21d2b23d0b150d059eb712ea3a3537d0a18643eb
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-02 12:52:59 -0700

    comment unused commands

--- cmd/service/resume.go
+++ cmd/service/resume.go
@@ -1,34 +1,28 @@
 package cmdService
 
-import (
-	"github.com/mesg-foundation/core/cmd/utils"
+// // Resume run the resume command for a service
+// var Resume = &cobra.Command{
+// 	Use:   "resume SERVICE_ID",
+// 	Short: "Resume a service",
+// 	Long: `Resume a previously paused service.
 
-	"github.com/spf13/cobra"
-)
+// To pause a service, see the [pause command](mesg-core_service_pause.md)`,
+// 	Args:              cobra.MinimumNArgs(1),
+// 	Example:           "mesg-core service resume SERVICE_ID --account ACCOUNT --confirm",
+// 	Run:               resumeHandler,
+// 	DisableAutoGenTag: true,
+// }
 
-// Resume run the resume command for a service
-var Resume = &cobra.Command{
-	Use:   "resume SERVICE_ID",
-	Short: "Resume a service",
-	Long: `Resume a previously paused service.
+// func resumeHandler(cmd *cobra.Command, args []string) {
+// 	account := cmdUtils.AccountFromFlagOrAsk(cmd, "Select an account:")
+// 	if !cmdUtils.Confirm(cmd, "Are you sure?") {
+// 		return
+// 	}
+// 	// TODO start and when ready resume (onchan) the service
+// 	fmt.Println("Service resumed with success", args, account)
+// }
 
-To pause a service, see the [pause command](mesg-core_service_pause.md)`,
-	Args:              cobra.MinimumNArgs(1),
-	Example:           "mesg-core service resume SERVICE_ID --account ACCOUNT --confirm",
-	Run:               resumeHandler,
-	DisableAutoGenTag: true,
-}
-
-func resumeHandler(cmd *cobra.Command, args []string) {
-	// account := cmdUtils.AccountFromFlagOrAsk(cmd, "Select an account:")
-	// if !cmdUtils.Confirm(cmd, "Are you sure?") {
-	// 	return
-	// }
-	// // TODO start and when ready resume (onchan) the service
-	// fmt.Println("Service resumed with success", args, account)
-}
-
-func init() {
-	cmdUtils.Confirmable(Resume)
-	cmdUtils.Accountable(Resume)
-}
+// func init() {
+// 	cmdUtils.Confirmable(Resume)
+// 	cmdUtils.Accountable(Resume)
+// }
