c314df4cf6648da440b497f22f93c36316fdc38d
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-02 12:25:17 -0700

    refactor build function

--- container/build_test.go
+++ container/build_test.go
@@ -1,8 +1,11 @@
 package container
 
 import (
+	"io/ioutil"
+	"strings"
 	"testing"
 
+	"github.com/docker/docker/api/types"
 	"github.com/stvp/assert"
 )
 
@@ -15,4 +18,33 @@ func TestBuild(t *testing.T) {
 func TestBuildWrongPath(t *testing.T) {
 	_, err := Build("testss/")
 	assert.NotNil(t, err)
+}
+
+func TestParseBuildResponseInvalidJSON(t *testing.T) {
+	body := ioutil.NopCloser(strings.NewReader("invalidJSON"))
+	response := types.ImageBuildResponse{
+		Body: body,
+	}
+	_, err := parseBuildResponse(response)
+	assert.NotNil(t, err)
+}
+
+func TestParseBuildResponse(t *testing.T) {
+	body := ioutil.NopCloser(strings.NewReader("{\"stream\":\"ok\"}"))
+	response := types.ImageBuildResponse{
+		Body: body,
+	}
+	tag, err := parseBuildResponse(response)
+	assert.Nil(t, err)
+	assert.Equal(t, tag, "ok")
+}
+
+func TestParseBuildResponseWithNewLine(t *testing.T) {
+	body := ioutil.NopCloser(strings.NewReader("{\"stream\":\"ok\\n\"}"))
+	response := types.ImageBuildResponse{
+		Body: body,
+	}
+	tag, err := parseBuildResponse(response)
+	assert.Nil(t, err)
+	assert.Equal(t, tag, "ok")
 }
