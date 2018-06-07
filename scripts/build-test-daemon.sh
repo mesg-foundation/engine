f0a58df47a7e5cc21d6fa7c3ae28640dd5b34e56
Nicolas Mahe <nicolas@mahe.me>
2018-06-07 12:02:42 +0700

    Rename mesg/daemon to mesg/core

--- scripts/build-test-daemon.sh
+++ scripts/build-test-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "building image mesg/daemon:local..."
-docker pull mesg/daemon:latest
-docker build -t mesg/daemon:build --target build .
-docker build -t mesg/daemon:local .
+echo "building image mesg/core:local..."
+docker pull mesg/core:latest
+docker build -t mesg/core:build --target build .
+docker build -t mesg/core:local .
