51ea5e5ee55be182a2415cff9fd39137b35f6b86
Nicolas Mahe <nicolas@mahe.me>
2018-06-14 15:49:55 +0700

    Add version number to Cobra

--- cli/main.go
+++ cli/main.go
@@ -6,6 +6,14 @@ import (
 	"github.com/mesg-foundation/core/cmd"
 )
 
+// version of this release. Will be replaced automatically when compiling in CI
+var version = "v1.0.0-beta"
+
+func init() {
+	cmd.RootCmd.Version = version
+	cmd.RootCmd.Short = cmd.RootCmd.Short + " " + version
+}
+
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
