f85d061ebf52e49d70b69da17935e1db03597ac0
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-16 17:42:06 +0700

    reduce visibility of the waitForContainerStatus function

--- container/service.go
+++ container/service.go
@@ -47,7 +47,7 @@ func StartService(options ServiceOptions) (serviceID string, err error) {
 		return
 	}
 	serviceID = response.ID
-	err = WaitForContainerStatus(options.Namespace, RUNNING)
+	err = waitForContainerStatus(options.Namespace, RUNNING)
 	return
 }
 
@@ -65,7 +65,7 @@ func StopService(namespace []string) (err error) {
 	if err != nil {
 		return
 	}
-	err = WaitForContainerStatus(namespace, STOPPED)
+	err = waitForContainerStatus(namespace, STOPPED)
 	return
 }
 
