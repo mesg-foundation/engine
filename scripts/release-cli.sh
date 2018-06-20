a8ebe2afc750e4304bdea4966d122c6793e873f6
Nicolas Mahe <nicolas@mahe.me>
2018-06-20 12:02:07 +0700

    Refacto release cli to a script

---
+++ scripts/release-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+go get ./...
+go get github.com/karalabe/xgo
+mkdir bin
+cd bin; xgo --targets=darwin/386,darwin/amd64,linux/386,linux/amd64,windows/386,windows/amd64 -ldflags="-X main.version=$1" -out mesg-core ../cli
+sudo chmod +x ./bin/*
+go get -u github.com/tcnksm/ghr
+ghr -u mesg-foundation -r core -delete $@ ./bin
