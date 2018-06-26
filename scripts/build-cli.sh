40fba1809ce2a023cbe571c55400266ca2d1d5b0
Nicolas Mahe <nicolas@mahe.me>
2018-06-26 10:59:14 +0700

    Fix build CLI command

--- scripts/build-cli.sh
+++ scripts/build-cli.sh
@@ -3,8 +3,9 @@
 go get ./...
 go get github.com/karalabe/xgo
 mkdir bin
-cd bin
-xgo --targets=darwin/386,darwin/amd64,linux/386,linux/amd64,windows/386,windows/amd64 -ldflags="-X main.version=$1" -out mesg-core ../cli
-pwd
-cd ../
+xgo --targets=darwin/386,darwin/amd64,linux/386,linux/amd64,windows/386,windows/amd64 \
+  -ldflags="-X 'main.version=$1'" \
+  -out mesg-core \
+  -dest ./bin \
+  ./cli
 sudo chmod +x ./bin/*
