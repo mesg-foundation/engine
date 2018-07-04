d0b394669421af906619b9cdd0fd30ae12a1e2ac
Nicolas Mahe <nicolas@mahe.me>
2018-07-04 16:00:37 +0700

    Improve command service gen doc text and explanation

--- cmd/service/docs.go
+++ cmd/service/docs.go
@@ -1,23 +1,25 @@
 package service
 
 import (
+	"fmt"
 	"html/template"
 	"os"
 	"path/filepath"
 
-	"gopkg.in/AlecAivazis/survey.v1"
-
+	"github.com/logrusorgru/aurora"
 	"github.com/mesg-foundation/core/cmd/service/assets"
 	"github.com/mesg-foundation/core/cmd/utils"
 	s "github.com/mesg-foundation/core/service"
 	"github.com/spf13/cobra"
+	survey "gopkg.in/AlecAivazis/survey.v1"
 )
 
 // Docs a service to the marketplace
 var Docs = &cobra.Command{
-	Use:               "gen-doc",
-	Short:             "Generate the documentation for the service in a README.md file",
-	Example:           `mesg-core service gen-doc PATH_TO_SERVICE`,
+	Use:   "gen-doc",
+	Short: "Generate the documentation for the service in a README.md file",
+	Example: `mesg-core service gen-doc
+mesg-core service gen-doc ./PATH_TO_SERVICE`,
 	Run:               genDocHandler,
 	DisableAutoGenTag: true,
 }
@@ -27,7 +29,7 @@ func genDocHandler(cmd *cobra.Command, args []string) {
 	readmePath := filepath.Join(path, "README.md")
 	if _, err := os.Stat(readmePath); err == nil {
 		var value bool
-		if survey.AskOne(&survey.Confirm{Message: "A README.md already exists do you want to overwrite it ?"}, &value, nil) != nil {
+		if survey.AskOne(&survey.Confirm{Message: "The file README.md already exists. Do you want to overwrite it?"}, &value, nil) != nil {
 			return
 		}
 		if !value {
@@ -48,4 +50,6 @@ func genDocHandler(cmd *cobra.Command, args []string) {
 	utils.HandleError(err)
 	err = tmpl.Execute(f, service)
 	utils.HandleError(err)
+
+	fmt.Println(aurora.Green("File README.md generated with success"))
 }
