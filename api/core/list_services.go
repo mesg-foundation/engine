c377e1dc87342fc961475c5fe46c20cc7f5f791c
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-07-17 00:03:25 +0300

    gofmt

--- api/core/list_services.go
+++ api/core/list_services.go
@@ -1,8 +1,8 @@
 package core
 
 import (
-	"github.com/mesg-foundation/core/database/services"
 	"context"
+	"github.com/mesg-foundation/core/database/services"
 )
 
 // ListServices return all services from the database
