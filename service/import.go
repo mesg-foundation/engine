c011f895b757280fc6104e3bbdd7bda7e6f808bf
Nicolas Mahe <nicolas@mahe.me>
2018-07-17 18:57:22 +0700

    Service Validation returns a ValidationResult struct

--- service/import.go
+++ service/import.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"io/ioutil"
 	"path/filepath"
 
@@ -15,21 +16,19 @@ func readFromPath(path string) (data []byte, err error) {
 
 // ImportFromPath returns the service of the given path
 func ImportFromPath(path string) (service *Service, err error) {
-	_, err = ValidService(path)
+	validation, err := ValidateService(path)
 	if err != nil {
 		return
 	}
-
+	if validation.IsValid() == false {
+		err = errors.New("Service is not valid. Run the command 'service validate' for more details")
+		return
+	}
 	data, err := readFromPath(path)
 	if err != nil {
 		return
 	}
-
 	service = &Service{}
 	err = yaml.UnmarshalStrict(data, service)
-	if err != nil {
-		return
-	}
-
 	return
 }
