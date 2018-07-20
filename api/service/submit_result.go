c2a9540e653d973b2df71db493b4335c8d024dbd
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-07-20 17:30:46 +0700

    - better error manager for api service - fix tests

--- api/service/submit_result.go
+++ api/service/submit_result.go
@@ -3,7 +3,6 @@ package service
 import (
 	"context"
 	"encoding/json"
-	"errors"
 
 	"github.com/mesg-foundation/core/execution"
 )
@@ -12,7 +11,9 @@ import (
 func (s *Server) SubmitResult(context context.Context, request *SubmitResultRequest) (*SubmitResultReply, error) {
 	execution := execution.InProgress(request.ExecutionID)
 	if execution == nil {
-		return nil, errors.New("No task in progress with the ID " + request.ExecutionID)
+		return nil, &MissingExecutionError{
+			ID: request.ExecutionID,
+		}
 	}
 	var data map[string]interface{}
 	err := json.Unmarshal([]byte(request.OutputData), &data)
