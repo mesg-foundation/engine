a749e790bee868faa7cef16262a42f7e086d8263
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-07-23 19:43:59 +0700

    add better error when connect to a non running core

--- cmd/utils/error.go
+++ cmd/utils/error.go
@@ -5,12 +5,28 @@ import (
 	"os"
 
 	"github.com/logrusorgru/aurora"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // HandleError display the error and stop the process if error exist
 func HandleError(err error) {
 	if err != nil {
-		fmt.Println(aurora.Red(err))
+		fmt.Println(errorMessage(err))
 		os.Exit(0)
 	}
+}
+
+func errorMessage(err error) string {
+	switch {
+	case coreConnectionError(err):
+		return aurora.Sprintf("%s\nPlease start the core by running: mesg-core start", aurora.Red("Cannot reach the Core."))
+	default:
+		return aurora.Red(err.Error()).String()
+	}
+}
+
+func coreConnectionError(err error) bool {
+	s := status.Convert(err)
+	return s.Code() == codes.Unavailable
 }
