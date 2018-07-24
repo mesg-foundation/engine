75639e1227c99e2e42b4ebcff14a0f66e2f0f6e2
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-07-24 12:05:07 +0700

    add errors test

--- cmd/utils/error.go
+++ cmd/utils/error.go
@@ -10,6 +10,13 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+const (
+	cannotReachTheCore = "Cannot reach the Core."
+	startCore          = "Please start the core by running: mesg-core start"
+	cannotReachDocker  = "Please make sure Docker is running"
+	installDocker      = "If Docker is not installed on your machine you can install it here https://store.docker.com/search?type=edition&offering=community"
+)
+
 // HandleError display the error and stop the process if error exist
 func HandleError(err error) {
 	if err != nil {
@@ -21,15 +28,9 @@ func HandleError(err error) {
 func errorMessage(err error) string {
 	switch {
 	case coreConnectionError(err):
-		return aurora.Sprintf("%s\n%s",
-			aurora.Red("Cannot reach the Core."),
-			"Please start the core by running: mesg-core start",
-		)
+		return aurora.Sprintf("%s\n%s", aurora.Red(cannotReachTheCore), startCore)
 	case dockerDaemonError(err):
-		return aurora.Sprintf("%s\n%s",
-			aurora.Red("Please make sure Docker is running"),
-			"If Docker is not installed on your machine you can install it here https://store.docker.com/search?type=edition&offering=community",
-		)
+		return aurora.Sprintf("%s\n%s", aurora.Red(cannotReachDocker), installDocker)
 	default:
 		return aurora.Red(err.Error()).String()
 	}
