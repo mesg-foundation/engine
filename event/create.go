d3d4d1fff5b079b5cc8681534f4c3e6ad2019ee6
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-07-25 15:43:41 +0700

    add event errors and add some tests

--- event/create.go
+++ event/create.go
@@ -1,7 +1,6 @@
 package event
 
 import (
-	"errors"
 	"time"
 
 	"github.com/mesg-foundation/core/service"
@@ -9,16 +8,18 @@ import (
 
 // Create an event
 func Create(serviceForEvent *service.Service, eventKey string, data map[string]interface{}) (*Event, error) {
-	serviceEvent, evenFound := serviceForEvent.Events[eventKey]
-	if !evenFound {
-		return nil, errors.New("Event " + eventKey + " doesn't exists in service " + serviceForEvent.Name)
+	serviceEvent, eventFound := serviceForEvent.Events[eventKey]
+	if !eventFound {
+		return nil, &service.EventNotFoundError{
+			Service:  serviceForEvent,
+			EventKey: eventKey,
+		}
 	}
 	if !serviceEvent.IsValid(data) {
-		errorString := "Invalid parameters: "
-		for _, warning := range serviceEvent.Validate(data) {
-			errorString = errorString + " " + warning.String()
+		return nil, &service.InvalidEventDataError{
+			Event: serviceEvent,
+			Data:  data,
 		}
-		return nil, errors.New(errorString)
 	}
 	return &Event{
 		Service:   serviceForEvent,
---
+++ service/errors.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"strings"
+)
+
+// EventNotFoundError is an error when we cannot find an event in a service
+type EventNotFoundError struct {
+	Service  *Service
+	EventKey string
+}
+
+func (e *EventNotFoundError) Error() string {
+	return strings.Join([]string{
+		"Event",
+		e.EventKey,
+		"not found in service",
+		e.Service.Name,
+	}, " ")
+}
+
+// InvalidEventDataError is an error when the data of an event are not valid
+type InvalidEventDataError struct {
+	Event *Event
+	Data  map[string]interface{}
+}
+
+func (e *InvalidEventDataError) Error() string {
+	errorString := "Invalid event parameters: "
+	for _, warning := range e.Event.Validate(e.Data) {
+		errorString = errorString + " " + warning.String()
+	}
+	return errorString
+}
