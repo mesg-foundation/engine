fe3144cd4d1eea7c151b356b28d0f87356507947
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-07-28 00:14:56 +0700

    code uality on the tutorials

--- docs/tutorials/services/send-email-with-sendgrid/index.js
+++ docs/tutorials/services/send-email-with-sendgrid/index.js
@@ -1,11 +1,11 @@
-const MESG = require('mesg-js').service()
-const sendgrid = require('@sendgrid/mail')
+const MESG = require("mesg-js").service();
+const sendgrid = require("@sendgrid/mail");
 
 const send = ({ from, to, subject, text }, { success, failure }) => {
-  sendgrid.setApiKey('__CHANGE_WITH_YOUR_SENDGRID_API_KEY__')
+  sendgrid.setApiKey("__CHANGE_WITH_YOUR_SENDGRID_API_KEY__");
   sendgrid.send({ from, to, subject, text })
     .then(([response, _]) => success({ status: response.statusCode }))
-    .catch(e => failure({ message: e.toString() }))
+    .catch((e) => failure({ message: e.toString() }));
 }
 
-MESG.listenTask({ send })
+MESG.listenTask({ send });
