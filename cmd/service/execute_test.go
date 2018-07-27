3372bd4dee3023da6faa2a868e5a21d1c991c5df
Nicolas Mahe <nicolas@mahe.me>
2018-07-27 17:43:49 +0700

    fix tests

--- cmd/service/execute_test.go
+++ cmd/service/execute_test.go
@@ -3,18 +3,28 @@ package service
 import (
 	"testing"
 
+	"github.com/mesg-foundation/core/service"
 	"github.com/stvp/assert"
 )
 
-func TestGetJSON(t *testing.T) {
-	d, e := getJSON("")
+func TestReadJSONFile(t *testing.T) {
+	d, e := readJSONFile("")
 	assert.Nil(t, e)
 	assert.Equal(t, "{}", d)
 
-	d, e = getJSON("./doesntexistsfile")
+	d, e = readJSONFile("./doesntexistsfile")
 	assert.NotNil(t, e)
 
-	d, e = getJSON("./tests/validData.json")
+	d, e = readJSONFile("./tests/validData.json")
 	assert.Nil(t, e)
 	assert.Equal(t, "{\"foo\":\"bar\"}", d)
+}
+
+func TestTaskKeysFromService(t *testing.T) {
+	keys := taskKeysFromService(&service.Service{
+		Tasks: map[string]*service.Task{
+			"task1": &service.Task{},
+		},
+	})
+	assert.Equal(t, "task1", keys[0])
 }
