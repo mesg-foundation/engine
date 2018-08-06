bf4ba0d6c18a072b5a7ea3f75116efa132490996
Nicolas Mahe <nicolas@mahe.me>
2018-08-06 14:34:26 +0700

    Remove test on taskKey (it will fail in the next test) and return last error directly

--- api/core/listen_result.go
+++ api/core/listen_result.go
@@ -55,20 +55,15 @@ func validateOutputKey(service *service.Service, taskKey string, outputFilter st
 	if outputFilter == "" || outputFilter == "*" {
 		return nil
 	}
-	if taskKey == "" {
-		return fmt.Errorf("Cannot filter output without specifying a task")
-	}
 	task, ok := service.Tasks[taskKey]
 	if !ok {
 		return fmt.Errorf("Task %q doesn't exist in this service", taskKey)
 	}
-
-	var err error
 	_, ok = task.Outputs[outputFilter]
 	if !ok {
-		err = fmt.Errorf("Output %q doesn't exist in the task %q of this service", outputFilter, taskKey)
+		return fmt.Errorf("Output %q doesn't exist in the task %q of this service", outputFilter, taskKey)
 	}
-	return err
+	return nil
 }
 
 func isSubscribedTask(request *ListenResultRequest, e *execution.Execution) bool {
