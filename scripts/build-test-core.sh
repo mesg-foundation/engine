4e0ed2f13ced046fd2bea46be78eebcbab7c3510
Nicolas Mahe <nicolas@mahe.me>
2018-08-16 15:19:59 +0700

    Change version of the local dev build of core

--- scripts/build-test-core.sh
+++ scripts/build-test-core.sh
@@ -2,5 +2,5 @@
 
 echo "Building mesg/core:local..."
 docker pull mesg/core:latest
-docker build -t mesg/core:build --target build --build-arg version="dev local" .
-docker build -t mesg/core:local --build-arg version="dev local" .
+docker build -t mesg/core:build --target build --build-arg version="local" .
+docker build -t mesg/core:local --build-arg version="local" .
