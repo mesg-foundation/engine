a0afcbf61017fad3272d1c01f817657473981dbb
Nicolas Mahe <nicolas@mahe.me>
2018-08-17 11:41:46 +0700

    Remove unnecessary docker build in local core building

--- scripts/build-test-core.sh
+++ scripts/build-test-core.sh
@@ -2,5 +2,4 @@
 
 echo "Building mesg/core:local..."
 docker pull mesg/core:latest
-docker build -t mesg/core:build --target build --build-arg version="local" .
 docker build -t mesg/core:local --build-arg version="local" .
