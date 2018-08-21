1f44ac3105fb8a753f6b9608113119689672332e
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-08-21 15:11:19 +0300

    improve namings

--- task.go
+++ task.go
@@ -2,13 +2,13 @@ package mesg
 
 // Taskable represents implementation of task executor.
 type Taskable interface {
-	// Name returns the task key.
-	Name() (key string)
+	// Key returns the task key.
+	Key() string
 
 	// Execute called when a task execution request arrived with the same key
 	// returned by Name().
-	// key is the output key and data is the output data of task.
-	Execute(execution *Execution) (key string, data Data)
+	// outputKey is the output key and data is the output data of task.
+	Execute(execution *Execution) (outputKey string, outputData Data)
 }
 
 // defaultTask implements Task.
@@ -17,16 +17,16 @@ type defaultTask struct {
 	handler func(execution *Execution) (key string, data Data)
 }
 
-func (t *defaultTask) Name() string {
+func (t *defaultTask) Key() string {
 	return t.name
 }
 
-func (t *defaultTask) Execute(execution *Execution) (key string, data Data) {
+func (t *defaultTask) Execute(execution *Execution) (outputKey string, outputData Data) {
 	return t.handler(execution)
 }
 
-// Task creates an executable task for given task name, handler called when a
+// Task creates an executable task for given task key, handler called when a
 // matching task execution request arrived.
-func Task(name string, handler func(*Execution) (key string, data Data)) Taskable {
-	return &defaultTask{name, handler}
+func Task(key string, handler func(*Execution) (key string, data Data)) Taskable {
+	return &defaultTask{key, handler}
 }
