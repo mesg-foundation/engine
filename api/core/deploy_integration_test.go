d2e50c8af7d5ade92cf712e410f9a4d3d202aa43
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-08-24 09:20:28 +0300

    fix dev-core script and integration test in api/core

--- api/core/deploy_integration_test.go
+++ api/core/deploy_integration_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/cnf/structhash"
 	"github.com/logrusorgru/aurora"
 	"github.com/mesg-foundation/core/database/services"
+	"github.com/mesg-foundation/core/mesg"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,7 +20,11 @@ func TestIntegrationDeployService(t *testing.T) {
 	stream := newTestDeployStream(url)
 
 	require.Nil(t, server.DeployService(stream))
+	defer services.Delete(stream.serviceID)
+
 	require.Equal(t, 1, structhash.Version(stream.serviceID))
-	require.True(t, stringSliceContains(stream.statuses, fmt.Sprintf("%s Completed.", aurora.Green("✔"))))
-	services.Delete(stream.serviceID)
+	require.Contains(t, stream.statuses, mesg.DeployStatus{
+		Message: fmt.Sprintf("%s Completed.", aurora.Green("✔")),
+		Type:    mesg.DONE,
+	})
 }
