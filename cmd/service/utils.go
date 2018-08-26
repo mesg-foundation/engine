8711a71037209288d39287805c7653f7bb043da9
krhubert <krhubert@gmail.com>
2018-08-26 10:57:40 +0200

    Fix

--- cmd/service/utils.go
+++ cmd/service/utils.go
@@ -2,33 +2,16 @@ package service
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 
-	"github.com/asaskevich/govalidator"
 	"github.com/logrusorgru/aurora"
 	"github.com/mesg-foundation/core/api/core"
 	"github.com/mesg-foundation/core/cmd/utils"
 	"github.com/mesg-foundation/core/config"
-	"github.com/mesg-foundation/core/container"
 	"github.com/mesg-foundation/core/service/importer"
-	"github.com/mesg-foundation/core/x/xgit"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 )
-
-// TODO(ilgooz): remove this after service package made Newable.
-var defaultContainer *container.Container
-
-// TODO(ilgooz): remove this after service package made Newable.
-func init() {
-	c, err := container.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defaultContainer = c
-}
 
 func cli() core.CoreClient {
 	connection, err := grpc.Dial(viper.GetString(config.APIClientTarget), grpc.WithInsecure())
@@ -50,28 +33,4 @@ func handleValidationError(err error) {
 		fmt.Println("Run the command 'service validate' for more details")
 		os.Exit(0)
 	}
-}
-
-func downloadServiceIfNeeded(path string) (newPath string, didDownload bool, err error) {
-	if !govalidator.IsURL(path) {
-		return path, false, nil
-	}
-	newPath, err = ioutil.TempDir("", utils.TempDirPrefix)
-	if err != nil {
-		return "", false, err
-	}
-	utils.ShowSpinnerForFunc(utils.SpinnerOptions{Text: "Downloading service..."}, func() {
-		err = xgit.Clone(path, newPath)
-	})
-	if err != nil {
-		return "", false, err
-	}
-	return newPath, true, nil
-}
-
-func buildDockerImage(path string) (imageHash string, err error) {
-	utils.ShowSpinnerForFunc(utils.SpinnerOptions{Text: "Building image..."}, func() {
-		imageHash, err = defaultContainer.Build(path)
-	})
-	return
 }
