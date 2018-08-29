042abb695b61a3da6934b0ea3574f05bd15ca279
krhubert <krhubert@gmail.com>
2018-08-29 19:29:38 +0200

    Fix container status info

--- daemon/status.go
+++ daemon/status.go
@@ -6,5 +6,19 @@ import (
 
 // Status returns the Status of the docker service of the daemon.
 func Status() (container.StatusType, error) {
-	return defaultContainer.ServiceStatus(Namespace()) //TODO: should it be containerStatus?
+	//TODO: should it be containerStatus?
+	serviceStatus, err := defaultContainer.ServiceStatus(Namespace())
+	if err != nil {
+		return container.STOPPED, err
+	}
+
+	containerStatus, err := defaultContainer.Status(Namespace())
+	if err != nil {
+		return container.STOPPED, err
+	}
+
+	if serviceStatus == containerStatus {
+		return serviceStatus, nil
+	}
+	return container.STOPPED, nil
 }
