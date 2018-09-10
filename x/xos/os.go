7a41f3c8ad51e568c495ecbb7586f58507d67dfb
krhubert <krhubert@gmail.com>
2018-09-10 16:17:22 +0700

    Create CopyDir

--- x/xos/os.go
+++ x/xos/os.go
@@ -2,7 +2,9 @@ package xos
 
 import (
 	"io"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 // Touch creates a new file, truncating it if it already exists.
@@ -27,6 +29,10 @@ func Copy(src, dest string) error {
 		return err
 	}
 
+	if si.IsDir() {
+		return CopyDir(src, dest)
+	}
+
 	// Handle symbolic link.
 	if si.Mode()&os.ModeSymlink != 0 {
 		return CopySymlink(src, dest)
@@ -42,6 +48,32 @@ func Copy(src, dest string) error {
 	}
 
 	return os.Chmod(dest, si.Mode())
+}
+
+// CopyDir copies direcotry from source to target path.
+func CopyDir(src, dest string) error {
+	si, err := os.Lstat(src)
+	if err != nil {
+		return err
+	}
+
+	entries, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(dest, si.Mode()); err != nil {
+		return err
+	}
+
+	for _, e := range entries {
+		newsrc := filepath.Join(src, e.Name())
+		newdst := filepath.Join(dest, e.Name())
+		if err := Copy(newsrc, newdst); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 // CopyFile copies file from source to target path.
