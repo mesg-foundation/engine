ac281fac67803acaff51a5a33f5a2de22e92219a
krhubert <krhubert@gmail.com>
2018-09-11 10:28:05 +0700

    Replace cmd with commands

--- interface/cli/main.go
+++ interface/cli/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/mesg-foundation/core/cmd"
-	"github.com/mesg-foundation/core/cmd/utils"
+	"github.com/mesg-foundation/core/commands"
+	"github.com/mesg-foundation/core/commands/provider"
+	"github.com/mesg-foundation/core/config"
+	"github.com/mesg-foundation/core/interface/grpc/core"
+	"github.com/mesg-foundation/core/utils/clierrors"
 	"github.com/mesg-foundation/core/version"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 func init() {
@@ -12,6 +21,15 @@ func init() {
 }
 
 func main() {
-	err := cmd.RootCmd.Execute()
-	utils.HandleError(err)
+	connection, err := grpc.Dial(viper.GetString(config.APIClientTarget), grpc.WithInsecure())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	p := provider.New(core.NewCoreClient(connection))
+	if err := commands.Build(p).Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, clierrors.ErrorMessage(err))
+		os.Exit(1)
+	}
 }
