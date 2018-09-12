5bec78a41ce9cf14a523acb94f020b90817dd1b2
Nicolas Mahe <nicolas@mahe.me>
2018-09-12 19:15:31 +0700

    Return error directly when service is not found

--- interface/grpc/core/get_service.go
+++ interface/grpc/core/get_service.go
@@ -7,5 +7,8 @@ import (
 // GetService returns service serviceID.
 func (s *Server) GetService(ctx context.Context, request *GetServiceRequest) (*GetServiceReply, error) {
 	srv, err := s.api.GetService(request.ServiceID)
-	return &GetServiceReply{Service: toProtoService(srv)}, err
+	if err != nil {
+		return nil, err
+	}
+	return &GetServiceReply{Service: toProtoService(srv)}, nil
 }
