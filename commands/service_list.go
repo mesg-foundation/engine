8f403cbc706dff223b69ff67074de2c6dc3b7db1
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-09-12 12:53:17 +0700

    fix issue with tabulation on lists

--- commands/service_list.go
+++ commands/service_list.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"text/tabwriter"
 
-	"github.com/mesg-foundation/core/utils/pretty"
 	"github.com/spf13/cobra"
 )
 
@@ -38,7 +37,7 @@ func (c *serviceListCmd) runE(cmd *cobra.Command, args []string) error {
 	services := toServices(coreServices)
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 4, ' ', 0)
-	fmt.Fprintf(w, pretty.Bold("STATUS\tSERVICE\tNAME\n"))
+	fmt.Fprintf(w, "STATUS\tSERVICE\tNAME\n")
 	for _, s := range services {
 		status, err := s.Status()
 		if err != nil {
