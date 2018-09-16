d4dd9a544375764175698a9029a1bc11174cef0c
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-09-16 21:45:28 +0700

    fix services command

---
+++ commands/root_test.go
@@ -0,0 +1,27 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+)
+
+func findCommandChildByUse(root *cobra.Command, use string) bool {
+	for _, cmd := range root.Commands() {
+		if cmd.Use == use {
+			return true
+		}
+	}
+	return false
+}
+func TestRootCmd(t *testing.T) {
+	cmd := Build(nil)
+	for _, tt := range []struct {
+		use string
+	}{
+		{"service"},
+	} {
+		require.Truef(t, findCommandChildByUse(cmd, tt.use), "command %q not found", tt.use)
+	}
+}
