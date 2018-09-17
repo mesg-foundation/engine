ae2b5bcf97c8bd97c36ffa3d5c64dc2347eaa1fe
Nicolas Mahe <nicolas@mahe.me>
2018-09-17 18:37:16 +0700

    Move protobuf files to /protobuf. Merge Service protobuf definition to core/api protobuf definition.

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -5,12 +5,10 @@ cd $GOPATH/src
 go get github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
 
 PROJECT=github.com/mesg-foundation/core
-GRPC=$PROJECT/interface/grpc
+GRPC=$PROJECT/protobuf
 CORE=$(pwd)/$PROJECT
 API_DOCS="--doc_out=$CORE/docs/api/ --doc_opt=$CORE/docs/api.template"
-DATA_DOCS="--doc_out=$CORE/docs/api/ --doc_opt=$CORE/docs/data.template"
 GRPC_PLUGIN="--go_out=plugins=grpc:./"
 
-protoc $GRPC_PLUGIN $DATA_DOCS,service-type.md --proto_path=./ $GRPC/core/service.proto
 protoc $GRPC_PLUGIN $API_DOCS,core.md          --proto_path=./ $GRPC/core/api.proto
 protoc $GRPC_PLUGIN $API_DOCS,service.md       --proto_path=./ $GRPC/service/api.proto
