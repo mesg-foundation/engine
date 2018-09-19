ea45586aac35b6d182cd721a3cc37a8764b56b3e
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-09-19 19:16:10 +0700

    update database path

--- database/services/db.go
+++ database/services/db.go
@@ -20,7 +20,7 @@ func open() (db *leveldb.DB, err error) {
 		if err != nil {
 			return nil, err
 		}
-		storagePath := filepath.Join(c.Core.Path, "database", "services")
+		storagePath := filepath.Join(c.CorePath(), "database", "services")
 		_instance, err = leveldb.OpenFile(storagePath, nil)
 		if err != nil {
 			return nil, err
