4ff65e0d92d7e72aac15587de19f006540129992
Nicolas Mahe <nicolas@mahe.me>
2018-09-19 16:57:41 +0700

    Warn decodeError in function database.service.all instead of returning it to not break the app when decoding deprecated service

--- database/services/all.go
+++ database/services/all.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/mesg-foundation/core/service"
+	"github.com/sirupsen/logrus"
 )
 
 // All returns all deployed services.
@@ -14,11 +15,17 @@ func All() ([]*service.Service, error) {
 	var services []*service.Service
 	iter := db.NewIterator(nil, nil)
 	for iter.Next() {
-			return nil, err
 		service, err := decode(string(iter.Key()), iter.Value())
 		if err != nil {
+			// Ignore all decode errors (possibly due to a service structure change or database corruption)
+			if decodeErr, ok := err.(*DecodeError); ok {
+				logrus.WithField("service", decodeErr.Hash).Warning(decodeErr.Error())
+			} else {
+				return nil, err
+			}
+		} else {
+			services = append(services, service)
 		}
-		services = append(services, &service)
 	}
 	iter.Release()
 	return services, iter.Error()
