7666fa37b80144a217be260700e7e23c60ea6cfb
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-09-24 08:37:38 +0300

    service/importer: support numbers in names, closes #396

--- service/importer/validation_test.go
+++ service/importer/validation_test.go
@@ -10,51 +10,59 @@ import (
 
 func TestValidate(t *testing.T) {
 	validation, err := Validate("./tests/service-valid")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.True(t, validation.IsValid())
 	require.True(t, validation.ServiceFileExist)
-	require.Equal(t, 0, len(validation.ServiceFileWarnings))
+	require.Len(t, validation.ServiceFileWarnings, 0)
 	require.True(t, validation.DockerfileExist)
+}
+
+func TestValidateYMLNames(t *testing.T) {
+	validation, err := Validate("./tests/service-names-valid")
+	require.NoError(t, err)
+	require.True(t, validation.IsValid())
+	require.True(t, validation.ServiceFileExist)
+	require.Len(t, validation.ServiceFileWarnings, 0)
 }
 
 func TestValidateDockerfileIsMissing(t *testing.T) {
 	validation, err := Validate("./tests/service-docker-missing")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.False(t, validation.IsValid())
 	require.True(t, validation.ServiceFileExist)
-	require.Equal(t, 0, len(validation.ServiceFileWarnings))
+	require.Len(t, validation.ServiceFileWarnings, 0)
 	require.False(t, validation.DockerfileExist)
 }
 
 func TestValidateFromMissingServiceFile(t *testing.T) {
 	validation, err := Validate("./tests/service-file-missing")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.False(t, validation.IsValid())
 	require.False(t, validation.ServiceFileExist)
-	require.Equal(t, 1, len(validation.ServiceFileWarnings))
+	require.Len(t, validation.ServiceFileWarnings, 1)
 	require.True(t, validation.DockerfileExist)
 }
 
 func TestValidateFromNonExistingPath(t *testing.T) {
 	validation, err := Validate("./tests/service-non-existing")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.False(t, validation.IsValid())
 	require.False(t, validation.ServiceFileExist)
-	require.Equal(t, 1, len(validation.ServiceFileWarnings))
+	require.Len(t, validation.ServiceFileWarnings, 1)
 	require.False(t, validation.DockerfileExist)
 }
 
 func TestValidateFromMalFormattedServiceFile(t *testing.T) {
 	_, err := Validate("./tests/service-file-mal-formatted")
-	require.NotNil(t, err)
+	require.Error(t, err)
 }
 
 func TestValidateFromInvalidServiceFile(t *testing.T) {
 	validation, err := Validate("./tests/service-file-invalid")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.False(t, validation.IsValid())
 	require.True(t, validation.ServiceFileExist)
-	require.Equal(t, 1, len(validation.ServiceFileWarnings))
+	require.Len(t, validation.ServiceFileWarnings, 1)
 	require.True(t, validation.DockerfileExist)
 }
 
@@ -62,11 +70,11 @@ func TestValidateFromInvalidServiceFile(t *testing.T) {
 
 func TestIsValid(t *testing.T) {
 	isValid, err := IsValid("./tests/service-valid")
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.True(t, isValid)
 }
 
 func TestIsValidMalFormattedServiceFile(t *testing.T) {
 	_, err := IsValid("./tests/service-file-mal-formatted")
-	require.NotNil(t, err)
+	require.Error(t, err)
 }
