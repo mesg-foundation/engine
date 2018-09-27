5e6e4d54180b20160e16cfca729a9bc851d79c90
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-09-27 17:01:25 +0300

    commands: add more comments to tests

--- commands/commands_test.go
+++ commands/commands_test.go
@@ -9,13 +9,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// outputStream is a testing utility to capture io writes and read
+// written data with different ways.
 type outputStream struct {
 	t *testing.T
 	w io.Writer
 	r *bufio.Reader
 }
 
-func newoutputStream(t *testing.T) *outputStream {
+// newOutputStream receives t to assert errors generated during
+// the writes and reads.
+func newOutputStream(t *testing.T) *outputStream {
 	r, w := io.Pipe()
 	return &outputStream{
 		t: t,
@@ -24,16 +28,19 @@ func newoutputStream(t *testing.T) *outputStream {
 	}
 }
 
+// Write implements io.Writer.
 func (o *outputStream) Write(b []byte) (n int, err error) {
 	return o.w.Write(b)
 }
 
+// ReadLine returns the next line in the stream.
 func (o *outputStream) ReadLine() []byte {
 	line, _, err := o.r.ReadLine()
 	require.NoError(o.t, err)
 	return line
 }
 
+// ReadAll returns all the data in thee stream until it's closed.
 func (o *outputStream) ReadAll() []byte {
 	bytes, err := ioutil.ReadAll(o.r)
 	require.NoError(o.t, err)
