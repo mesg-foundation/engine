cc3bae2f398f2340f45895318736571d43133ea1
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-10-10 16:22:55 +0300

    systemservices: split system services and deploy funcs to their own files

---
+++ systemservices/service_resolver.go
@@ -0,0 +1,8 @@
+package systemservices
+
+import "github.com/mesg-foundation/core/systemservices/resolver"
+
+// Resolver returns the Resolver instance using the running Resolver service.
+func (s *SystemServices) Resolver() (*resolver.Resolver, error) {
+	return nil, nil
+}
