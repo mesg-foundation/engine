49c2258524446dd1a7bc408f4858d07f3b37af28
krhubert <krhubert@gmail.com>
2018-10-10 18:10:44 +0200

    Implement resolver

--- systemservices/resolver/resolver.go
+++ systemservices/resolver/resolver.go
@@ -1,15 +1,71 @@
 package resolver
 
-import "github.com/mesg-foundation/core/api"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/mesg-foundation/core/api"
+)
+
+// Resolvers tasks.
+const (
+	addPeersTask string = "AddPeers"
+	resolveTask         = "Resolve"
+)
 
 // Resolver is the system service responsible for getting the address of
 // other node of the network that are running the desired service.
-type Resolver struct{}
+type Resolver struct {
+	api       *api.API
+	serviceID string
+}
 
 // New creates a new instance of the Resolver system service.
-// It communicates with the service by only using the API package.
-// serviceID of the associated MESG Service.
-// api is an instance to the api package required to communication with the service.
-func New(serviceID string, api *api.API) (*Resolver, error) {
-	return nil, nil
+func New(serviceID string, api *api.API) *Resolver {
+	return &Resolver{
+		api:       api,
+		serviceID: serviceID,
+	}
+}
+
+// AddPeers is the task that actually new add peers to the service.
+func (r *Resolver) AddPeers(addresses []string) error {
+	e, err := r.api.ExecuteAndListen(r.serviceID, addPeersTask, map[string]interface{}{
+		"addresses": addresses,
+	})
+	if err != nil {
+		return err
+	}
+
+	switch e.Output {
+	case "success":
+		return nil
+	case "error":
+		return errors.New(e.OutputData["error"].(string))
+	default:
+		return fmt.Errorf("unexpected output %s", e.Output)
+	}
+}
+
+// ResolveFoundOutput is the found output data of resolve task.
+type ResolveFoundOutput struct {
+	Address string `json:"address"` // Address is the IP address of core peer.
+}
+
+// Resolve is the task that return the address of a core that runs the desired service.
+func (r *Resolver) Resolve(serviceID string) (address string, err error) {
+	e, err := r.api.ExecuteAndListen(r.serviceID, resolveTask, map[string]interface{}{
+		"serviceID": serviceID,
+	})
+
+	switch e.Output {
+	case "found":
+		return e.OutputData["found"].(*ResolveFoundOutput).Address, nil
+	case "notFound":
+		return "", fmt.Errorf("address for service id %s not found", serviceID)
+	case "error":
+		return "", errors.New(e.OutputData["error"].(string))
+	default:
+		return "", fmt.Errorf("unexpected output %s", e.Output)
+	}
 }
