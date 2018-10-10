68066d8482b7ca7c045815d25f1543688f787798
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-10-10 17:21:43 +0300

    systemservices: add functionality to init system services & init resolver service

--- systemservices/service_resolver.go
+++ systemservices/service_resolver.go
@@ -3,6 +3,6 @@ package systemservices
 import "github.com/mesg-foundation/core/systemservices/resolver"
 
 // Resolver returns the Resolver instance using the running Resolver service.
-func (s *SystemServices) Resolver() (*resolver.Resolver, error) {
-	return nil, nil
+func (s *SystemServices) Resolver() *resolver.Resolver {
+	return s.resolverService
 }
