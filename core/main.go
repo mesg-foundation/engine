417343e2459b3bc5dd0e6b2040c575e3f633db50
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-10-11 19:02:47 +0300

    systemservices/resolver: fix task executions
    * core: add temporary lines to manually test systemservices pkg

--- core/main.go
+++ core/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"path/filepath"
 
+	"github.com/mesg-foundation/core/api"
 	"github.com/mesg-foundation/core/config"
 	"github.com/mesg-foundation/core/database"
 	"github.com/mesg-foundation/core/interface/grpc"
 	"github.com/mesg-foundation/core/logger"
+	"github.com/mesg-foundation/core/systemservices"
 	"github.com/mesg-foundation/core/version"
 	"github.com/mesg-foundation/core/x/xsignal"
 	"github.com/sirupsen/logrus"
@@ -37,6 +39,46 @@ func main() {
 		if err := tcpServer.Serve(); err != nil {
 			logrus.Fatalln(err)
 		}
+	}()
+
+	// TODO: rm this goroutine.
+	// we have it here temporarily to test systemservices pkg.
+	go func() {
+		logrus.Println("service resolver: testing...")
+
+		systemServicesPath := filepath.Join(c.Core.Path, c.SystemServices.RelativePath)
+
+		a, err := api.New(db)
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+		s, err := systemservices.New(a, systemServicesPath)
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+
+		ss, validationErr, err := a.DeployServiceFromURL("https://github.com/mesg-foundation/service-webhook")
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+		if validationErr != nil {
+			logrus.Fatalln(err)
+		}
+
+		peers := []string{"core:50052"}
+		logrus.Println("service resolver: webhook service started on peer:", peers[0])
+
+		if err := s.Resolver().AddPeers(peers); err != nil {
+			logrus.Fatalln(err)
+		}
+		logrus.Println("service resolver: webhook service's peer registered to resolver as:", peers[0])
+
+		address, err := s.Resolver().Resolve(ss.ID)
+		if err != nil {
+			logrus.Fatalln(err)
+		}
+		logrus.Println("service resolver: peer address resolved for webhook service as:", address)
+		logrus.Println("service resolver: works great!")
 	}()
 
 	<-xsignal.WaitForInterrupt()
