c61d4f628ba767bbf99ffc5aad817c336657d519
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-10-11 11:01:40 +0300

    systemservices: move system services' access methods back to systemservices.go, improve docs

--- systemservices/service_resolver.go
+++
@@ -1,8 +0,0 @@
-package systemservices
-
-import "github.com/mesg-foundation/core/systemservices/resolver"
-
-// Resolver returns the Resolver instance using the running Resolver service.
-func (s *SystemServices) Resolver() *resolver.Resolver {
-	return s.resolverService
-}
