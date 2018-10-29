7fd8e277330e1e06cfd9643c003f56d29df24951
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-10-29 14:28:42 +0100

    add more context to the leveldb name

--- execution/leveldbexecutiondb.go
+++ execution/leveldbexecutiondb.go
@@ -12,24 +12,24 @@ import (
 )
 
 // LevelDB is a concrete implementation of the DB interface
-type LevelDB struct {
+type LevelDBExecutionDB struct {
 	db *leveldb.DB
 }
 
 // New creates a new DB instance
-func New(path string) (*LevelDB, error) {
+func New(path string) (*LevelDBExecutionDB, error) {
 	db, err := leveldb.OpenFile(path, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	return &LevelDB{db: db}, nil
+	return &LevelDBExecutionDB{db: db}, nil
 }
 
 // Create a record in the database to store this execution and returns the id
 // returns an error if any problem happen with the database
 // returns an error if inputs are invalid
-func (db *LevelDB) Create(service *service.Service, taskKey string, inputs map[string]interface{}, tags []string) (*Execution, error) {
+func (db *LevelDBExecutionDB) Create(service *service.Service, taskKey string, inputs map[string]interface{}, tags []string) (*Execution, error) {
 	task, err := service.GetTask(taskKey)
 	if err != nil {
 		return nil, err
@@ -48,7 +48,7 @@ func (db *LevelDB) Create(service *service.Service, taskKey string, inputs map[s
 }
 
 // Find the execution based on an executionID, returns an error if not found
-func (db *LevelDB) Find(executionID string) (*Execution, error) {
+func (db *LevelDBExecutionDB) Find(executionID string) (*Execution, error) {
 	data, err := db.db.Get([]byte(executionID), nil)
 	if err != nil {
 		return nil, err
@@ -61,7 +61,7 @@ func (db *LevelDB) Find(executionID string) (*Execution, error) {
 // Execute a given execution
 // Returns an error if the execution doesn't exists in the database
 // Returns an error if the status of the execution is different of `Created`
-func (db *LevelDB) Execute(executionID string) (*Execution, error) {
+func (db *LevelDBExecutionDB) Execute(executionID string) (*Execution, error) {
 	e, err := db.Find(executionID)
 	if err != nil {
 		return nil, err
@@ -81,7 +81,7 @@ func (db *LevelDB) Execute(executionID string) (*Execution, error) {
 // Returns an error if the executionID doesn't exists
 // Returns an error if the execution is not `InProgress`
 // Returns an error if the `outputKey` or `outputData` are not valid
-func (db *LevelDB) Complete(executionID, outputKey string, outputData map[string]interface{}) (*Execution, error) {
+func (db *LevelDBExecutionDB) Complete(executionID, outputKey string, outputData map[string]interface{}) (*Execution, error) {
 	e, err := db.Find(executionID)
 	if err != nil {
 		return nil, err
@@ -113,11 +113,11 @@ func (db *LevelDB) Complete(executionID, outputKey string, outputData map[string
 }
 
 // Close closes database.
-func (db *LevelDB) Close() error {
+func (db *LevelDBExecutionDB) Close() error {
 	return db.db.Close()
 }
 
-func (db *LevelDB) save(execution *Execution) (*Execution, error) {
+func (db *LevelDBExecutionDB) save(execution *Execution) (*Execution, error) {
 	id := fmt.Sprintf("%x", sha1.Sum(structhash.Dump(execution, 1)))
 	execution.ID = string(id)
 	data, err := json.Marshal(execution)
