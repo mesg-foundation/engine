2d99764db92a8ea489dd1d1ea674f004d5a6ac21
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-10-31 20:09:45 +0300

    scripts: correctly print script names in require-mesg-dev regardless of what paths scripts are executed from

--- scripts/require-mesg-dev.sh
+++ scripts/require-mesg-dev.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# generate relative path of script from core's root.
+# scripts can be called from any path including the scripts dir so,
+# we need to remove parent paths to get the naked script name.
+SCRIPT_PATH=$0
+SCRIPT_PATH=${SCRIPT_PATH#*scripts/}
+SCRIPT_PATH=./scripts/${SCRIPT_PATH#./}
+
 if [ ! "$MESG_DEV" = "true" ]; then
    echo "you must run scripts via ./mesg-dev script,";
    echo "try executing the following command from the root of core:";
-   echo -e "\t./mesg-dev $0 $*"
+   echo -e "\t./mesg-dev $SCRIPT_PATH $*"
    exit 1;
 fi
