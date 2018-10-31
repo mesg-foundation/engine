d552e05c6dca448c10c9ae2831f2cf742c191a37
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-10-31 19:24:53 +0300

    scripts: cosmetic updates

--- scripts/require-mesg-dev.sh
+++ scripts/require-mesg-dev.sh
@@ -1,5 +1,6 @@
 if [ ! "$MESG_DEV" = "true" ]; then
    echo "you must run scripts via ./mesg-dev script,";
-   echo "try executing this command from the root of core: ./mesg-dev $0 $*";
+   echo "try executing the following command from the root of core:";
+   echo -e "\t./mesg-dev $0 $*"
    exit 1;
 fi
