f9bde19737c8d5475d42cdbf19f29b745c8d1d6e
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-11-09 12:26:26 +0300

    wss feature: accept workflow definition object instead yaml for creating workflows

--- commands/provider/workflow_provider.go
+++ commands/provider/workflow_provider.go
@@ -2,10 +2,9 @@ package provider
 
 import (
 	"context"
-	"io/ioutil"
-	"os"
 
 	"github.com/mesg-foundation/core/protobuf/coreapi"
+	"github.com/mesg-foundation/core/utils/workflowparser/yaml"
 )
 
 // WorkflowProvider is a struct that provides all methods required by workflow command.
@@ -21,20 +20,13 @@ func NewWorkflowProvider(c coreapi.CoreClient) *WorkflowProvider {
 // CreateWorkflow creates and runs a new workflow with given yaml file and
 // optionally given unique name.
 func (p *WorkflowProvider) CreateWorkflow(filePath string, name string) (id string, err error) {
-	file, err := os.Open(filePath)
+	definition, err := yaml.ParseFromFile(filePath)
 	if err != nil {
 		return "", err
 	}
-	defer file.Close()
-
-	fileData, err := ioutil.ReadAll(file)
-	if err != nil {
-		return "", err
-	}
-
 	reply, err := p.client.CreateWorkflow(context.Background(), &coreapi.CreateWorkflowRequest{
-		YAML: fileData,
-		Name: name,
+		Definition: p.toProtoWorkflowDefinition(definition),
+		Name:       name,
 	})
 	if err != nil {
 		return "", err
@@ -46,4 +38,8 @@ func (p *WorkflowProvider) CreateWorkflow(filePath string, name string) (id stri
 func (p *WorkflowProvider) DeleteWorkflow(id string) error {
 	_, err := p.client.DeleteWorkflow(context.Background(), &coreapi.DeleteWorkflowRequest{ID: id})
 	return err
+}
+
+func (p *WorkflowProvider) toProtoWorkflowDefinition(definition yaml.WorkflowDefinition) *coreapi.CreateWorkflowRequest_WorkflowDefinition {
+	return &coreapi.CreateWorkflowRequest_WorkflowDefinition{}
 }
--- systemservices/workflow/workflow.go
+++ systemservices/workflow/workflow.go
@@ -1,9 +1,11 @@
 package workflow
 
 import (
+	"encoding/json"
 	"errors"
 
 	"github.com/mesg-foundation/core/execution"
+	"github.com/mesg-foundation/core/utils/workflowparser/yaml"
 )
 
 // WSS's tasks.
@@ -14,11 +16,22 @@ const (
 
 // CreateInputs maps create task's inputs.
 // name is optional and has to be unique.
-func CreateInputs(yaml []byte, name string) map[string]interface{} {
-	return map[string]interface{}{
-		"yaml": string(yaml),
-		"name": name,
+func CreateInputs(definition yaml.WorkflowDefinition, name string) (map[string]interface{}, error) {
+	// TODO: this hack is not something that we should do but
+	// it's needed because *parameterValidator is not able to identify
+	// structs for now.
+	definitionData, err := json.Marshal(definition)
+	if err != nil {
+		return nil, err
 	}
+	var data interface{}
+	if err := json.Unmarshal(definitionData, &data); err != nil {
+		return nil, err
+	}
+	return map[string]interface{}{
+		"definition": data,
+		"name":       name,
+	}, nil
 }
 
 // CreateOutputs maps create task's outputs.
