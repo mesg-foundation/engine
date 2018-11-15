3966f328ef8f28e399fe78e56da23066e0d32d1d
Nicolas Mahe <nicolas@mahe.me>
2018-11-15 16:55:22 +0700

    Add more test to deployer

--- systemservices/deployer/deployer_test.go
+++ systemservices/deployer/deployer_test.go
@@ -1,6 +1,7 @@
 package deployer
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -12,17 +13,17 @@ import (
 	"github.com/mesg-foundation/core/systemservices"
 )
 
-func TestNew(t *testing.T) {
+func newDeployer(t *testing.T) (*Deployer, func()) {
 	conf, err := config.Global()
 	require.NoError(t, err)
 
 	var (
-		systemServicesPath = filepath.Join(conf.Core.Path, conf.SystemServices.RelativePath)
-		databasePath       = filepath.Join(conf.Core.Path, conf.Core.Database.ServiceRelativePath)
-		execDatabasePath   = filepath.Join(conf.Core.Path, conf.Core.Database.ExecutionRelativePath)
+		systemServicesPath  = filepath.Join(conf.Core.Path, conf.SystemServices.RelativePath)
+		serviceDatabasePath = "service.db.test"
+		execDatabasePath    = "exec.db.test"
 	)
 
-	serviceDB, err := database.NewServiceDB(databasePath)
+	serviceDB, err := database.NewServiceDB(serviceDatabasePath)
 	require.NoError(t, err)
 
 	execDB, err := database.NewExecutionDB(execDatabasePath)
@@ -35,4 +36,30 @@ func TestNew(t *testing.T) {
 
 	d := New(a, systemServicesPath, ss)
 	require.NotZero(t, d)
+
+	closer := func() {
+		require.NoError(t, serviceDB.Close())
+		require.NoError(t, execDB.Close())
+		require.NoError(t, os.RemoveAll(serviceDatabasePath))
+		require.NoError(t, os.RemoveAll(execDatabasePath))
+	}
+
+	return d, closer
+}
+
+func TestNotExisting(t *testing.T) {
+	d, closer := newDeployer(t)
+	defer closer()
+	err := d.Deploy([]string{"noExisting"})
+	expectedErr := &systemServiceNotFoundError{name: "noExisting"}
+	require.EqualError(t, err, expectedErr.Error())
+}
+
+// TODO: Should have a mock on API in order to convert this integration test to unit test
+func TestDeploy(t *testing.T) {
+	d, closer := newDeployer(t)
+	defer closer()
+	err := d.Deploy([]string{systemservices.ResolverService})
+	// TODO: should stop the deployed ss
+	require.NoError(t, err)
 }
