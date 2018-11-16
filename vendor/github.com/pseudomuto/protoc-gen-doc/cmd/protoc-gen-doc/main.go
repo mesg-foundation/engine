90ddb9fb8fef2cdbe8b45365ac2255e9b3f922be
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-11-16 17:08:39 +0300

    update vendor/

--- vendor/github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc/main.go
+++ vendor/github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc/main.go
@@ -14,36 +14,37 @@
 package main
 
 import (
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/plugin"
-	"github.com/pseudomuto/protoc-gen-doc"
-	"io/ioutil"
+	"github.com/pseudomuto/protokit"
+
 	"log"
 	"os"
+
+	"github.com/pseudomuto/protoc-gen-doc"
 )
 
 func main() {
-	input, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("Could not read contents from stdin")
+	if flags := ParseFlags(os.Stdout, os.Args); HandleFlags(flags) {
+		os.Exit(flags.Code())
 	}
 
-	req := new(plugin_go.CodeGeneratorRequest)
-	if err = proto.Unmarshal(input, req); err != nil {
+	if err := protokit.RunPlugin(new(gendoc.Plugin)); err != nil {
 		log.Fatal(err)
+	}
+}
+
+// HandleFlags checks if there's a match and returns true if it was "handled"
+func HandleFlags(f *Flags) bool {
+	if !f.HasMatch() {
+		return false
 	}
 
-	resp, err := gendoc.RunPlugin(req)
-	if err != nil {
-		log.Fatal(err)
+	if f.ShowHelp() {
+		f.PrintHelp()
 	}
 
-	data, err := proto.Marshal(resp)
-	if err != nil {
-		log.Fatal(err)
+	if f.ShowVersion() {
+		f.PrintVersion()
 	}
 
-	if _, err := os.Stdout.Write(data); err != nil {
-		log.Fatal(err)
-	}
+	return true
 }
