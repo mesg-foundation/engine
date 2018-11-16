cbd398a2d2c3b389fba81a27b9b877edead81cc4
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-11-16 09:29:52 +0300

    workflow feature: move workflow creation and deletion logic to api pkg

--- interface/grpc/core/delete_workflow.go
+++ interface/grpc/core/delete_workflow.go
@@ -4,15 +4,9 @@ import (
 	"context"
 
 	"github.com/mesg-foundation/core/protobuf/coreapi"
-	"github.com/mesg-foundation/core/systemservices/workflow"
 )
 
 // DeleteWorkflow stops and deletes a workflow.
 func (s *Server) DeleteWorkflow(ctx context.Context, request *coreapi.DeleteWorkflowRequest) (*coreapi.DeleteWorkflowReply, error) {
-	exec, err := s.api.ExecuteAndListen(s.ss.WorkflowServiceID(), workflow.DeleteTaskKey, workflow.DeleteInputs(request.ID))
-	if err != nil {
-		return nil, err
-	}
-
-	return &coreapi.DeleteWorkflowReply{}, workflow.DeleteOutputs(exec)
+	return &coreapi.DeleteWorkflowReply{}, s.api.DeleteWorkflow(request.ID)
 }
