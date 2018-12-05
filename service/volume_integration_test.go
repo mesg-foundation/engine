3998e17a3b29f9f8db17e5203a7ebdd71806bd1c
Nicolas Mahe <nicolas@mahe.me>
2018-12-05 10:05:40 +0700

    Add delete volume integration test

---
+++ service/volume_integration_test.go
@@ -0,0 +1,39 @@
+// +build integration
+
+package service
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIntegrationDeleteVolumes(t *testing.T) {
+	var (
+		dependencyKey1 = "1"
+		dependencyKey2 = "2"
+		volumeA        = "/a"
+		volumeB        = "/b"
+		s, _           = FromService(&Service{
+			Name: "TestIntegrationDeleteVolumes",
+			Dependencies: []*Dependency{
+				{
+					Key:     dependencyKey1,
+					Image:   "http-server",
+					Volumes: []string{volumeA, volumeB},
+				},
+				{
+					Key:         dependencyKey2,
+					Image:       "http-server",
+					VolumesFrom: []string{dependencyKey1},
+				},
+			},
+		}, ContainerOption(newIntegrationContainer(t)))
+	)
+	_, err := s.Start()
+	require.NoError(t, err)
+	err = s.Stop()
+	require.NoError(t, err)
+	err = s.DeleteVolumes()
+	require.NoError(t, err)
+}
