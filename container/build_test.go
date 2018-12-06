0e94f00589bda45828ead4bc775db24c8de50246
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-12-06 23:11:15 +0300

    container: convert some unit tests from dockertest to testify & improvements

--- container/build_test.go
+++ container/build_test.go
@@ -2,63 +2,101 @@ package container
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"strings"
 	"testing"
 
 	"github.com/docker/docker/api/types"
-	"github.com/mesg-foundation/core/container/dockertest"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 func TestBuild(t *testing.T) {
-	path := "test/"
-	tag := "sha256:1f6359c933421f53a7ef9e417bfa51b1c313c54878fdeb16de827f427e16d836"
+	var (
+		tag   = "sha256:1f6359c933421f53a7ef9e417bfa51b1c313c54878fdeb16de827f427e16d836"
+		c, mc = newTesting(t)
+	)
 
-	dt := dockertest.New()
-	c, _ := New(ClientOption(dt.Client()))
-
-	dt.ProvideImageBuild(ioutil.NopCloser(strings.NewReader(
-		fmt.Sprintf(`{"stream":"%s\n"}`, tag),
-	)), nil)
-
-	tag1, err := c.Build(path)
-	require.NoError(t, err)
-	require.Equal(t, tag, tag1)
-
-	li := <-dt.LastImageBuild()
-	require.True(t, len(li.FileData) > 0)
-	require.Equal(t, types.ImageBuildOptions{
+	mc.On("ImageBuild", mock.Anything, mock.Anything, types.ImageBuildOptions{
 		Remove:         true,
 		ForceRemove:    true,
 		SuppressOutput: true,
-	}, li.Options)
+	}).Once().
+		Return(types.ImageBuildResponse{
+			Body: ioutil.NopCloser(strings.NewReader(
+				fmt.Sprintf(`{"stream":"%s\n"}`, tag),
+			)),
+		}, nil).
+		Run(func(args mock.Arguments) {
+			buildContext, err := archive.TarWithOptions("test/", &archive.TarOptions{
+				Compression:     archive.Gzip,
+				ExcludePatterns: []string{"ignoreme"},
+			})
+			require.NoError(t, err)
+			defer buildContext.Close()
+
+			wantedData, err := ioutil.ReadAll(buildContext)
+			require.NoError(t, err)
+
+			data, err := ioutil.ReadAll(args.Get(1).(io.Reader))
+			require.NoError(t, err)
+			require.Equal(t, wantedData, data)
+		})
+
+	tag1, err := c.Build("test/")
+	require.NoError(t, err)
+	require.Equal(t, tag, tag1)
+
+	mc.AssertExpectations(t)
 }
 
 func TestBuildNotWorking(t *testing.T) {
-	path := "test-not-valid/"
+	var (
+		path  = "test-not-valid"
+		c, mc = newTesting(t)
+	)
 
-	dt := dockertest.New()
-	c, _ := New(ClientOption(dt.Client()))
-
-	dt.ProvideImageBuild(ioutil.NopCloser(strings.NewReader(`
-{"stream":"Step 1/2 : FROM notExistingImage"}
-{"stream":"\n"}
-{"errorDetail":{"message":"invalid reference format: repository name must be lowercase"},"error":"invalid reference format: repository name must be lowercase"}`)), nil)
+	mc.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
+		Once().
+		Return(types.ImageBuildResponse{
+			Body: ioutil.NopCloser(strings.NewReader(`
+		{"stream":"Step 1/2 : FROM notExistingImage"}
+		{"stream":"\n"}
+		{"errorDetail":{"message":"invalid reference format: repository name must be lowercase"},"error":"invalid reference format: repository name must be lowercase"}`)),
+		}, nil).
+		Run(func(args mock.Arguments) {
+			_, err := ioutil.ReadAll(args.Get(1).(io.Reader))
+			require.NoError(t, err)
+		})
 
 	tag, err := c.Build(path)
 	require.Equal(t, "image build failed. invalid reference format: repository name must be lowercase", err.Error())
 	require.Equal(t, "", tag)
+
+	mc.AssertExpectations(t)
 }
 
 func TestBuildWrongPath(t *testing.T) {
-	dt := dockertest.New()
-	c, _ := New(ClientOption(dt.Client()))
+	var (
+		c, mc = newTesting(t)
+	)
 
-	dt.ProvideImageBuild(ioutil.NopCloser(strings.NewReader("")), nil)
+	mc.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
+		Once().
+		Return(types.ImageBuildResponse{
+			Body: ioutil.NopCloser(strings.NewReader("")),
+		}, nil).
+		Run(func(args mock.Arguments) {
+			_, err := ioutil.ReadAll(args.Get(1).(io.Reader))
+			require.NoError(t, err)
+		})
 
 	_, err := c.Build("testss/")
 	require.Equal(t, "could not parse container build response", err.Error())
+
+	mc.AssertExpectations(t)
 }
 
 func TestParseBuildResponseInvalidJSON(t *testing.T) {
