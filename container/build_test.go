92f88a684abd4b866d9fe49e3942e51c7051f1b3
krhubert <krhubert@gmail.com>
2018-12-07 08:45:49 +0100

    Lint tests

--- container/build_test.go
+++ container/build_test.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -13,117 +14,93 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestBuild(t *testing.T) {
-	var (
-		tag   = "sha256:1f6359c933421f53a7ef9e417bfa51b1c313c54878fdeb16de827f427e16d836"
-		c, mc = newTesting(t)
-	)
-
-	mc.On("ImageBuild", mock.Anything, mock.Anything, types.ImageBuildOptions{
-		Remove:         true,
-		ForceRemove:    true,
-		SuppressOutput: true,
-	}).Once().
-		Return(types.ImageBuildResponse{
-			Body: ioutil.NopCloser(strings.NewReader(
-				fmt.Sprintf(`{"stream":"%s\n"}`, tag),
-			)),
-		}, nil).
-		Run(func(args mock.Arguments) {
-			buildContext, err := archive.TarWithOptions("test/", &archive.TarOptions{
-				Compression:     archive.Gzip,
-				ExcludePatterns: []string{"ignoreme"},
-			})
-			require.NoError(t, err)
-			defer buildContext.Close()
-
-			wantedData, err := ioutil.ReadAll(buildContext)
-			require.NoError(t, err)
-
-			data, err := ioutil.ReadAll(args.Get(1).(io.Reader))
-			require.NoError(t, err)
-			require.Equal(t, wantedData, data)
-		})
-
-	tag1, err := c.Build("test/")
-	require.NoError(t, err)
-	require.Equal(t, tag, tag1)
-
-	mc.AssertExpectations(t)
+func newImageBuildResponse(body string) types.ImageBuildResponse {
+	return types.ImageBuildResponse{
+		Body: ioutil.NopCloser(strings.NewReader(body)),
+	}
 }
 
-func TestBuildNotWorking(t *testing.T) {
+func TestBuild(t *testing.T) {
 	var (
-		path  = "test-not-valid"
-		c, mc = newTesting(t)
+		expectedTag = "sha256:1f6359c933421f53a7ef9e417bfa51b1c313c54878fdeb16de827f427e16d836"
+		options     = types.ImageBuildOptions{
+			Remove:         true,
+			ForceRemove:    true,
+			SuppressOutput: true,
+		}
+		c, m = newTesting(t)
 	)
 
-	mc.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
-		Once().
-		Return(types.ImageBuildResponse{
-			Body: ioutil.NopCloser(strings.NewReader(`
-		{"stream":"Step 1/2 : FROM notExistingImage"}
-		{"stream":"\n"}
-		{"errorDetail":{"message":"invalid reference format: repository name must be lowercase"},"error":"invalid reference format: repository name must be lowercase"}`)),
-		}, nil).
-		Run(func(args mock.Arguments) {
-			_, err := ioutil.ReadAll(args.Get(1).(io.Reader))
-			require.NoError(t, err)
+	matchContext := func(context io.Reader) bool {
+		buildContext, err := archive.TarWithOptions("test/", &archive.TarOptions{
+			Compression:     archive.Gzip,
+			ExcludePatterns: []string{"ignoreme"},
 		})
+		if err != nil {
+			return false
+		}
+		defer buildContext.Close()
+
+		wantedData, err := ioutil.ReadAll(buildContext)
+		if err != nil {
+			return false
+		}
+
+		data, err := ioutil.ReadAll(context)
+		if err != nil {
+			return false
+		}
+		return bytes.Equal(wantedData, data)
+	}
+
+	m.On("ImageBuild", mock.Anything, mock.MatchedBy(matchContext), options).
+		Once().Return(newImageBuildResponse(fmt.Sprintf(`{"stream":"%s\n"}`, expectedTag)), nil).
+		Run(func(args mock.Arguments) { ioutil.ReadAll(args.Get(1).(io.Reader)) })
+
+	tag, err := c.Build("test/")
+	require.NoError(t, err)
+	require.Equal(t, expectedTag, tag)
+
+	m.AssertExpectations(t)
+}
+
+func TestBuildResponseError(t *testing.T) {
+	var (
+		path = "test-not-valid"
+		c, m = newTesting(t)
+	)
+
+	const body = `
+{"stream":"Step 1/2 : FROM notExistingImage"}
+{"stream":"\n"}
+{"errorDetail":{"message": "invalid reference format: repository name must be lowercase"},"error":"invalid reference format: repository name must be lowercase"}`
+
+	m.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
+		Once().Return(newImageBuildResponse(body), nil).
+		Run(func(args mock.Arguments) { ioutil.ReadAll(args.Get(1).(io.Reader)) })
 
 	tag, err := c.Build(path)
+	require.Empty(t, tag)
 	require.Equal(t, "image build failed. invalid reference format: repository name must be lowercase", err.Error())
-	require.Equal(t, "", tag)
 
-	mc.AssertExpectations(t)
+	m.AssertExpectations(t)
 }
 
 func TestBuildWrongPath(t *testing.T) {
-	var (
-		c, mc = newTesting(t)
-	)
+	var c, m = newTesting(t)
 
-	mc.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
-		Once().
-		Return(types.ImageBuildResponse{
-			Body: ioutil.NopCloser(strings.NewReader("")),
-		}, nil).
-		Run(func(args mock.Arguments) {
-			_, err := ioutil.ReadAll(args.Get(1).(io.Reader))
-			require.NoError(t, err)
-		})
+	m.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
+		Once().Return(newImageBuildResponse(""), nil).
+		Run(func(args mock.Arguments) { ioutil.ReadAll(args.Get(1).(io.Reader)) })
 
 	_, err := c.Build("testss/")
 	require.Equal(t, "could not parse container build response", err.Error())
 
-	mc.AssertExpectations(t)
-}
-
-func TestParseBuildResponseInvalidJSON(t *testing.T) {
-	body := ioutil.NopCloser(strings.NewReader("invalidJSON"))
-	response := types.ImageBuildResponse{
-		Body: body,
-	}
-	_, err := parseBuildResponse(response)
-	require.Error(t, err)
+	m.AssertExpectations(t)
 }
 
 func TestParseBuildResponse(t *testing.T) {
-	body := ioutil.NopCloser(strings.NewReader("{\"stream\":\"ok\"}"))
-	response := types.ImageBuildResponse{
-		Body: body,
-	}
-	tag, err := parseBuildResponse(response)
-	require.NoError(t, err)
-	require.Equal(t, tag, "ok")
-}
-
-func TestParseBuildResponseWithNewLine(t *testing.T) {
-	body := ioutil.NopCloser(strings.NewReader("{\"stream\":\"ok\\n\"}"))
-	response := types.ImageBuildResponse{
-		Body: body,
-	}
-	tag, err := parseBuildResponse(response)
+	tag, err := parseBuildResponse(newImageBuildResponse("{\"stream\":\"ok\\n\"}"))
 	require.NoError(t, err)
 	require.Equal(t, tag, "ok")
 }
