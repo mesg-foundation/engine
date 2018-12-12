95001f6a11e948aeb81f3f9657921b585095f0c4
krhubert <krhubert@gmail.com>
2018-12-12 10:55:47 +0100

    Move MapToEnv to xos + Add MergeMapEnvs

--- x/xos/env.go
+++ x/xos/env.go
@@ -1,6 +1,10 @@
 package xos
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"sort"
+)
 
 // GetenvDefault retrieves the value of the environment variable named by the key.
 // It returns the value, which will be set to fallback if the variable is empty.
@@ -9,4 +13,27 @@ func GetenvDefault(key, fallback string) string {
 		return val
 	}
 	return fallback
+}
+
+// MapToEnv transform a map of key value to a slice of env in the form "key=value".
+// Env vars are sorted by names to get an accurate order while testing.
+func MapToEnv(data map[string]string) []string {
+	env := make([]string, 0, len(data))
+	for key, value := range data {
+		env = append(env, fmt.Sprintf("%s=%s", key, value))
+	}
+	sort.Strings(env)
+	return env
+}
+
+// MergeMapEnvs merges multiple maps storing environment varialbes into single one.
+// If the same key exist multiple time, it will be overwritten by the latest occurrence.
+func MergeMapEnvs(envs ...map[string]string) map[string]string {
+	env := make(map[string]string)
+	for _, e := range envs {
+		for k, v := range e {
+			env[k] = v
+		}
+	}
+	return env
 }
