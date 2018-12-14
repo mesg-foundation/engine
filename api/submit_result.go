e2b8819d10cc156ecf322f6906f768976868df97
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2018-12-14 14:47:07 +0300

    api>submit result: verbose fail on any kind of error while submitting a result

--- api/submit_result.go
+++ api/submit_result.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/mesg-foundation/core/execution"
 	"github.com/mesg-foundation/core/pubsub"
 	"github.com/mesg-foundation/core/service"
 )
@@ -27,32 +28,39 @@ func newResultSubmitter(api *API) *resultSubmitter {
 
 // Submit submits results for executionID.
 func (s *resultSubmitter) Submit(executionID string, outputKey string, outputData []byte) error {
+	exec, err := s.processExecution(executionID, outputKey, outputData)
+	go pubsub.Publish(exec.Service.ResultSubscriptionChannel(), exec)
+	return err
+}
+
+// processExecution processes execution and marks it as complated or failed.
+func (s *resultSubmitter) processExecution(executionID string, outputKey string, outputData []byte) (*execution.Execution, error) {
 	exec, err := s.api.execDB.Find(executionID)
 	if err != nil {
-		return err
+		exec.Failed(err)
+		return nil, err
 	}
+
 	exec.Service, err = service.FromService(exec.Service, service.ContainerOption(s.api.container))
 	if err != nil {
-		return err
+		exec.Failed(err)
+		return nil, err
 	}
 
 	var outputDataMap map[string]interface{}
 	if err = json.Unmarshal(outputData, &outputDataMap); err != nil {
 		err = fmt.Errorf("invalid output data error: %s", err)
-	}
-
-	if err == nil {
-		err = exec.Complete(outputKey, outputDataMap)
-	}
-
-	if err != nil {
 		exec.Failed(err)
+	} else {
+		if err = exec.Complete(outputKey, outputDataMap); err != nil {
+			exec.Failed(err)
+		}
 	}
 
 	if err := s.api.execDB.Save(exec); err != nil {
-		return err
+		exec.Failed(err)
+		return nil, err
 	}
 
-	go pubsub.Publish(exec.Service.ResultSubscriptionChannel(), exec)
-	return err
+	return exec, nil
 }
