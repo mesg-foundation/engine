961e490155fe8c745cb66fd4f1857c4381b2d898
Nicolas Mahe <nicolas@mahe.me>
2018-12-18 12:30:41 +0700

    Improve logic of API submit result

--- api/submit_result.go
+++ api/submit_result.go
@@ -29,8 +29,11 @@ func newResultSubmitter(api *API) *resultSubmitter {
 // Submit submits results for executionID.
 func (s *resultSubmitter) Submit(executionID string, outputKey string, outputData []byte) error {
 	exec, err := s.processExecution(executionID, outputKey, outputData)
+	if err != nil {
+		return err
+	}
 	go pubsub.Publish(exec.Service.ResultSubscriptionChannel(), exec)
-	return err
+	return nil
 }
 
 // processExecution processes execution and marks it as complated or failed.
@@ -42,24 +45,30 @@ func (s *resultSubmitter) processExecution(executionID string, outputKey string,
 
 	exec.Service, err = service.FromService(exec.Service, service.ContainerOption(s.api.container))
 	if err != nil {
-		exec.Failed(err)
-		return exec, err
+		return exec, s.failedAndSave(exec, err)
 	}
 
 	var outputDataMap map[string]interface{}
-	if err = json.Unmarshal(outputData, &outputDataMap); err != nil {
-		err = fmt.Errorf("invalid output data error: %s", err)
-		exec.Failed(err)
-	} else {
-		if err = exec.Complete(outputKey, outputDataMap); err != nil {
-			exec.Failed(err)
-		}
+	if err := json.Unmarshal(outputData, &outputDataMap); err != nil {
+		return exec, s.failedAndSave(exec, fmt.Errorf("invalid output data error: %s", err))
+	}
+
+	if err = exec.Complete(outputKey, outputDataMap); err != nil {
+		return exec, s.failedAndSave(exec, err)
 	}
 
 	if err := s.api.execDB.Save(exec); err != nil {
-		exec.Failed(err)
-		return exec, err
+		// if save already fail, it's not necessary to try to save again
+		return exec, exec.Failed(err)
 	}
 
 	return exec, nil
+}
+
+// failedAndSave set the execution as failed and save it.
+func (s *resultSubmitter) failedAndSave(exec *execution.Execution, err error) error {
+	if errFailed := exec.Failed(err); errFailed != nil {
+		return errFailed
+	}
+	return s.api.execDB.Save(exec)
 }
