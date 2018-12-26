477ad2f9251a202b3436b8d1da2f1638975c992b
Nicolas Mahe <nicolas@mahe.me>
2018-12-26 18:02:13 +0700

    Improve tabulation

--- container/build_test.go
+++ container/build_test.go
@@ -28,34 +28,43 @@ func TestBuild(t *testing.T) {
 			ForceRemove:    true,
 			SuppressOutput: true,
 		}
-		c, m = newTesting(t)
+		c, m                   = newTesting(t)
+		imageBuildMatchContext = func(context io.Reader) bool {
+			buildContext, err := archive.TarWithOptions("test/", &archive.TarOptions{
+				Compression:     archive.Gzip,
+				ExcludePatterns: []string{"ignoreme"},
+			})
+			if err != nil {
+				return false
+			}
+			defer buildContext.Close()
+
+			wantedData, err := ioutil.ReadAll(buildContext)
+			if err != nil {
+				return false
+			}
+
+			data, err := ioutil.ReadAll(context)
+			if err != nil {
+				return false
+			}
+			return bytes.Equal(wantedData, data)
+		}
+		imageBuildArguments = []interface{}{
+			mock.Anything,
+			mock.MatchedBy(imageBuildMatchContext),
+			options,
+		}
+		imageBuildResponse = []interface{}{
+			newImageBuildResponse(fmt.Sprintf(`{"stream":"%s\n"}`, expectedTag)),
+			nil,
+		}
+		imageBuildRun = func(args mock.Arguments) {
+			ioutil.ReadAll(args.Get(1).(io.Reader))
+		}
 	)
 
-	matchContext := func(context io.Reader) bool {
-		buildContext, err := archive.TarWithOptions("test/", &archive.TarOptions{
-			Compression:     archive.Gzip,
-			ExcludePatterns: []string{"ignoreme"},
-		})
-		if err != nil {
-			return false
-		}
-		defer buildContext.Close()
-
-		wantedData, err := ioutil.ReadAll(buildContext)
-		if err != nil {
-			return false
-		}
-
-		data, err := ioutil.ReadAll(context)
-		if err != nil {
-			return false
-		}
-		return bytes.Equal(wantedData, data)
-	}
-
-	m.On("ImageBuild", mock.Anything, mock.MatchedBy(matchContext), options).
-		Once().Return(newImageBuildResponse(fmt.Sprintf(`{"stream":"%s\n"}`, expectedTag)), nil).
-		Run(func(args mock.Arguments) { ioutil.ReadAll(args.Get(1).(io.Reader)) })
+	m.On("ImageBuild", imageBuildArguments...).Once().Return(imageBuildResponse...).Run(imageBuildRun)
 
 	tag, err := c.Build("test/")
 	require.NoError(t, err)
@@ -66,18 +75,26 @@ func TestBuild(t *testing.T) {
 
 func TestBuildResponseError(t *testing.T) {
 	var (
-		path = "test-not-valid"
-		c, m = newTesting(t)
-	)
-
-	const body = `
+		path                = "test-not-valid"
+		c, m                = newTesting(t)
+		imageBuildArguments = []interface{}{
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+		}
+		imageBuildResponse = []interface{}{
+			newImageBuildResponse(`
 {"stream":"Step 1/2 : FROM notExistingImage"}
 {"stream":"\n"}
-{"errorDetail":{"message": "invalid reference format: repository name must be lowercase"},"error":"invalid reference format: repository name must be lowercase"}`
+{"errorDetail":{"message": "invalid reference format: repository name must be lowercase"},"error":"invalid reference format: repository name must be lowercase"}`),
+			nil,
+		}
+		imageBuildRun = func(args mock.Arguments) {
+			ioutil.ReadAll(args.Get(1).(io.Reader))
+		}
+	)
 
-	m.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
-		Once().Return(newImageBuildResponse(body), nil).
-		Run(func(args mock.Arguments) { ioutil.ReadAll(args.Get(1).(io.Reader)) })
+	m.On("ImageBuild", imageBuildArguments...).Once().Return(imageBuildResponse...).Run(imageBuildRun)
 
 	tag, err := c.Build(path)
 	require.Empty(t, tag)
@@ -87,11 +104,23 @@ func TestBuildResponseError(t *testing.T) {
 }
 
 func TestBuildWrongPath(t *testing.T) {
-	var c, m = newTesting(t)
+	var (
+		c, m                = newTesting(t)
+		imageBuildArguments = []interface{}{
+			mock.Anything,
+			mock.Anything,
+			mock.Anything,
+		}
+		imageBuildResponse = []interface{}{
+			newImageBuildResponse(""),
+			nil,
+		}
+		imageBuildRun = func(args mock.Arguments) {
+			ioutil.ReadAll(args.Get(1).(io.Reader))
+		}
+	)
 
-	m.On("ImageBuild", mock.Anything, mock.Anything, mock.Anything).
-		Once().Return(newImageBuildResponse(""), nil).
-		Run(func(args mock.Arguments) { ioutil.ReadAll(args.Get(1).(io.Reader)) })
+	m.On("ImageBuild", imageBuildArguments...).Once().Return(imageBuildResponse...).Run(imageBuildRun)
 
 	_, err := c.Build("testss/")
 	require.Equal(t, "could not parse container build response", err.Error())
