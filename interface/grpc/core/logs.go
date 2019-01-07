b8e486eb3534cb1f5c0098672ba76eba5ecebad0
Nicolas Mahe <nicolas@mahe.me>
2019-01-07 20:34:46 +0700

    Move acknowledgement from coreapi to dedicated acknowledgement package

--- interface/grpc/core/logs.go
+++ interface/grpc/core/logs.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"github.com/mesg-foundation/core/api"
+	"github.com/mesg-foundation/core/protobuf/acknowledgement"
 	"github.com/mesg-foundation/core/protobuf/coreapi"
 	"github.com/mesg-foundation/core/utils/chunker"
 )
@@ -35,7 +36,7 @@ func (s *Server) ServiceLogs(request *coreapi.ServiceLogsRequest, stream coreapi
 	}
 
 	// send header to notify client that the stream is ready.
-	if err := coreapi.SetStreamReady(stream); err != nil {
+	if err := acknowledgement.SetStreamReady(stream); err != nil {
 		return err
 	}
 
