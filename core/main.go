c40c75cd53ee8b19c2c4b348601dd0192d613e3f
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-01-23 14:00:13 +0700

    do not use config attribute for the list of services

--- core/main.go
+++ core/main.go
@@ -42,7 +42,7 @@ func initGRPCServer(c *config.Config) (*grpc.Server, error) {
 }
 
 func deployCoreServices(c *config.Config, api *api.API) error {
-	for _, service := range c.Services {
+	for _, service := range c.Services() {
 		logrus.Infof("Deploy service from %s", service.URL)
 		s, valid, err := api.DeployServiceFromURL(service.URL, service.Env)
 		if valid != nil {
