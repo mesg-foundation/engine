47a1966dbb36c40680863a0a082fd679a9b0ac44
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-01-24 23:23:47 +0700

    support multiple arguments for start command

--- commands/service_start.go
+++ commands/service_start.go
@@ -20,24 +20,24 @@ func newServiceStartCmd(e ServiceExecutor) *serviceStartCmd {
 		Short:   "Start a service",
 		Long:    "Start a service previously published services",
 		Example: `mesg-core service start SERVICE`,
-		Args:    cobra.ExactArgs(1),
 		RunE:    c.runE,
 	})
 	return c
 }
 
 func (c *serviceStartCmd) runE(cmd *cobra.Command, args []string) error {
-	var (
-		serviceID = args[0]
-		err       error
-	)
-	pretty.Progress("Starting service...", func() {
-		err = c.e.ServiceStart(serviceID)
-	})
-	if err != nil {
-		return err
+	for _, serviceID := range args {
+		var err error
+		pretty.Progress("Starting service...", func() {
+			err = c.e.ServiceStart(serviceID)
+		})
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s Service %q started\n", pretty.SuccessSign, serviceID)
+		if len(args) == 1 {
+			fmt.Printf("To see its logs, run the command:\n\tmesg-core service logs %s\n", serviceID)
+		}
 	}
-	fmt.Printf("%s Service started\n", pretty.SuccessSign)
-	fmt.Printf("To see its logs, run the command:\n\tmesg-core service logs %s\n", serviceID)
 	return nil
 }
