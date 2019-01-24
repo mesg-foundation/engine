e3f00b30aeca81be2e8adac1580a4a5b19fd1597
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-01-24 23:23:33 +0700

    support multiple arguments for stop command

--- commands/service_stop.go
+++ commands/service_stop.go
@@ -19,20 +19,21 @@ func newServiceStopCmd(e ServiceExecutor) *serviceStopCmd {
 		Use:     "stop SERVICE",
 		Short:   "Stop a service",
 		Example: `mesg-core service stop SERVICE`,
-		Args:    cobra.ExactArgs(1),
 		RunE:    c.runE,
 	})
 	return c
 }
 
 func (c *serviceStopCmd) runE(cmd *cobra.Command, args []string) error {
-	var err error
-	pretty.Progress("Stopping service...", func() {
-		err = c.e.ServiceStop(args[0])
-	})
-	if err != nil {
-		return err
+	for _, serviceID := range args {
+		var err error
+		pretty.Progress("Stopping service...", func() {
+			err = c.e.ServiceStop(serviceID)
+		})
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s Service %q stopped\n", pretty.SuccessSign, serviceID)
 	}
-	fmt.Printf("%s Service stopped\n", pretty.SuccessSign)
 	return nil
 }
