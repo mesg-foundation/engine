470a753527a904aaf9aeac5c7d44927d22bf5c96
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-02-03 22:55:14 +0700

    update service config

--- config/services.go
+++ config/services.go
@@ -5,15 +5,42 @@ package config
 // The service name should be the name of the directory inside `systemservices` but capitalized
 // example:
 // 		var (
-// 			FooURL string
 // 			BarURL string
 // 		)
-var ()
+var (
+	EthwalletURL string
+)
 
-func (c *Config) initServices() {
+// ServiceConfig contains information related to services that the config knows about
+type ServiceConfig struct {
+	URL  string
+	Env  map[string]string
+	Sid  string
+	Hash string
+}
+
+// ServiceConfigWithKey contains information related to services that the config knows about and their key
+type ServiceConfigWithKey struct {
+	*ServiceConfig
+	Key string
+}
+
+// ServiceConfigGroup is the struct that contains all the `ServiceConfig` related to the config
+type ServiceConfigGroup struct {
+	Ethwallet ServiceConfig
+}
+
+// getServiceConfigGroup return the config for all services.
+// DO NOT USE STRING HERE but variable defined on top of this file `XxxURL` for config injection
+func (c *Config) getServiceConfigGroup() ServiceConfigGroup {
+	return ServiceConfigGroup{
+		Ethwallet: ServiceConfig{URL: EthwalletURL},
+	}
 }
 
 // Services returns all services that the configuration package is aware of
-func (c *Config) Services() []ServiceConfig {
-	return []ServiceConfig{}
+func (c *Config) Services() []*ServiceConfigWithKey {
+	return []*ServiceConfigWithKey{
+		{&c.Service.Ethwallet, "Ethwallet"},
+	}
 }
