cacfea247adb0c6f1bbf91a94e5d8406f5a79ad3
Nicolas Mahe <nicolas@mahe.me>
2019-02-12 12:52:09 +0700

    Improve get manifest function

--- systemservices/marketplace/src/contracts/manifest.ts
+++ systemservices/marketplace/src/contracts/manifest.ts
@@ -1,28 +1,37 @@
 import { Manifest } from "../types/service";
-import Request, { RequestPromise } from "request-promise-native"
-
-const getManifest = async (protocol: string, source: string): Promise<Manifest|undefined> => {
-  const data: any = {
-    'ipfs': getIpfs,
-    'https': getHttp,
-    'http': getHttp,
-  }
-  if (!data[protocol]) {
-    console.warn('protocol ' + protocol + ' is not compatible with this service')
-    return
-  }
-  const manifest = await data[protocol](source)
-  if (typeof manifest === 'object') {
-    return manifest as Manifest
-  }
-  console.warn('source ' + source + ' is not an object')
-}
+import Request from "request-promise-native"
 
 const getIpfs = async (source: string): Promise<any> => {
-  return await Request.get('https://ipfs.io/ipfs/' + source, { json: true })
+  return await Request.get('https://gateway.ipfs.io/ipfs/' + source, { json: true, timeout: 10000 })
 }
+
 const getHttp = async (source: string): Promise<any> => {
-  return await Request.get(source, { json: true })
+  return await Request.get(source, { json: true, timeout: 10000 })
+}
+
+const get: {[key: string]: (source: string) => Promise<any>} = {
+  'ipfs': getIpfs,
+  'https': getHttp,
+  'http': getHttp,
+}
+
+const getManifest = async (protocol: string, source: string): Promise<Manifest|undefined> => {
+  try {
+    if (!get[protocol]) {
+      console.warn('protocol', protocol, 'is not compatible with this service')
+      return
+    }
+    const manifest = await get[protocol](source)
+    if (typeof manifest === 'object') {
+      return manifest as Manifest
+    }
+    console.warn('manifest ', protocol, '::', source, 'is not a valid manifest')
+    return
+  }
+  catch (error) {
+    console.warn('error while downloading manifest ', protocol, '::', source, error.toString())
+    return
+  }
 }
 
 export { getManifest }
