48c2f8e7a333ba5ac0ea9b50cd9db6fb9227991a
krhubert <krhubert@gmail.com>
2019-02-14 00:04:45 +0100

    Add validator and check domain name funcs

---
+++ x/xvalidator/validator.go
@@ -0,0 +1,19 @@
+package xvalidator
+
+import (
+	"strconv"
+
+	"github.com/mesg-foundation/core/x/xnet"
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+// IsDomainName validates if given field is valid domain name.
+func IsDomainName(fl validator.FieldLevel) bool {
+	return xnet.IsDomainName(fl.Field().String())
+}
+
+// IsPort validates if given field is valid port.
+func IsPort(fl validator.FieldLevel) bool {
+	i, err := strconv.Atoi(fl.Field().String())
+	return err == nil && 0 < i && i <= 65535
+}
