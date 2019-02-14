4926163fabd10eeaf3fba4e0dae2bf658e731bf2
krhubert <krhubert@gmail.com>
2019-02-15 00:35:44 +0100

    Fix package name

--- x/xnet/dnsclient.go
+++ x/xnet/dnsclient.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package net
+package xnet
 
 // IsDomainName checks if a string is a presentation-format domain name
 // (currently restricted to hostname-compatible "preferred name" LDH labels and
