f361dcb103e8cb1750a351df7d24f8af1491407c
Nicolas Mahe <nicolas@mahe.me>
2019-02-21 17:21:30 +0700

    Refactor some function for commands marketplace

--- commands/marketplace_publish.go
+++ commands/marketplace_publish.go
@@ -3,24 +3,19 @@ package commands
 import (
 	"crypto/sha256"
 	"fmt"
-	"strings"
 
 	"github.com/mesg-foundation/core/commands/provider"
 	"github.com/mesg-foundation/core/utils/pretty"
 	"github.com/spf13/cobra"
-	"golang.org/x/crypto/sha3"
 	survey "gopkg.in/AlecAivazis/survey.v1"
 )
 
 type marketplacePublishCmd struct {
-	baseCmd
+	baseMarketplaceCmd
 
-	noPassphrase bool
-	passphrase   string
-	address      string
-	path         string
-	manifest     provider.ManifestData
-	service      *provider.MarketplaceService
+	path     string
+	manifest provider.ManifestData
+	service  *provider.MarketplaceService
 
 	e Executor
 }
@@ -33,11 +28,9 @@ func newMarketplacePublishCmd(e Executor) *marketplacePublishCmd {
 		Example: `mesg-core marketplace publish PATH_TO_SERVICE`,
 		PreRunE: c.preRunE,
 		RunE:    c.runE,
-		Args:    cobra.MaximumNArgs(1),
+		Args:    cobra.MinimumNArgs(1),
 	})
-	c.cmd.Flags().StringVarP(&c.address, "address", "a", c.address, "Address to use to publish the service")
-	c.cmd.Flags().BoolVarP(&c.noPassphrase, "no-passphrase", "n", c.noPassphrase, "Leave passphrase empty")
-	c.cmd.Flags().StringVarP(&c.passphrase, "passphrase", "p", c.passphrase, "Passphrase to use with the account")
+	c.setupFlags()
 	return c
 }
 
@@ -46,17 +39,11 @@ func (c *marketplacePublishCmd) preRunE(cmd *cobra.Command, args []string) error
 		err      error
 		question = "a new service"
 	)
-	if c.address == "" {
-		if err := askInput("Enter the address to use", &c.address); err != nil {
-			return err
-		}
+
+	if err := c.askAccountAndPassphrase(); err != nil {
+		return nil
 	}
-	c.address = strings.ToLower(c.address)
-	if !c.noPassphrase && c.passphrase == "" {
-		if err := askPass("Enter passphrase", &c.passphrase); err != nil {
-			return err
-		}
-	}
+
 	c.path = getFirstOrDefault(args)
 	c.manifest, err = c.e.CreateManifest(c.path)
 	if err != nil {
@@ -78,14 +65,14 @@ func (c *marketplacePublishCmd) preRunE(cmd *cobra.Command, args []string) error
 		if err != nil {
 			return err
 		}
-		if c.service.Owner != c.address {
-			return fmt.Errorf("the service's owner (%q) is different than the specified address", c.service.Owner)
+		if c.service.Owner != c.account {
+			return fmt.Errorf("the service's owner (%q) is different than the specified account", c.service.Owner)
 		}
 	}
 
 	var confirmed bool
 	if err := survey.AskOne(&survey.Confirm{
-		Message: fmt.Sprintf("Are you sure to publish %s %q from path %q using address %q?", question, c.manifest.Definition.Sid, c.path, c.address),
+		Message: fmt.Sprintf("Are you sure to publish %s %q from path %q using account %q?", question, c.manifest.Definition.Sid, c.path, c.account),
 	}, &confirmed, nil); err != nil {
 		return err
 	}
@@ -106,11 +93,11 @@ func (c *marketplacePublishCmd) runE(cmd *cobra.Command, args []string) error {
 	)
 	if c.service == nil {
 		pretty.Progress("Publishing service on the marketplace...", func() {
-			tx, err = c.e.CreateService(c.manifest.Definition.Sid, c.address)
+			tx, err = c.e.CreateService(c.manifest.Definition.Sid, c.account)
 			if err != nil {
 				return
 			}
-			_, err = c.signAndSendTransaction(tx)
+			_, err = c.signAndSendTransaction(c.e, tx)
 		})
 		if err != nil {
 			return err
@@ -129,32 +116,17 @@ func (c *marketplacePublishCmd) runE(cmd *cobra.Command, args []string) error {
 		h := sha256.New()
 		h.Write([]byte(manifestSource))
 		hash = fmt.Sprintf("0x%x", h.Sum(nil))
-		tx, err = c.e.CreateServiceVersion(c.manifest.Definition.Sid, hash, manifestSource, manifestProtocol, c.address)
+		tx, err = c.e.CreateServiceVersion(c.manifest.Definition.Sid, hash, manifestSource, manifestProtocol, c.account)
 		if err != nil {
 			return
 		}
-		_, err = c.signAndSendTransaction(tx)
+		_, err = c.signAndSendTransaction(c.e, tx)
 	})
 	if err != nil {
 		return err
 	}
 	fmt.Printf("%s Version created with success\n", pretty.SuccessSign)
-	h := sha3.NewLegacyKeccak256()
-	h.Write([]byte(c.manifest.Definition.Sid))
-	sidHash := fmt.Sprintf("0x%x", h.Sum(nil))
-	fmt.Printf("%s See it on the marketplace:\nhttps://marketplace.mesg.com/services/%s/%s\n", pretty.SuccessSign, sidHash, hash)
+	fmt.Printf("%s See it on the marketplace: https://marketplace.mesg.com/services/%s/%s\n", pretty.SuccessSign, c.sha3(c.manifest.Definition.Sid), hash)
 
 	return nil
-}
-
-func (c *marketplacePublishCmd) signAndSendTransaction(transaction *provider.Transaction) (*provider.TransactionReceipt, error) {
-	signedTransaction, err := c.e.Sign(c.address, c.passphrase, transaction)
-	if err != nil {
-		return nil, err
-	}
-	receipt, err := c.e.SendSignedTransaction(signedTransaction)
-	if err != nil {
-		return nil, err
-	}
-	return receipt, nil
 }
