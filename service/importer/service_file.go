99c17694402dbfbb4e26a008775784ac66db7312
Nicolas Mahe <nicolas@mahe.me>
2019-03-08 18:29:10 +0700

    Improving validation error

--- service/importer/service_file.go
+++ service/importer/service_file.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
+	"strings"
 
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/mesg-foundation/core/x/xvalidator"
 	validator "gopkg.in/go-playground/validator.v9"
+	en_translations "gopkg.in/go-playground/validator.v9/translations/en"
 	yaml "gopkg.in/yaml.v2"
+)
+
+const (
+	namespacePrefix = "ServiceDefinition."
 )
 
 func readServiceFile(path string) ([]byte, error) {
@@ -19,24 +27,50 @@ func readServiceFile(path string) ([]byte, error) {
 func validateServiceFile(data []byte) []string {
 	var service ServiceDefinition
 	if err := yaml.UnmarshalStrict(data, &service); err != nil {
-		return []string{fmt.Sprintf("parse mesg.yml error: %s", err)}
+		errs, ok := err.(*yaml.TypeError)
+		if !ok {
+			return []string{err.Error()}
+		}
+		return errs.Errors
 	}
 	return validateServiceStruct(&service)
 }
 
 func validateServiceStruct(service *ServiceDefinition) []string {
+	en := en.New()
+	uni := ut.New(en, en)
+	trans, _ := uni.GetTranslator("en")
 	validate := validator.New()
 	validate.RegisterValidation("port", xvalidator.IsPort)
+	validate.RegisterTranslation("port", trans, func(ut ut.Translator) error {
+		return ut.Add("port", "{0} must be a valid port", false)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("port", fe.Field(), namespacePrefix)
+		return t
+	})
 	validate.RegisterValidation("domain", xvalidator.IsDomainName)
+	validate.RegisterTranslation("domain", trans, func(ut ut.Translator) error {
+		return ut.Add("domain", "{0} must respect domain-style notation. Eg: author.name", false)
+	}, func(ut ut.Translator, fe validator.FieldError) string {
+		t, _ := ut.T("domain", fe.Field())
+		return t
+	})
+	en_translations.RegisterDefaultTranslations(validate, trans)
 
-	errs := validate.Struct(service)
+	err := validate.Struct(service)
 	warnings := []string{}
-	if errs != nil {
-		for _, err := range errs.(validator.ValidationErrors) {
-			warnings = append(
-				warnings,
-				fmt.Sprintf("%s with value %q is invalid: %s %s", err.Namespace(), err.Value(), err.Tag(), err.Param()),
-			)
+	if err != nil {
+		errs := err.(validator.ValidationErrors)
+		for _, e := range errs {
+			// Remove the name of the struct and the field from namespace
+			trimmedNamespace := strings.TrimPrefix(e.Namespace(), namespacePrefix)
+			trimmedNamespace = strings.TrimSuffix(trimmedNamespace, e.Field())
+			// Only use it when in-cascade field
+			namespace := ""
+			if e.Field() != trimmedNamespace {
+				namespace = trimmedNamespace
+			}
+			warnings = append(warnings, fmt.Sprintf("%s%s", namespace, e.Translate(trans)))
 		}
 	}
 
@@ -44,7 +78,7 @@ func validateServiceStruct(service *ServiceDefinition) []string {
 		if d.Image == "" {
 			warnings = append(
 				warnings,
-				fmt.Sprintf("dependencies[%s].image is invalid: empty", k),
+				fmt.Sprintf("Dependencies[%s].Image is a required field", k),
 			)
 		}
 	}
