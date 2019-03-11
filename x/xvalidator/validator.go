436c3869376bfab5bff22797447bad9f45c10978
Nicolas Mahe <nicolas@mahe.me>
2019-03-11 12:09:28 +0700

    Fix port validation

--- x/xvalidator/validator.go
+++ x/xvalidator/validator.go
@@ -16,9 +16,12 @@ func IsDomainName(fl validator.FieldLevel) bool {
 	return xnet.IsDomainName(fl.Field().String())
 }
 
-// IsPort validates if given field is valid port.
-func IsPort(fl validator.FieldLevel) bool {
+// IsPortMapping validates if given field is valid port mapping.
+func IsPortMapping(fl validator.FieldLevel) bool {
 	ports := strings.Split(fl.Field().String(), ":")
+	if len(ports) != 1 && len(ports) != 2 {
+		return false
+	}
 	for _, port := range ports {
 		i, err := strconv.Atoi(port)
 		if !(err == nil && 0 < i && i <= 65535) {
