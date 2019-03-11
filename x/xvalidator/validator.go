7b04dade71d877693b9bed81d3fd1adf86421f41
krhubert <krhubert@gmail.com>
2019-03-11 12:22:32 +0100

    Refactor validator funcs

--- x/xvalidator/validator.go
+++ x/xvalidator/validator.go
@@ -8,23 +8,28 @@ import (
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
+const (
+	minPort = 1
+	maxPort = 65535
+
+	portSeparator = ":"
+)
+
 // IsDomainName validates if given field is valid domain name.
 func IsDomainName(fl validator.FieldLevel) bool {
-	if fl.Field().String() == "" {
-		return true
-	}
 	return xnet.IsDomainName(fl.Field().String())
 }
 
 // IsPortMapping validates if given field is valid port mapping.
 func IsPortMapping(fl validator.FieldLevel) bool {
-	ports := strings.Split(fl.Field().String(), ":")
+	ports := strings.Split(fl.Field().String(), portSeparator)
 	if len(ports) != 1 && len(ports) != 2 {
 		return false
 	}
+
 	for _, port := range ports {
 		i, err := strconv.Atoi(port)
-		if !(err == nil && 0 < i && i <= 65535) {
+		if err != nil || !(minPort <= i && i <= maxPort) {
 			return false
 		}
 	}
