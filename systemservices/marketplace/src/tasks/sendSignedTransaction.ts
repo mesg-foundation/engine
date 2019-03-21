1f3a61d3fe9a55bc4313b8891c5e9d4e7b7cf173
Nicolas Mahe <nicolas@mahe.me>
2019-03-21 16:48:39 +0700

    Improve return of the sendSignTransaction task

--- systemservices/marketplace/src/tasks/sendSignedTransaction.ts
+++ systemservices/marketplace/src/tasks/sendSignedTransaction.ts
@@ -4,7 +4,7 @@ import Web3 from "web3"
 export default (web3: Web3) => async (inputs: TaskInputs, outputs: TaskOutputs): Promise<void> => {
   try {
     const receipt = await web3.eth.sendSignedTransaction(inputs.signedTransaction)
-    return outputs.success({ receipt })
+    return outputs.success(receipt)
   }
   catch (error) {
     console.error('error in sendSignedRawTx', error)
