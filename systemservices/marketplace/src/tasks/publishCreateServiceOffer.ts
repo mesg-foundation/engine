80d49a5e041aed8c216297ebacaf10b7cd4a3470
Nicolas Mahe <nicolas@mahe.me>
2019-03-29 17:00:29 +0700

    Add publishCreateServiceOffer task

---
+++ systemservices/marketplace/src/tasks/publishCreateServiceOffer.ts
@@ -0,0 +1,21 @@
+import { TaskInputs, TaskOutputs } from "mesg-js/lib/service"
+import Web3 from "web3"
+import { decodeLog } from "../contracts/utils";
+import { eventHandlers } from "../events";
+
+export default (
+  web3: Web3,
+) => async (inputs: TaskInputs, outputs: TaskOutputs): Promise<void> => {
+  try {
+    const receipt = await web3.eth.sendSignedTransaction(inputs.signedTransaction)
+    if (receipt.logs === undefined) throw new Error('receipt does not contain logs')
+    const eventHandler = eventHandlers['ServiceOfferCreated']
+    const decodedLog = decodeLog(web3, eventHandler.abi, receipt.logs[receipt.logs.length - 1])
+    const event = eventHandler.parse(decodedLog)
+    return outputs.success(event)
+  }
+  catch (error) {
+    console.error('error in publishCreateServiceOffer', error)
+    return outputs.error({ message: error.toString() })
+  }
+}
