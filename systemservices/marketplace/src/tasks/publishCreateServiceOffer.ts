4fe00c8a6cbea188ec361eafb560cbb56de2cdf1
Nicolas Mahe <nicolas@mahe.me>
2019-04-03 18:34:15 +0700

    Match event in logs instead of forcing the index

--- systemservices/marketplace/src/tasks/publishCreateServiceOffer.ts
+++ systemservices/marketplace/src/tasks/publishCreateServiceOffer.ts
@@ -1,17 +1,19 @@
 import { TaskInputs, TaskOutputs } from "mesg-js/lib/service"
 import Web3 from "web3"
-import { decodeLog } from "../contracts/utils";
-import { eventHandlers } from "../events";
+import { extractEventFromLogs } from "../contracts/utils";
+import { Marketplace } from "../contracts/Marketplace";
+import { serviceOfferCreated } from "../contracts/parseEvents";
+
 
 export default (
   web3: Web3,
+  marketplace: Marketplace,
 ) => async (inputs: TaskInputs, outputs: TaskOutputs): Promise<void> => {
   try {
     const receipt = await web3.eth.sendSignedTransaction(inputs.signedTransaction)
     if (receipt.logs === undefined) throw new Error('receipt does not contain logs')
-    const eventHandler = eventHandlers['ServiceOfferCreated']
-    const decodedLog = decodeLog(web3, eventHandler.abi, receipt.logs[receipt.logs.length - 1])
-    const event = eventHandler.parse(decodedLog)
+    const decodedLog = extractEventFromLogs(web3, marketplace, 'ServiceOfferCreated', receipt.logs)
+    const event = serviceOfferCreated(decodedLog)
     return outputs.success(event)
   }
   catch (error) {
