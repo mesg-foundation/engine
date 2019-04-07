d6dbd7a0fd2016b847ab480dd957a302781f7931
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-04-07 11:49:47 +1000

    remove container from grpc

--- interface/grpc/core/type.go
+++ interface/grpc/core/type.go
@@ -2,16 +2,14 @@ package core
 
 import (
 	"github.com/mesg-foundation/core/api"
-	"github.com/mesg-foundation/core/container"
 )
 
 // Server is the type to aggregate all the APIs.
 type Server struct {
-	container container.Container
-	api       *api.API
+	api *api.API
 }
 
 // NewServer creates a new Server.
-func NewServer(c container.Container, api *api.API) *Server {
-	return &Server{container: c, api: api}
+func NewServer(api *api.API) *Server {
+	return &Server{api: api}
 }
