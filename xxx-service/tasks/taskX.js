05ba84e52817a7e649edb98828c2c3fb55effbb9
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-04-09 20:59:04 +1000

    remove wrong commit

--- xxx-service/tasks/taskX.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ foo, bar }, { success, error }) => {
-  if (foo === 'hello' && bar === 'world') {
-    success({ message: 'Hello world is valid' })
-  } else {
-    error({ error: 'invalid inputs' })
-  }
-}
