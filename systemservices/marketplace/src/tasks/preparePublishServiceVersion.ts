0148581d48baa3405b92a9a485edb49546ca6aef
Nicolas Mahe <nicolas@mahe.me>
2019-04-11 14:42:19 +1000

    Add data check in publishServiceVersion

--- systemservices/marketplace/src/tasks/preparePublishServiceVersion.ts
+++ systemservices/marketplace/src/tasks/preparePublishServiceVersion.ts
@@ -1,19 +1,34 @@
 import { TaskInputs, TaskOutputs } from "mesg-js/lib/service"
 import { Marketplace } from "../contracts/Marketplace"
-import { asciiToHex, CreateTransaction, hashToHex } from "../contracts/utils";
+import { asciiToHex, CreateTransaction, computeVersionHash } from "../contracts/utils";
 import { Manifest } from "../types/manifest";
+import { getService, isServiceExist } from "../contracts/service";
 
 export default (
   marketplace: Marketplace,
   createTransaction: CreateTransaction
 ) => async (inputs: TaskInputs, outputs: TaskOutputs): Promise<void> => {
   try {
+    // check inputs
+    const sid = inputs.service.definition.sid
+    if (!(1 <= sid.length() && sid.length() <= 63)) throw new Error('Sid must have a length between 1 and 63')
+    // TODO: add check on SID is domain name (see go implementation)
+
+    if(await isServiceExist(marketplace, sid)) {
+      const service = await getService(marketplace, sid)
+      // check ownership
+      if (service.owner.toLowerCase() !== inputs.from.toLowerCase()) throw new Error(`Service's owner is different that the specified 'from'`)
+    }
+
+    // upload manifest
     const manifestHash = await uploadManifest({
       version: '1',
       service: inputs.service
     })
+
+    // create transaction
     const transactionData = marketplace.methods.publishServiceVersion(
-      asciiToHex(inputs.service.definition.sid),
+      asciiToHex(sid),
       asciiToHex(manifestHash),
       asciiToHex('ipfs')
     ).encodeABI()
