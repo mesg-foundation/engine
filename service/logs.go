3dbe935ccf29ab5a243309aab27b26e00dc70d84
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-04-17 12:38:20 +0300

    service: simplify Logs() & improve docs
    * moved Dependency.Logs() logic into Service.Logs().
    * removed unneded TestDependencyLogs() test.
    * simplify logs tests.

--- service/logs.go
+++ service/logs.go
@@ -3,8 +3,10 @@ package service
 import (
 	"io"
 
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/mesg-foundation/core/container"
 	"github.com/mesg-foundation/core/x/xstrings"
+	"github.com/sirupsen/logrus"
 )
 
 // Log holds log streams of dependency.
@@ -14,35 +16,42 @@ type Log struct {
 	Error      io.ReadCloser
 }
 
-// Logs gives service's logs and applies dependencies filter to filter logs.
-// if dependencies has a length of zero all dependency logs will be provided.
+// Logs gives service's logs streams. when dependencies filter is not provided, it'll give
+// logs for all dependencies otherwise it'll only give logs for specified dependencies.
+// note that, service itself is also a dependency defined with special "service" key.
+// in order to get service's own logs, "service" key must be included to dependencies filter.
 func (s *Service) Logs(c container.Container, dependencies ...string) ([]*Log, error) {
 	var (
 		logs       []*Log
 		isNoFilter = len(dependencies) == 0
 	)
-	addLog := func(dep *Dependency, name string) error {
-		if isNoFilter || xstrings.SliceContains(dependencies, name) {
-			rstd, rerr, err := dep.Logs(c, s.namespace())
+	for _, d := range append(s.Dependencies, s.Configuration) {
+		// Service.Configuration can be nil so, here is a check for it.
+		if d == nil {
+			continue
+		}
+		if isNoFilter || xstrings.SliceContains(dependencies, d.Key) {
+			var r io.ReadCloser
+			r, err := c.ServiceLogs(d.namespace(s.namespace()))
 			if err != nil {
-				return err
+				return nil, err
+			}
+			rstd, sw := io.Pipe()
+			rerr, ew := io.Pipe()
+			go func(dstout, dsterr io.Writer, r io.ReadCloser) {
+				if _, err := stdcopy.StdCopy(dstout, dsterr, r); err != nil {
+					r.Close()
+					logrus.Errorln(err)
+				}
+			}(sw, ew, r)
+			if err != nil {
+				return nil, err
 			}
 			logs = append(logs, &Log{
-				Dependency: name,
+				Dependency: d.Key,
 				Standard:   rstd,
 				Error:      rerr,
 			})
-		}
-		return nil
-	}
-	if s.Configuration != nil {
-		if err := addLog(s.Configuration, MainServiceKey); err != nil {
-			return nil, err
-		}
-	}
-	for _, dep := range s.Dependencies {
-		if err := addLog(dep, dep.Key); err != nil {
-			return nil, err
 		}
 	}
 	return logs, nil
