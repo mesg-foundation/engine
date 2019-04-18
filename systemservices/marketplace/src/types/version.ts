6280c49cc5f190716509bfdc3db6e2f89a8cacd0
Nicolas Mahe <nicolas@mahe.me>
2019-04-18 16:52:35 +0700

    Manifests are not automatically downloaded alongside the versions.

--- systemservices/marketplace/src/types/version.ts
+++ systemservices/marketplace/src/types/version.ts
@@ -1,9 +1,6 @@
-import { Manifest } from "./manifest";
-
 export interface Version {
   versionHash: string;
   manifest: string;
   manifestProtocol: string;
-  manifestData: Manifest|null;
   createTime: Date;
 }
