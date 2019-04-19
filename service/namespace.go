95334b3d2d805ac9bbc3ee7f368db585cb494559
krhubert <krhubert@gmail.com>
2019-04-19 13:49:15 +0200

    Change service channel to topic and merge result+task into execution

--- service/namespace.go
+++ service/namespace.go
@@ -8,9 +8,8 @@ import (
 )
 
 // NAMESPACE is the namespace used for the docker services.
-const eventChannel string = "Event"
-const taskChannel string = "Task"
-const resultChannel string = "Result"
+const eventTopic string = "Event"
+const executionTopic string = "Execution"
 
 // namespace returns the namespace of the service.
 func (service *Service) namespace() []string {
@@ -23,26 +22,12 @@ func (d *Dependency) namespace(serviceNamespace []string) []string {
 	return append(serviceNamespace, d.Key)
 }
 
-// EventSubscriptionChannel returns the channel to listen for events from this service.
-func (service *Service) EventSubscriptionChannel() string {
-	return hash.Calculate(append(
-		service.namespace(),
-		eventChannel,
-	))
+// EventSubTopic returns the topic to listen for events from this service.
+func (service *Service) EventSubTopic() string {
+	return hash.Calculate(append(service.namespace(), eventTopic))
 }
 
-// TaskSubscriptionChannel returns the channel to listen for tasks from this service.
-func (service *Service) TaskSubscriptionChannel() string {
-	return hash.Calculate(append(
-		service.namespace(),
-		taskChannel,
-	))
-}
-
-// ResultSubscriptionChannel returns the channel to listen for tasks from this service.
-func (service *Service) ResultSubscriptionChannel() string {
-	return hash.Calculate(append(
-		service.namespace(),
-		resultChannel,
-	))
+// ExecutionSubTopic returns the topic to listen for executions from this service.
+func (service *Service) ExecutionSubTopic() string {
+	return hash.Calculate(append(service.namespace(), executionTopic))
 }
