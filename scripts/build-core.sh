fcc152e216e289cce86626affcd04b8591f32686
Nicolas Mahe <nicolas@mahe.me>
2019-05-03 16:11:03 +0700

    Add prod marketplace env data

--- scripts/build-core.sh
+++ scripts/build-core.sh
@@ -3,9 +3,9 @@
 MESG_SERVICE_SERVER=ipfs.app.mesg.com
 
 LDFLAGS="-X 'github.com/mesg-foundation/core/version.Version=$version'"
-LDFLAGS+=" -X 'github.com/mesg-foundation/core/config.EnvMarketplaceEndpoint='"
-LDFLAGS+=" -X 'github.com/mesg-foundation/core/config.EnvMarketplaceAddress='"
-LDFLAGS+=" -X 'github.com/mesg-foundation/core/config.EnvMarketplaceToken='"
+LDFLAGS+=" -X 'github.com/mesg-foundation/core/config.EnvMarketplaceEndpoint=https://mainnet.infura.io/v3/7690bc6d35e140d2be4e771a1237f636'"
+LDFLAGS+=" -X 'github.com/mesg-foundation/core/config.EnvMarketplaceAddress=0x0C6e8d0eC4770fDa8A56CD912392d2ff14822952'"
+LDFLAGS+=" -X 'github.com/mesg-foundation/core/config.EnvMarketplaceToken=0x420167d87d35c3a249b32ef6225872fbd9ab85d2'"
 
 # upload system services
 for s in systemservices/* ; do
