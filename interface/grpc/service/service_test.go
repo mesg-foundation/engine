f4bf864918bfa88e6578133fd6d24523d619e471
krhubert <krhubert@gmail.com>
2019-05-16 22:07:08 +0200

    Remove output non existing tests from grpc/service package

--- interface/grpc/service/service_test.go
+++ interface/grpc/service/service_test.go
@@ -240,43 +240,6 @@ func TestSubmitWithInvalidID(t *testing.T) {
 	require.Error(t, err)
 }
 
-func TestSubmitWithNonExistentOutputKey(t *testing.T) {
-	var (
-		taskKey  = "call"
-		taskData = map[string]interface{}{
-			"url":     "https://mesg.com",
-			"data":    map[string]interface{}{},
-			"headers": map[string]interface{}{},
-		}
-		outputKey      = "nonExistent"
-		outputData     = `{"foo":{}}`
-		server, closer = newServer(t)
-	)
-	defer closer()
-
-	s, validationErr, err := server.api.DeployService(serviceTar(t, taskServicePath), nil)
-	require.Zero(t, validationErr)
-	require.NoError(t, err)
-	defer server.api.DeleteService(s.Hash, false)
-
-	require.NoError(t, server.api.StartService(s.Hash))
-	defer server.api.StopService(s.Hash)
-
-	executionID, err := server.api.ExecuteTask(s.Hash, taskKey, taskData, nil)
-	require.NoError(t, err)
-
-	_, err = server.SubmitResult(context.Background(), &serviceapi.SubmitResultRequest{
-		ExecutionID: executionID,
-		OutputKey:   outputKey,
-		OutputData:  outputData,
-	})
-	require.Error(t, err)
-	notFoundErr, ok := err.(*service.TaskOutputNotFoundError)
-	require.True(t, ok)
-	require.Equal(t, outputKey, notFoundErr.TaskOutputKey)
-	require.Equal(t, s.Name, notFoundErr.ServiceName)
-}
-
 func TestSubmitWithInvalidTaskOutputs(t *testing.T) {
 	var (
 		taskKey  = "call"
