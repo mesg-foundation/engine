2dcc0fd4db2a11cf74d03dcad58fbb2350c87d62
Nicolas Mahe <nicolas@mahe.me>
2019-05-29 16:51:49 +0700

    Remove scripts/generate-assets.sh only use for the service gen-doc command

--- scripts/generate-assets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# make sure script is running inside mesg/tools container.
-source $(dirname $0)/require-mesg-tools.sh
-
-go-bindata -o commands/provider/assets/readme_template.go -pkg assets ./commands/provider/assets/readme_template.md
