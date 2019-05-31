712635a8f309412f1549eed1839e66a43eb2fd11
krhubert <krhubert@gmail.com>
2019-05-31 12:15:25 +0200

    Add exec proto files to build

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -6,6 +6,8 @@ source $(dirname $0)/require-mesg-tools.sh
 PROJECT=/project
 GRPC=$PROJECT/protobuf
 GRPC_PLUGIN="--go_out=plugins=grpc,paths=source_relative:."
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/execution.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/service.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/coreapi/api.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/api.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/serviceapi/api.proto
