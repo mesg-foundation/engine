74c7f372b0705d56b204b784ca7cb909a7b037d9
Anthony ESTEBE <anthony@mesg.com>
2019-06-07 17:39:06 +0700

    add start instance

---
+++ sdk/instance/stop.go
@@ -0,0 +1,10 @@
+package instancesdk
+
+import (
+	"github.com/mesg-foundation/core/instance"
+)
+
+// Stop stops an instance.
+func (i *Instance) stop(inst *instance.Instance) error {
+	return nil
+}
