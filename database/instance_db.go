9e6a286b7dd060eea311ed44fcbfe689f7cd8aac
Anthony ESTEBE <anthony@mesg.com>
2019-06-07 12:14:35 +0700

    go fmt

--- database/instance_db.go
+++ database/instance_db.go
@@ -1,30 +1,30 @@
 package database
 
- import (
+import (
 	"encoding/json"
 
- 	"github.com/mesg-foundation/core/instance"
+	"github.com/mesg-foundation/core/instance"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
- // InstanceDB describes the API of Instance database.
+// InstanceDB describes the API of Instance database.
 type InstanceDB interface {
 	// Get retrives instance by instance hash.
 	Get(hash string) (*instance.Instance, error)
 
- 	// Save saves instance to database.
+	// Save saves instance to database.
 	Save(i *instance.Instance) error
 
- 	// Close closes underlying database connection.
+	// Close closes underlying database connection.
 	Close() error
 }
 
- // LevelDBInstanceDB is a database for storing services' instances.
+// LevelDBInstanceDB is a database for storing services' instances.
 type LevelDBInstanceDB struct {
 	db *leveldb.DB
 }
 
- // NewInstanceDB returns the database which is located under given path.
+// NewInstanceDB returns the database which is located under given path.
 func NewInstanceDB(path string) (*LevelDBInstanceDB, error) {
 	db, err := leveldb.OpenFile(path, nil)
 	if err != nil {
@@ -33,12 +33,12 @@ func NewInstanceDB(path string) (*LevelDBInstanceDB, error) {
 	return &LevelDBInstanceDB{db: db}, nil
 }
 
- // marshal returns the byte slice from service.
+// marshal returns the byte slice from service.
 func (d *LevelDBInstanceDB) marshal(i *instance.Instance) ([]byte, error) {
 	return json.Marshal(i)
 }
 
- // unmarshal returns the service from byte slice.
+// unmarshal returns the service from byte slice.
 func (d *LevelDBInstanceDB) unmarshal(id string, value []byte) (*instance.Instance, error) {
 	var s instance.Instance
 	if err := json.Unmarshal(value, &s); err != nil {
@@ -47,7 +47,7 @@ func (d *LevelDBInstanceDB) unmarshal(id string, value []byte) (*instance.Instan
 	return &s, nil
 }
 
- // Get retrives instance by instance hash.
+// Get retrives instance by instance hash.
 func (d *LevelDBInstanceDB) Get(hash string) (*instance.Instance, error) {
 	tx, err := d.db.OpenTransaction()
 	if err != nil {
@@ -69,36 +69,36 @@ func (d *LevelDBInstanceDB) Get(hash string) (*instance.Instance, error) {
 	return i, tx.Commit()
 }
 
- // Save saves instance to database.
+// Save saves instance to database.
 func (d *LevelDBInstanceDB) Save(i *instance.Instance) error {
 	// check service
 	if i.Hash == "" {
 		return errCannotSaveWithoutHash
 	}
 
- 	// open database transaction
+	// open database transaction
 	tx, err := d.db.OpenTransaction()
 	if err != nil {
 		return err
 	}
 
- 	// encode service
+	// encode service
 	b, err := d.marshal(i)
 	if err != nil {
 		tx.Discard()
 		return err
 	}
 
- 	// save instance with hash.
+	// save instance with hash.
 	if err := tx.Put([]byte(i.Hash), b, nil); err != nil {
 		tx.Discard()
 		return err
 	}
 
- 	return tx.Commit()
+	return tx.Commit()
 }
 
- // Close closes database.
+// Close closes database.
 func (d *LevelDBInstanceDB) Close() error {
 	return d.db.Close()
 }
