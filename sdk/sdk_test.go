64446f7c22aee6650b7de321ec02d02b91779880
Anthony ESTEBE <anthony@mesg.com>
2019-06-10 22:54:25 +0700

    fix test due to wrong rebase

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -17,13 +17,15 @@ import (
 )
 
 const (
-	servicedbname = "service.db.test"
-	execdbname    = "exec.db.test"
+	servicedbname  = "service.db.test"
+	instancedbname = "instance.db.test"
+	execdbname     = "exec.db.test"
 )
 
 type apiTesting struct {
 	*testing.T
 	serviceDB     *database.LevelDBServiceDB
+	instanceDB    *database.LevelDBInstanceDB
 	executionDB   *database.LevelDBExecutionDB
 	containerMock *mocks.Container
 }
@@ -44,14 +46,18 @@ func newTesting(t *testing.T) (*SDK, *apiTesting) {
 	db, err := database.NewServiceDB(servicedbname)
 	require.NoError(t, err)
 
+	instanceDB, err := database.NewInstanceDB(instancedbname)
+	require.NoError(t, err)
+
 	execDB, err := database.NewExecutionDB(execdbname)
 	require.NoError(t, err)
 
-	a := New(m, containerMock, db, nil, execDB)
+	a := New(m, containerMock, db, instanceDB, execDB)
 
 	return a, &apiTesting{
 		T:             t,
 		serviceDB:     db,
+		instanceDB:    instanceDB,
 		executionDB:   execDB,
 		containerMock: containerMock,
 	}
