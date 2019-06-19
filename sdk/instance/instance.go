b37c547039ff222906637a47283934701175278d
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-06-19 11:40:30 +0000

    sdk/instancesdk: directly pass env vars to start()
    instead keeping it in Instance type since it can cause to security issues when instance db is distributed to peers(network feature).

--- sdk/instance/instance.go
+++ sdk/instance/instance.go
@@ -65,7 +65,7 @@ func (i *Instance) Create(id string, env []string) (*instance.Instance, error) {
 		return nil, err
 	}
 
-	// calculate env vars by overwriting user defined one's with defaults.
+	// calculate the final env vars by overwriting user defined one's with defaults.
 	instanceEnv := xos.EnvMergeMaps(xos.EnvSliceToMap(srv.Configuration.Env), xos.EnvSliceToMap(env))
 
 	// calculate instance's hash.
@@ -87,13 +87,12 @@ func (i *Instance) Create(id string, env []string) (*instance.Instance, error) {
 	o := &instance.Instance{
 		Hash:        instanceHash,
 		ServiceHash: srv.Hash,
-		Env:         xos.EnvMapToSlice(instanceEnv),
 	}
 	if err := i.instanceDB.Save(o); err != nil {
 		return nil, err
 	}
 
-	_, err = i.start(o)
+	_, err = i.start(o, xos.EnvMapToSlice(instanceEnv))
 	return o, err
 }
 
