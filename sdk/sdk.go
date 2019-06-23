fc40d88ba9efb89452e8867a35258af6f37b2ea4
Anthony ESTEBE <anthony@mesg.com>
2019-06-23 21:56:34 +0700

    remove unused sdk functions

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -4,12 +4,10 @@ import (
 	"github.com/cskr/pubsub"
 	"github.com/mesg-foundation/core/container"
 	"github.com/mesg-foundation/core/database"
-	"github.com/mesg-foundation/core/hash"
 	eventsdk "github.com/mesg-foundation/core/sdk/event"
 	executionsdk "github.com/mesg-foundation/core/sdk/execution"
 	instancesdk "github.com/mesg-foundation/core/sdk/instance"
 	servicesdk "github.com/mesg-foundation/core/sdk/service"
-	"github.com/mesg-foundation/core/service"
 	"github.com/mesg-foundation/core/service/manager"
 )
 
@@ -42,60 +40,4 @@ func New(m manager.Manager, c container.Container, db database.ServiceDB, instan
 		db:        db,
 		execDB:    execDB,
 	}
-}
-
-// GetService returns service serviceID.
-func (sdk *SDK) GetService(serviceHash hash.Hash) (*service.Service, error) {
-	return sdk.db.Get(serviceHash)
-}
-
-// ListServices returns all services.
-func (sdk *SDK) ListServices() ([]*service.Service, error) {
-	return sdk.db.All()
-}
-
-// Status returns the status of a service
-func (sdk *SDK) Status(service *service.Service) (service.StatusType, error) {
-	return sdk.m.Status(service)
-}
-
-// StartService starts service serviceID.
-func (sdk *SDK) StartService(serviceHash hash.Hash) error {
-	s, err := sdk.db.Get(serviceHash)
-	if err != nil {
-		return err
-	}
-	_, err = sdk.m.Start(s)
-	return err
-}
-
-// StopService stops service serviceID.
-func (sdk *SDK) StopService(serviceHash hash.Hash) error {
-	s, err := sdk.db.Get(serviceHash)
-	if err != nil {
-		return err
-	}
-	return sdk.m.Stop(s)
-}
-
-// DeleteService stops and deletes service serviceID.
-// when deleteData is enabled, any persistent data that belongs to
-// the service and to its dependencies also will be deleted.
-func (sdk *SDK) DeleteService(serviceHash hash.Hash, deleteData bool) error {
-	s, err := sdk.db.Get(serviceHash)
-	if err != nil {
-		return err
-	}
-	if err := sdk.m.Stop(s); err != nil {
-		return err
-	}
-	// delete volumes first before the service. this way if
-	// deleting volumes fails, process can be retried by the user again
-	// because service still will be in the db.
-	if deleteData {
-		if err := sdk.m.Delete(s); err != nil {
-			return err
-		}
-	}
-	return sdk.db.Delete(serviceHash)
 }
