62fd1feffad085054c6f72fbbceda42bbe1455b4
Anthony ESTEBE <anthony@mesg.com>
2019-06-24 00:40:22 +0700

    remove importer package

--- service/importer/errors.go
+++
@@ -1,8 +0,0 @@
-package importer
-
-// ValidationError is the error type for the Validation of service.
-type ValidationError struct{}
-
-func (v *ValidationError) Error() string {
-	return "Service is not valid"
-}
