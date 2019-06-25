587a73fce61a49867a8bdea5abd51bac27c13a48
krhubert <krhubert@gmail.com>
2019-06-25 07:43:40 +0200

    Move dirhash to hash package

--- sdk/service/service.go
+++ sdk/service/service.go
@@ -11,8 +11,8 @@ import (
 	"github.com/mesg-foundation/core/container"
 	"github.com/mesg-foundation/core/database"
 	"github.com/mesg-foundation/core/hash"
+	"github.com/mesg-foundation/core/hash/dirhash"
 	"github.com/mesg-foundation/core/service"
-	"github.com/mesg-foundation/core/utils/dirhash"
 )
 
 // Service exposes service APIs of MESG.
