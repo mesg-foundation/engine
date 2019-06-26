50bfaa494532a7a151892d9b1a07ee23bdc7a245
krhubert <krhubert@gmail.com>
2019-06-26 19:00:40 +0200

    Add hash tag to event

--- event/event.go
+++ event/event.go
@@ -6,10 +6,10 @@ import (
 
 // Event stores all informations about Events.
 type Event struct {
-	Hash         hash.Hash
-	InstanceHash hash.Hash
-	Key          string
-	Data         map[string]interface{}
+	Hash         hash.Hash              `hash:"-"`
+	InstanceHash hash.Hash              `hash:"name:instanceHash"`
+	Key          string                 `hash:"name:key"`
+	Data         map[string]interface{} `hash:"name:data"`
 }
 
 // Create creates an event eventKey with eventData for service s.
