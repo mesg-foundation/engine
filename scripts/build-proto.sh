99dd099a019484d504575df1819fe783556a35db
krhubert <krhubert@gmail.com>
2019-06-26 13:29:56 +0200

    Create event proto definition and api

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -6,11 +6,16 @@ source $(dirname $0)/require-mesg-tools.sh
 PROJECT=/project
 GRPC=$PROJECT/protobuf
 GRPC_PLUGIN="--go_out=plugins=grpc,paths=source_relative:."
+
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/event.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/execution.proto
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/service.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/instance.proto
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/service.proto
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/instance.proto
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/coreapi/api.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/service.proto
+
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/event.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/execution.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/instance.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/service.proto
+
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/coreapi/api.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/serviceapi/api.proto
