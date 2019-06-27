5e37be33e18ab7dddc829ea1bd498deda5b9c796
krhubert <krhubert@gmail.com>
2019-06-27 13:27:08 +0200

    Simplfiy grpc server api

--- server/grpc/server_test.go
+++ server/grpc/server_test.go
@@ -7,34 +7,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	waitForServe = 500 * time.Millisecond
-)
-
 func TestServerServe(t *testing.T) {
-	s := New("localhost:50052", nil)
+	s := New(nil)
 	go func() {
-		time.Sleep(waitForServe)
+		time.Sleep(500 * time.Millisecond)
 		s.Close()
 	}()
-	err := s.Serve()
-	require.NoError(t, err)
-}
-
-func TestServerServeNoAddress(t *testing.T) {
-	s := Server{}
-	go func() {
-		time.Sleep(waitForServe)
-		s.Close()
-	}()
-	err := s.Serve()
-	require.Error(t, err)
-}
-
-func TestServerListenAfterClose(t *testing.T) {
-	s := New("localhost:50052", nil)
-	go s.Serve()
-	time.Sleep(waitForServe)
-	s.Close()
-	require.Error(t, s.Serve())
+	require.NoError(t, s.Serve("localhost:50052"))
 }
