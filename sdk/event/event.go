4ca7fa34267983ba2f94fc0b12c8e02fc1f59909
krhubert <krhubert@gmail.com>
2019-06-27 13:17:24 +0200

    Rename event Emit to Create

--- sdk/event/event.go
+++ sdk/event/event.go
@@ -3,7 +3,6 @@ package eventsdk
 import (
 	"github.com/cskr/pubsub"
 	"github.com/mesg-foundation/core/event"
-	"github.com/mesg-foundation/core/hash"
 	instancesdk "github.com/mesg-foundation/core/sdk/instance"
 	servicesdk "github.com/mesg-foundation/core/sdk/service"
 )
@@ -11,7 +10,6 @@ import (
 const (
 	// streamTopic is topic used to broadcast events.
 	streamTopic = "event-stream"
-	topic       = "Event"
 )
 
 // Event exposes event APIs of MESG.
@@ -31,25 +29,23 @@ func New(ps *pubsub.PubSub, service *servicesdk.Service, instance *instancesdk.I
 }
 
 // Emit emits a MESG event eventKey with eventData for service token.
-func (e *Event) Emit(instanceHash hash.Hash, eventKey string, eventData map[string]interface{}) (*event.Event, error) {
-	instance, err := e.instance.Get(instanceHash)
+func (e *Event) Create(event *event.Event) error {
+	instance, err := e.instance.Get(event.InstanceHash)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	service, err := e.service.Get(instance.ServiceHash)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	if err := service.RequireEventData(eventKey, eventData); err != nil {
-		return nil, err
+	if err := service.RequireEventData(event.Key, event.Data); err != nil {
+		return err
 	}
 
-	event := event.Create(instanceHash, eventKey, eventData)
 	go e.ps.Pub(event, streamTopic)
-	go e.ps.Pub(event, subTopic(instanceHash))
-	return event, nil
+	return nil
 }
 
 // GetStream broadcasts all events.
@@ -57,27 +53,4 @@ func (e *Event) GetStream(f *Filter) *Listener {
 	l := NewListener(e.ps, streamTopic, f)
 	go l.Listen()
 	return l
-}
-
-// Listen listens events matches with eventFilter on serviceID.
-func (e *Event) Listen(serviceHash hash.Hash, f *Filter) (*Listener, error) {
-	s, err := e.service.Get(serviceHash)
-	if err != nil {
-		return nil, err
-	}
-
-	if f.HasKey() {
-		if _, err := s.GetEvent(f.Key); err != nil {
-			return nil, err
-		}
-	}
-
-	l := NewListener(e.ps, subTopic(s.Hash), f)
-	go l.Listen()
-	return l, nil
-}
-
-// subTopic returns the topic to listen for events from this service.
-func subTopic(serviceHash hash.Hash) string {
-	return serviceHash.String() + "." + topic
 }
