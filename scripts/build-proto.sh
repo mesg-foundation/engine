f3fee40ea7304d9d10164712960734a16a34fada
krhubert <krhubert@gmail.com>
2019-06-27 13:56:15 +0200

    Rename definition to types in protobuf

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -7,10 +7,10 @@ PROJECT=/project
 GRPC=$PROJECT/protobuf
 GRPC_PLUGIN="--go_out=plugins=grpc,paths=source_relative:."
 
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/event.proto
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/execution.proto
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/instance.proto
-protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/definition/service.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/types/event.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/types/execution.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/types/instance.proto
+protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/types/service.proto
 
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/event.proto
 protoc $GRPC_PLUGIN --proto_path=$PROJECT $GRPC/api/execution.proto
