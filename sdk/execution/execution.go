d1528b8a7f60eff94ecc58436ae4471612ec0534
Anthony ESTEBE <anthony@mesg.com>
2019-06-28 16:06:19 +0700

    fix confusion with service and instance hash

--- sdk/execution/execution.go
+++ sdk/execution/execution.go
@@ -111,7 +111,12 @@ func (e *Execution) validateExecutionOutput(instanceHash hash.Hash, taskKey stri
 		return nil, fmt.Errorf("invalid output: %s", err)
 	}
 
-	s, err := e.service.Get(instanceHash)
+	i, err := e.instance.Get(instanceHash)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := e.service.Get(i.ServiceHash)
 	if err != nil {
 		return nil, err
 	}
@@ -141,7 +146,7 @@ func (e *Execution) Execute(instanceHash hash.Hash, taskKey string, inputData ma
 
 	// execute the task.
 	eventID := uuid.NewV4().String()
-	exec := execution.New(s.Hash, nil, eventID, taskKey, inputData, tags)
+	exec := execution.New(instance.Hash, nil, eventID, taskKey, inputData, tags)
 	if err := exec.Execute(); err != nil {
 		return nil, err
 	}
@@ -150,7 +155,7 @@ func (e *Execution) Execute(instanceHash hash.Hash, taskKey string, inputData ma
 	}
 
 	go e.ps.Pub(exec, streamTopic)
-	go e.ps.Pub(exec, subTopic(s.Hash))
+	go e.ps.Pub(exec, subTopic(instance.Hash))
 	return exec.Hash, nil
 }
 
