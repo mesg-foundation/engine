0191c5bda3ed5fb5a75fa0d41a74b74f0fa0f0f7
Anthony <anthony@mesg.com>
2019-07-02 15:40:36 +0700

    Update sdk/instance/instance.go
    Co-Authored-By: Nicolas Mahé <nicolas@mesg.com>

--- sdk/instance/instance.go
+++ sdk/instance/instance.go
@@ -156,5 +156,5 @@ type AlreadyExistsError struct {
 }
 
 func (e *AlreadyExistsError) Error() string {
-	return fmt.Sprintf("instance already exists")
+	return fmt.Sprintf("instance %q already exists", e.Hash.ToString())
 }
