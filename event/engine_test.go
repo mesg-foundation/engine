4acf5fd9dc2e418a8d33101bdef8c6b3c85c13b5
Anthony ESTEBE <anthony@mesg.com>
2019-07-14 14:43:48 +0700

    test(event): fix tests with simplification of engine events

--- event/engine_test.go
+++ event/engine_test.go
@@ -10,8 +10,8 @@ func TestDigest(t *testing.T) {
 	e := EngineEvent(EngineAPIExecution, map[string]interface{}{
 		"foo": "bar",
 	})
-	assert.Equal(t, e.InstanceHash.String(), engineEventHash)
-	assert.Equal(t, e.Key, string(EngineAPIExecution))
+	assert.Equal(t, e.InstanceHash.String(), "")
+	assert.Equal(t, e.Key, "mesg:"+string(EngineAPIExecution))
 	assert.Equal(t, e.Data["foo"], "bar")
-	assert.Equal(t, e.Hash.String(), "5oHsDq9Njo4uRmjo6ehD3FREYuH6o6PbELn6vYahHAay")
+	assert.Equal(t, e.Hash.String(), "6UW521vQ9KbdDpCYbcKu2FGPLfK8Gsy9kfyF77pzYbYC")
 }
