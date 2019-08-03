6b7531d58eea756eb7ee95fb78baa6f8bd3f2312
Anthony ESTEBE <anthony@mesg.com>
2019-08-03 18:56:32 +0700

    expose workflow api

--- server/grpc/server.go
+++ server/grpc/server.go
@@ -67,6 +67,7 @@ func (s *Server) register() {
 	protobuf_api.RegisterExecutionServer(s.instance, api.NewExecutionServer(s.sdk))
 	protobuf_api.RegisterInstanceServer(s.instance, api.NewInstanceServer(s.sdk))
 	protobuf_api.RegisterServiceServer(s.instance, api.NewServiceServer(s.sdk))
+	protobuf_api.RegisterWorkflowServer(s.instance, api.NewWorkflowServer(s.sdk))
 
 	reflection.Register(s.instance)
 }
