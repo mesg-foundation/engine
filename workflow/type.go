a7cb266199fdb5f9527bd1a70cb32f172021bf37
Anthony ESTEBE <anthony@mesg.com>
2019-08-09 15:15:59 +0700

    update type including node and edge

--- workflow/type.go
+++ workflow/type.go
@@ -22,23 +22,32 @@ const (
 // Workflow describes a workflow of a service
 type Workflow struct {
 	Hash    hash.Hash `hash:"-" validate:"required"`
-	Trigger *Trigger  `hash:"name:1" validate:"required"`
-	Tasks   []*Task   `hash:"name:2" validate:"required"`
-	Key     string    `hash:"name:3" validate:"required"`
+	Key     string    `hash:"name:1" validate:"required"`
+	Trigger Trigger   `hash:"name:2" validate:"required"`
+	Nodes   []Node    `hash:"name:3" validate:"dive,required"`
+	Edges   []Edge    `hash:"name:4" validate:"dive,required"`
 }
 
-// Task describes the instructions for the workflow to execute a task
-type Task struct {
+// Node describes the instructions for the workflow to execute a task
+type Node struct {
+	Key          string    `hash:"name:1" validate:"required"`
 	InstanceHash hash.Hash `hash:"name:1" validate:"required"`
-	TaskKey      string    `hash:"name:2" validate:"printascii"`
+	TaskKey      string    `hash:"name:2" validate:"required,printascii"`
+}
+
+// Edge describes the instructions for the workflow to execute a task
+type Edge struct {
+	Src string `hash:"name:1"`
+	Dst string `hash:"name:2"`
 }
 
 // Trigger is an event that triggers a workflow
 type Trigger struct {
 	InstanceHash hash.Hash        `hash:"name:1" validate:"required"`
-	Key          string           `hash:"name:2" validate:"printascii"`
+	Key          string           `hash:"name:2" validate:"required,printascii"`
 	Type         TriggerType      `hash:"name:3" validate:"required"`
 	Filters      []*TriggerFilter `hash:"name:4" validate:"dive,required"`
+	InitialNode  string           `hash:"name:5"`
 }
 
 // TriggerFilter is the filter definition that can be applied to a workflow trigger
