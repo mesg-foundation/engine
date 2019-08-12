fa38e570db6409e99413bf5574806dd5b6cb6331
Nicolas Mahe <nicolas@mahe.me>
2019-08-12 11:41:08 +0700

    Add tuto managers to cosmos app

--- tendermint/app/app.go
+++ tendermint/app/app.go
@@ -1,29 +1,58 @@
 package app
 
 import (
-	"github.com/cosmos/cosmos-sdk/baseapp"
+	"encoding/json"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	dbm "github.com/tendermint/tendermint/libs/db"
+	"github.com/tendermint/tendermint/libs/log"
+	tmtypes "github.com/tendermint/tendermint/types"
+
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/cosmos/cosmos-sdk/x/auth"
-	dbm "github.com/tendermint/tendermint/libs/db"
-	"github.com/tendermint/tendermint/libs/log"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	distr "github.com/cosmos/cosmos-sdk/x/distribution"
+	"github.com/cosmos/cosmos-sdk/x/genaccounts"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/cosmos/cosmos-sdk/x/slashing"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	"github.com/cosmos/cosmos-sdk/x/supply"
+
+	"github.com/cosmos/sdk-application-tutorial/x/nameservice"
 )
 
-const appName = "mesg-app"
+const appName = "nameservice"
 
 var (
-	// ModuleBasics is BasicManager is in charge of setting up basic,
-	// non-dependant module elements, such as codec registration
-	// and genesis verification.
-	ModuleBasics = module.NewBasicManager()
+	// NewBasicManager is in charge of setting up basic module elemnets
+	ModuleBasics = module.NewBasicManager(
+		genaccounts.AppModuleBasic{},
+		genutil.AppModuleBasic{},
+		auth.AppModuleBasic{},
+		bank.AppModuleBasic{},
+		staking.AppModuleBasic{},
+		distr.AppModuleBasic{},
+		params.AppModuleBasic{},
+		slashing.AppModuleBasic{},
+		supply.AppModuleBasic{},
 
-	mainStoreKey = sdk.NewKVStoreKey(baseapp.MainStoreKey)
+		nameservice.AppModule{},
+	)
+	// account permissions
+	maccPerms = map[string][]string{
+		auth.FeeCollectorName:     nil,
+		distr.ModuleName:          nil,
+		staking.BondedPoolName:    []string{supply.Burner, supply.Staking},
+		staking.NotBondedPoolName: []string{supply.Burner, supply.Staking},
+	}
 )
 
-// custom tx codec
+// MakeCodec generates the necessary codecs for Amino
 func MakeCodec() *codec.Codec {
 	var cdc = codec.New()
 	ModuleBasics.RegisterCodec(cdc)
@@ -32,31 +61,261 @@ func MakeCodec() *codec.Codec {
 	return cdc
 }
 
-// Extended ABCI application
-type App struct {
+type nameServiceApp struct {
 	*bam.BaseApp
 	cdc *codec.Codec
 
-	// the module manager
+	// Keys to access the substores
+	keyMain     *sdk.KVStoreKey
+	keyAccount  *sdk.KVStoreKey
+	keySupply   *sdk.KVStoreKey
+	keyStaking  *sdk.KVStoreKey
+	tkeyStaking *sdk.TransientStoreKey
+	keyDistr    *sdk.KVStoreKey
+	tkeyDistr   *sdk.TransientStoreKey
+	keyNS       *sdk.KVStoreKey
+	keyParams   *sdk.KVStoreKey
+	tkeyParams  *sdk.TransientStoreKey
+	keySlashing *sdk.KVStoreKey
+
+	// Keepers
+	accountKeeper  auth.AccountKeeper
+	bankKeeper     bank.Keeper
+	stakingKeeper  staking.Keeper
+	slashingKeeper slashing.Keeper
+	distrKeeper    distr.Keeper
+	supplyKeeper   supply.Keeper
+	paramsKeeper   params.Keeper
+	nsKeeper       nameservice.Keeper
+
+	// Module Manager
 	mm *module.Manager
 }
 
-// New returns a reference to an initialized App.
-func New(logger log.Logger, db dbm.DB) (*App, *codec.Codec) {
+// NewNameServiceApp is a constructor function for nameServiceApp
+func NewNameServiceApp(logger log.Logger, db dbm.DB) *nameServiceApp {
+
+	// First define the top level codec that will be shared by the different modules
 	cdc := MakeCodec()
 
+	// BaseApp handles interactions with Tendermint through the ABCI protocol
 	bApp := bam.NewBaseApp(appName, logger, db, auth.DefaultTxDecoder(cdc))
-	bApp.SetAppVersion(version.Version)
 
-	var app = &App{
+	// Here you initialize your application with the store keys it requires
+	var app = &nameServiceApp{
 		BaseApp: bApp,
 		cdc:     cdc,
+
+		keyMain:     sdk.NewKVStoreKey(bam.MainStoreKey),
+		keyAccount:  sdk.NewKVStoreKey(auth.StoreKey),
+		keySupply:   sdk.NewKVStoreKey(supply.StoreKey),
+		keyStaking:  sdk.NewKVStoreKey(staking.StoreKey),
+		tkeyStaking: sdk.NewTransientStoreKey(staking.TStoreKey),
+		keyDistr:    sdk.NewKVStoreKey(distr.StoreKey),
+		tkeyDistr:   sdk.NewTransientStoreKey(distr.TStoreKey),
+		keyNS:       sdk.NewKVStoreKey(nameservice.StoreKey),
+		keyParams:   sdk.NewKVStoreKey(params.StoreKey),
+		tkeyParams:  sdk.NewTransientStoreKey(params.TStoreKey),
+		keySlashing: sdk.NewKVStoreKey(slashing.StoreKey),
 	}
 
-	app.mm = module.NewManager()
+	// The ParamsKeeper handles parameter storage for the application
+	app.paramsKeeper = params.NewKeeper(app.cdc, app.keyParams, app.tkeyParams, params.DefaultCodespace)
+	// Set specific supspaces
+	authSubspace := app.paramsKeeper.Subspace(auth.DefaultParamspace)
+	bankSupspace := app.paramsKeeper.Subspace(bank.DefaultParamspace)
+	stakingSubspace := app.paramsKeeper.Subspace(staking.DefaultParamspace)
+	distrSubspace := app.paramsKeeper.Subspace(distr.DefaultParamspace)
+	slashingSubspace := app.paramsKeeper.Subspace(slashing.DefaultParamspace)
 
-	app.MountStores(mainStoreKey)
-	app.LoadLatestVersion(mainStoreKey)
+	// The AccountKeeper handles address -> account lookups
+	app.accountKeeper = auth.NewAccountKeeper(
+		app.cdc,
+		app.keyAccount,
+		authSubspace,
+		auth.ProtoBaseAccount,
+	)
 
-	return app, cdc
+	// The BankKeeper allows you perform sdk.Coins interactions
+	app.bankKeeper = bank.NewBaseKeeper(
+		app.accountKeeper,
+		bankSupspace,
+		bank.DefaultCodespace,
+	)
+
+	// The SupplyKeeper collects transaction fees and renders them to the fee distribution module
+	app.supplyKeeper = supply.NewKeeper(
+		app.cdc,
+		app.keySupply,
+		app.accountKeeper,
+		app.bankKeeper,
+		supply.DefaultCodespace,
+		maccPerms)
+
+	// The staking keeper
+	stakingKeeper := staking.NewKeeper(
+		app.cdc,
+		app.keyStaking,
+		app.tkeyStaking,
+		app.supplyKeeper,
+		stakingSubspace,
+		staking.DefaultCodespace,
+	)
+
+	app.distrKeeper = distr.NewKeeper(
+		app.cdc,
+		app.keyDistr,
+		distrSubspace,
+		&stakingKeeper,
+		app.supplyKeeper,
+		distr.DefaultCodespace,
+		auth.FeeCollectorName,
+	)
+
+	app.slashingKeeper = slashing.NewKeeper(
+		app.cdc,
+		app.keySlashing,
+		&stakingKeeper,
+		slashingSubspace,
+		slashing.DefaultCodespace,
+	)
+
+	// register the staking hooks
+	// NOTE: stakingKeeper above is passed by reference, so that it will contain these hooks
+	app.stakingKeeper = *stakingKeeper.SetHooks(
+		staking.NewMultiStakingHooks(
+			app.distrKeeper.Hooks(),
+			app.slashingKeeper.Hooks()),
+	)
+
+	// The NameserviceKeeper is the Keeper from the module for this tutorial
+	// It handles interactions with the namestore
+	app.nsKeeper = nameservice.NewKeeper(
+		app.bankKeeper,
+		app.keyNS,
+		app.cdc,
+	)
+
+	app.mm = module.NewManager(
+		genaccounts.NewAppModule(app.accountKeeper),
+		genutil.NewAppModule(app.accountKeeper, app.stakingKeeper, app.BaseApp.DeliverTx),
+		auth.NewAppModule(app.accountKeeper),
+		bank.NewAppModule(app.bankKeeper, app.accountKeeper),
+		nameservice.NewAppModule(app.nsKeeper, app.bankKeeper),
+		supply.NewAppModule(app.supplyKeeper, app.accountKeeper),
+		distr.NewAppModule(app.distrKeeper, app.supplyKeeper),
+		slashing.NewAppModule(app.slashingKeeper, app.stakingKeeper),
+		staking.NewAppModule(app.stakingKeeper, app.distrKeeper, app.accountKeeper, app.supplyKeeper),
+	)
+
+	app.mm.SetOrderBeginBlockers(distr.ModuleName, slashing.ModuleName)
+	app.mm.SetOrderEndBlockers(staking.ModuleName)
+
+	// Sets the order of Genesis - Order matters, genutil is to always come last
+	app.mm.SetOrderInitGenesis(
+		genaccounts.ModuleName,
+		distr.ModuleName,
+		staking.ModuleName,
+		auth.ModuleName,
+		bank.ModuleName,
+		slashing.ModuleName,
+		nameservice.ModuleName,
+		genutil.ModuleName,
+	)
+
+	// register all module routes and module queriers
+	app.mm.RegisterRoutes(app.Router(), app.QueryRouter())
+
+	// The initChainer handles translating the genesis.json file into initial state for the network
+	app.SetInitChainer(app.InitChainer)
+	app.SetBeginBlocker(app.BeginBlocker)
+	app.SetEndBlocker(app.EndBlocker)
+
+	// The AnteHandler handles signature verification and transaction pre-processing
+	app.SetAnteHandler(
+		auth.NewAnteHandler(
+			app.accountKeeper,
+			app.supplyKeeper,
+			auth.DefaultSigVerificationGasConsumer,
+		),
+	)
+
+	app.MountStores(
+		app.keyMain,
+		app.keyAccount,
+		app.keySupply,
+		app.keyStaking,
+		app.tkeyStaking,
+		app.keyDistr,
+		app.tkeyDistr,
+		app.keySlashing,
+		app.keyNS,
+		app.keyParams,
+		app.tkeyParams,
+	)
+
+	err := app.LoadLatestVersion(app.keyMain)
+	if err != nil {
+		cmn.Exit(err.Error())
+	}
+
+	return app
+}
+
+// GenesisState represents chain state at the start of the chain. Any initial state (account balances) are stored here.
+type GenesisState map[string]json.RawMessage
+
+func NewDefaultGenesisState() GenesisState {
+	return ModuleBasics.DefaultGenesis()
+}
+
+func (app *nameServiceApp) InitChainer(ctx sdk.Context, req abci.RequestInitChain) abci.ResponseInitChain {
+	var genesisState GenesisState
+
+	err := app.cdc.UnmarshalJSON(req.AppStateBytes, &genesisState)
+	if err != nil {
+		panic(err)
+	}
+
+	return app.mm.InitGenesis(ctx, genesisState)
+}
+
+func (app *nameServiceApp) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) abci.ResponseBeginBlock {
+	return app.mm.BeginBlock(ctx, req)
+}
+func (app *nameServiceApp) EndBlocker(ctx sdk.Context, req abci.RequestEndBlock) abci.ResponseEndBlock {
+	return app.mm.EndBlock(ctx, req)
+}
+func (app *nameServiceApp) LoadHeight(height int64) error {
+	return app.LoadVersion(height, app.keyMain)
+}
+
+//_________________________________________________________
+
+func (app *nameServiceApp) ExportAppStateAndValidators(forZeroHeight bool, jailWhiteList []string,
+) (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
+
+	// as if they could withdraw from the start of the next block
+	ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight()})
+
+	genState := app.mm.ExportGenesis(ctx)
+	appState, err = codec.MarshalJSONIndent(app.cdc, genState)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	validators = staking.WriteValidators(ctx, app.stakingKeeper)
+
+	return appState, validators, nil
+}
+
+func (app *nameServiceApp) ExportInitialAppState() (json.RawMessage, error) {
+	// as if they could withdraw from the start of the next block
+	genState := ModuleBasics.DefaultGenesis()
+	return json.Marshal(genState)
+	// return codec.MarshalJSONIndent(app.cdc, genState)
+}
+
+func (app *nameServiceApp) GetCodec() *codec.Codec {
+	return app.cdc
 }
--- tendermint/node.go
+++ tendermint/node.go
@@ -1,14 +1,23 @@
 package tendermint
 
 import (
+	"encoding/json"
 	"os"
 	"path/filepath"
 	"time"
 
-	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/config"
-	"github.com/tendermint/tendermint/crypto"
-	"github.com/tendermint/tendermint/libs/log"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	authutils "github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/mesg-foundation/engine/config"
+	"github.com/mesg-foundation/engine/logger"
+	"github.com/mesg-foundation/engine/tendermint/app"
+	"github.com/sirupsen/logrus"
+	tmconfig "github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	"github.com/tendermint/tendermint/libs/db"
 	"github.com/tendermint/tendermint/node"
 	"github.com/tendermint/tendermint/p2p"
 	"github.com/tendermint/tendermint/privval"
@@ -16,8 +25,12 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
+var (
+	chainId = "xxx"
+)
+
 // NewNode retruns new tendermint node that runs the app.
-func NewNode(logger log.Logger, app abci.Application, root, seeds, externalAddress string, validatorPubKey crypto.PubKey) (*node.Node, error) {
+func NewNode(root, seeds, externalAddress string, validatorPukKey config.PubKeyEd25519) (*node.Node, error) {
 	if err := os.MkdirAll(filepath.Join(root, "config"), 0755); err != nil {
 		return nil, err
 	}
@@ -25,7 +38,7 @@ func NewNode(logger log.Logger, app abci.Application, root, seeds, externalAddre
 		return nil, err
 	}
 
-	cfg := config.DefaultConfig()
+	cfg := tmconfig.DefaultConfig()
 	cfg.P2P.PersistentPeers = seeds
 	// cfg.P2P.Seeds = seeds
 	// cfg.P2P.SeedMode = true
@@ -42,34 +55,123 @@ func NewNode(logger log.Logger, app abci.Application, root, seeds, externalAddre
 
 	me := privval.LoadOrGenFilePV(cfg.PrivValidatorKeyFile(), cfg.PrivValidatorStateFile())
 
+	// init app
+	db := db.NewMemDB()
+	logger := logger.TendermintLogger()
+	app := app.NewNameServiceApp(logger, db)
+
+	appState, err := app.ExportInitialAppState()
+	if err != nil {
+		return nil, err
+	}
+	logrus.WithField("state", string(appState)).Info("state")
+
+	// gen validator tx
+	msg, err := newValidatorTx(validatorPukKey)
+	if err != nil {
+		return nil, err
+	}
+	logrus.WithField("msg", msg).Info("validator tx")
+	// sign it
+	fees := authtypes.NewStdFee(flags.DefaultGasLimit, sdktypes.NewCoins())
+	gasPrices := sdktypes.NewDecCoins(sdktypes.NewCoins())
+	stdTx := authtypes.NewStdTx([]sdktypes.Msg{msg}, fees, []authtypes.StdSignature{}, "")
+	txBldr := authtypes.NewTxBuilder(
+		authutils.GetTxEncoder(app.GetCodec()),
+		0,
+		0,
+		flags.DefaultGasLimit,
+		flags.DefaultGasAdjustment,
+		true,
+		chainId,
+		"", // equal to "625b516112604044ea8e7d80e6c3544bd7766de8@10.0.2.15:26656" is genesis example
+		sdktypes.NewCoins(),
+		gasPrices,
+	)
+	signedTx, err := txBldr.SignStdTx("bob", "1", stdTx, true)
+	if err != nil {
+		return nil, err
+	}
+	logrus.WithField("signedTx", signedTx).Info("signed tx")
+
+	// appMessage, err := genutil.GenAppStateFromConfig(cdc, config, initCfg, *genDoc, genAccIterator)
+	// logrus.WithField("appMessage", appMessage).Info()
+
 	return node.NewNode(cfg,
 		me,
 		nodeKey,
 		proxy.NewLocalClientCreator(app),
-		genesisLoader(validatorPubKey),
+		genesisLoader(appState),
 		node.DefaultDBProvider,
 		node.DefaultMetricsProvider(cfg.Instrumentation),
 		logger,
 	)
 }
 
-func genesisLoader(validator crypto.PubKey) func() (*types.GenesisDoc, error) {
+func genesisLoader(appState json.RawMessage) func() (*types.GenesisDoc, error) {
 	return func() (*types.GenesisDoc, error) {
 		genesis := &types.GenesisDoc{
 			GenesisTime:     time.Date(2019, 8, 8, 0, 0, 0, 0, time.UTC),
-			ChainID:         "xxx",
+			ChainID:         chainId,
 			ConsensusParams: types.DefaultConsensusParams(),
-			Validators: []types.GenesisValidator{{
-				Address: validator.Address(),
-				PubKey:  validator,
-				Power:   1,
-				Name:    "validator",
-			}},
-			AppState: []byte("{}"),
+			Validators:      []types.GenesisValidator{
+				// 	{
+				// 	Address: validator.Address(),
+				// 	PubKey:  validator,
+				// 	Power:   1,
+				// 	Name:    "validator",
+				// }
+			},
+			// AppState: []byte("{}"),
+			AppState: appState,
 		}
 		if err := genesis.ValidateAndComplete(); err != nil {
 			panic(err)
 		}
 		return genesis, nil
 	}
+}
+
+func newValidatorTx(validatorPukKey config.PubKeyEd25519) (sdktypes.Msg, error) {
+	emptyReturn := stakingtypes.MsgCreateValidator{}
+	// default value see github.com/cosmos/cosmos-sdk@v0.36.0-rc1/x/staking/client/cli/tx.go
+
+	validator := ed25519.PubKeyEd25519(validatorPukKey)
+	// validatorAddress, err := sdktypes.ValAddressFromBech32("")
+
+	defaultTokens := sdktypes.TokensFromConsensusPower(100)
+	amount, err := sdktypes.ParseCoin(defaultTokens.String() + sdktypes.DefaultBondDenom)
+	if err != nil {
+		return emptyReturn, err
+	}
+	description := stakingtypes.NewDescription(
+		"moniker-nico",
+		"identify-nico",
+		"website-nico",
+		"details-nico",
+	)
+
+	rate, err := sdktypes.NewDecFromStr("0.1")
+	if err != nil {
+		return emptyReturn, err
+	}
+	maxRate, err := sdktypes.NewDecFromStr("0.2")
+	if err != nil {
+		return emptyReturn, err
+	}
+	maxChangeRate, err := sdktypes.NewDecFromStr("0.01")
+	if err != nil {
+		return emptyReturn, err
+	}
+	commissionRates := stakingtypes.NewCommissionRates(rate, maxRate, maxChangeRate)
+
+	msg := stakingtypes.NewMsgCreateValidator(
+		sdktypes.ValAddress(validator.Address()),
+		validator,
+		amount,
+		description,
+		commissionRates,
+		sdktypes.NewInt(1),
+	)
+	return msg, nil
 }
