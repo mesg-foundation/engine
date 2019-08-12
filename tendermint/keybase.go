4e0b5d56db3bbc999b6771c30a6ae9678838445f
krhubert <krhubert@gmail.com>
2019-08-12 18:28:47 +0200

    Refactor + clean up creation of the app

---
+++ tendermint/keybase.go
@@ -0,0 +1,36 @@
+package tendermint
+
+import (
+	clientkey "github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/cosmos/go-bip39"
+)
+
+const mnemonicEntropySize = 256
+
+type keybase struct {
+	keys.Keybase
+}
+
+// NewFSKeybas initializes a filesystem keybase at a particular dir.
+func NewFSKeybase(dir string) (*keybase, error) {
+	kb, err := clientkey.NewKeyBaseFromDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	return &keybase{kb}, nil
+}
+
+// CreateAccount creates an account.
+func (kb *keybase) GenerateAccount(name, password string) (keys.Info, error) {
+	// read entropy seed straight from crypto.Rand and convert to mnemonic
+	entropySeed, err := bip39.NewEntropy(mnemonicEntropySize)
+	if err != nil {
+		return nil, err
+	}
+	mnemonic, err := bip39.NewMnemonic(entropySeed[:])
+	if err != nil {
+		return nil, err
+	}
+	return kb.CreateAccount(name, mnemonic, "", password, 0, 0)
+}
