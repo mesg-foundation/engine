54e6008817277181e1fb01547f775182dc67b177
Nicolas Mahe <nicolas@mahe.me>
2019-08-13 15:57:18 +0700

    simplify (a bit) the cosmos app

--- tendermint/app/app.go
+++ tendermint/app/app.go
@@ -265,10 +265,6 @@ func NewNameServiceApp(logger log.Logger, db dbm.DB) (*NameServiceApp, *codec.Co
 // GenesisState represents chain state at the start of the chain. Any initial state (account balances) are stored here.
 type GenesisState map[string]json.RawMessage
 
-func NewDefaultGenesisState() GenesisState {
-	return ModuleBasics.DefaultGenesis()
-}
-
 func (app *NameServiceApp) InitChainer(ctx sdk.Context, req abci.RequestInitChain) abci.ResponseInitChain {
 	var genesisState GenesisState
 
@@ -288,23 +284,4 @@ func (app *NameServiceApp) EndBlocker(ctx sdk.Context, req abci.RequestEndBlock)
 }
 func (app *NameServiceApp) LoadHeight(height int64) error {
 	return app.LoadVersion(height, app.keyMain)
-}
-
-//_________________________________________________________
-
-func (app *NameServiceApp) ExportAppStateAndValidators(forZeroHeight bool, jailWhiteList []string,
-) (appState json.RawMessage, validators []tmtypes.GenesisValidator, err error) {
-
-	// as if they could withdraw from the start of the next block
-	ctx := app.NewContext(true, abci.Header{Height: app.LastBlockHeight()})
-
-	genState := app.mm.ExportGenesis(ctx)
-	appState, err = codec.MarshalJSONIndent(app.cdc, genState)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	validators = staking.WriteValidators(ctx, app.stakingKeeper)
-
-	return appState, validators, nil
 }
