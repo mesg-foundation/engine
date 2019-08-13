56c711a03509d3699e2b62eb1d3b4773064c1f51
Anthony ESTEBE <anthony@mesg.com>
2019-08-13 13:26:46 +0700

    add basic workflow validation

--- workflow/workflow.go
+++ workflow/workflow.go
@@ -4,7 +4,29 @@ import (
 	"fmt"
 
 	"github.com/mesg-foundation/engine/hash"
+	"gopkg.in/go-playground/validator.v9"
 )
+
+// Validate returns an error if the workflow is invalid for whatever reason
+func (w *Workflow) Validate() error {
+	if err := validator.New().Struct(w); err != nil {
+		return err
+	}
+	// Check that the initial trigger connects to an existing node.
+	if _, err := w.FindNode(w.Trigger.NodeKey); err != nil {
+		return err
+	}
+	// Check that all edges are associated to an existing node.
+	for _, edge := range w.Edges {
+		if _, err := w.FindNode(edge.Src); err != nil {
+			return err
+		}
+		if _, err := w.FindNode(edge.Dst); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 // Match returns true if a workflow trigger is matching the given parameters
 func (t *Trigger) Match(trigger TriggerType, instanceHash hash.Hash, key string, data map[string]interface{}) bool {
@@ -56,5 +78,5 @@ func (w Workflow) FindNode(key string) (Node, error) {
 			return node, nil
 		}
 	}
-	return Node{}, fmt.Errorf("%q not found", key)
+	return Node{}, fmt.Errorf("node %q not found", key)
 }
