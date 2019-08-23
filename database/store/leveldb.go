536c4709615cc08ad9b9dd7448c66120df764ecc
krhubert <krhubert@gmail.com>
2019-08-23 12:10:49 +0200

    Refactor database pacakge
    - add exist method to instance db
    - use db.exist in instance sdk

--- database/store/leveldb.go
+++ database/store/leveldb.go
@@ -30,16 +30,9 @@ func (s *LevelDBStore) Has(key []byte) (bool, error) {
 	return s.db.Has(key, nil)
 }
 
-// Get retrives service from store. It returns ErrNotFound if the store does not contains the key.
+// Get retrives service from store. It returns error if the store does not contains the key.
 func (s *LevelDBStore) Get(key []byte) ([]byte, error) {
-	value, err := s.db.Get(key, nil)
-	if err == leveldb.ErrNotFound {
-		return nil, ErrNotFound
-	}
-	if err != nil {
-		return nil, err
-	}
-	return value, nil
+	return s.db.Get(key, nil)
 }
 
 // Delete deletes the value for the given key. Delete will not returns error if key doesn't exist.
