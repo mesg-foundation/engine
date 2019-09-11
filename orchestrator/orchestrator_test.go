1a4bb596742448293cd8756050bf91d40dbb4bc6
Anthony ESTEBE <anthony@mesg.com>
2019-09-11 22:22:17 +0700

    add tests to process task and resolve inputs

--- orchestrator/orchestrator_test.go
+++ orchestrator/orchestrator_test.go
@@ -1,6 +1,7 @@
 package orchestrator
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/mesg-foundation/engine/event"
@@ -8,8 +9,7 @@ import (
 	"github.com/mesg-foundation/engine/hash"
 	"github.com/mesg-foundation/engine/orchestrator/mocks"
 	"github.com/mesg-foundation/engine/process"
-	eventsdk "github.com/mesg-foundation/engine/sdk/event"
-	executionsdk "github.com/mesg-foundation/engine/sdk/execution"
+	"github.com/mesg-foundation/engine/protobuf/types"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
@@ -143,17 +143,137 @@ func TestFilter(t *testing.T) {
 	}
 }
 
-func TestStart(t *testing.T) {
-	event := &mocks.EventSDK{}
-	exec := &mocks.ExecutionSDK{}
-	process := &mocks.ProcessSDK{}
-	o := New(event, exec, process)
-	eventListener := &eventsdk.Listener{}
-	execListener := &executionsdk.Listener{}
-	event.On("GetStream", mock.Anything).Return(eventListener)
-	exec.On("GetStream", mock.Anything).Return(execListener)
-	err := o.Start()
-	eventListener.Close()
-	execListener.Close()
-	require.NoError(t, err)
+func TestFindNode(t *testing.T) {
+	o := New(&mocks.EventSDK{}, &mocks.ExecutionSDK{}, &mocks.ProcessSDK{})
+	data := &process.Process{
+		Hash: hash.Int(1),
+		Nodes: []*process.Process_Node{
+			&process.Process_Node{
+				Type: &process.Process_Node_Event_{
+					Event: &process.Process_Node_Event{
+						Key: "1",
+					},
+				},
+			},
+		},
+	}
+	require.Len(t, o.findNodes(data, func(p *process.Process, n *process.Process_Node) (bool, error) {
+		return true, nil
+	}), 1)
+	require.Len(t, o.findNodes(data, func(p *process.Process, n *process.Process_Node) (bool, error) {
+		return n.GetEvent().Key == "1", nil
+	}), 1)
+	require.Len(t, o.findNodes(data, func(p *process.Process, n *process.Process_Node) (bool, error) {
+		return n.GetEvent().Key == "2", nil
+	}), 0)
 }
+
+// func TestProcessMap(t *testing.T) {
+// 	e := &mocks.ExecutionSDK{}
+// 	o := New(&mocks.EventSDK{}, e, &mocks.ProcessSDK{})
+// 	exec := &execution.Execution{
+// 		ProcessHash: hash.Int(1),
+// 		StepID:      "1",
+// 		ParentHash:  hash.Int(2),
+// 		Outputs: &types.Struct{
+// 			Fields: map[string]*types.Value{
+// 				"xxx": &types.Value{
+// 					Kind: &types.Value_StringValue{StringValue: "str"},
+// 				},
+// 			},
+// 		},
+// 	}
+// 	o.processMap(&process.Process_Node_Map{
+// 		Outputs: []*process.Process_Node_Map_Output{},
+// 	}, )
+// }
+
+func TestResolveInput(t *testing.T) {
+	e := &mocks.ExecutionSDK{}
+	o := New(&mocks.EventSDK{}, e, &mocks.ProcessSDK{})
+	exec := &execution.Execution{
+		ProcessHash: hash.Int(2),
+		StepID:      "2",
+		ParentHash:  hash.Int(3),
+		Outputs: &types.Struct{
+			Fields: map[string]*types.Value{
+				"xxx": &types.Value{
+					Kind: &types.Value_StringValue{StringValue: "str"},
+				},
+			},
+		},
+	}
+	// Different processes
+	_, err := o.resolveInput(hash.Int(1), exec, "2", "xxx")
+	require.Error(t, err)
+	// Different steps, should return the value of the data
+	val, err := o.resolveInput(hash.Int(2), exec, "2", "xxx")
+	require.NoError(t, err)
+	require.Equal(t, val, exec.Outputs.Fields["xxx"])
+	// Invalid execution parent hash
+	e.On("Get", mock.Anything).Once().Return(nil, fmt.Errorf("err"))
+	_, err = o.resolveInput(hash.Int(2), exec, "-", "xxx")
+	require.Error(t, err)
+	// Output from a previous exec
+	execMock := &execution.Execution{
+		StepID:      "3",
+		ProcessHash: hash.Int(2),
+		Outputs: &types.Struct{
+			Fields: map[string]*types.Value{
+				"yyy": &types.Value{
+					Kind: &types.Value_StringValue{StringValue: "str2"},
+				},
+			},
+		},
+	}
+	e.On("Get", mock.Anything).Once().Return(execMock, nil)
+	val, err = o.resolveInput(hash.Int(2), exec, "3", "yyy")
+	require.NoError(t, err)
+	require.Equal(t, val, execMock.Outputs.Fields["yyy"])
+}
+
+func TestProcessTask(t *testing.T) {
+	e := &mocks.ExecutionSDK{}
+	e.On("Execute", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Once().Return(nil, nil)
+	o := New(&mocks.EventSDK{}, e, &mocks.ProcessSDK{})
+	err := o.processTask(&process.Process_Node_Task{
+		InstanceHash: hash.Int(1),
+		Key:          "-",
+		TaskKey:      "-",
+	}, &process.Process{
+		Hash: hash.Int(2),
+	}, &execution.Execution{
+		Hash: hash.Int(3),
+	}, nil, &types.Struct{
+		Fields: map[string]*types.Value{},
+	})
+	require.NoError(t, err)
+	e.On("Execute", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Once().Return(nil, fmt.Errorf("error"))
+	err = o.processTask(&process.Process_Node_Task{
+		InstanceHash: hash.Int(1),
+		Key:          "-",
+		TaskKey:      "-",
+	}, &process.Process{
+		Hash: hash.Int(2),
+	}, nil, &event.Event{
+		Hash: hash.Int(3),
+	}, &types.Struct{
+		Fields: map[string]*types.Value{},
+	})
+	require.Error(t, err)
+}
+
+// func TestStart(t *testing.T) {
+// 	event := &mocks.EventSDK{}
+// 	exec := &mocks.ExecutionSDK{}
+// 	process := &mocks.ProcessSDK{}
+// 	o := New(event, exec, process)
+// 	eventListener := &eventsdk.Listener{}
+// 	execListener := &executionsdk.Listener{}
+// 	event.On("GetStream", mock.Anything).Return(eventListener)
+// 	exec.On("GetStream", mock.Anything).Return(execListener)
+// 	err := o.Start()
+// 	eventListener.Close()
+// 	execListener.Close()
+// 	require.NoError(t, err)
+// }
