9e812a3fd8ce4edb1116442f9c2a5163ca339b1b
Anthony ESTEBE <anthony@mesg.com>
2019-09-13 17:01:38 +0700

    add test to reproduce the issue

--- database/process_db_test.go
+++ database/process_db_test.go
@@ -63,3 +63,36 @@ func TestProcessDB(t *testing.T) {
 		require.NoError(t, db.Delete(p.Hash))
 	})
 }
+
+func TestMultipleInsert(t *testing.T) {
+	dir, _ := ioutil.TempDir("", "process.db.test")
+	defer os.Remove(dir)
+
+	db, err := NewProcessDB(dir)
+	require.NoError(t, err)
+	defer db.Close()
+
+	p1 := &process.Process{
+		Hash: hash.Int(1),
+		Key:  "1",
+	}
+
+	p2 := &process.Process{
+		Hash: hash.Int(2),
+		Key:  "2",
+	}
+
+	db.Save(p1)
+	defer db.Delete(p1.Hash)
+	db.Save(p2)
+	defer db.Delete(p2.Hash)
+	list, err := db.All()
+	require.NoError(t, err)
+	require.Len(t, list, 2)
+
+	require.Equal(t, list[0].Key, p1.Key)
+	require.Equal(t, list[1].Key, p2.Key)
+
+	require.Equal(t, list[0].Hash, p1.Hash)
+	require.Equal(t, list[1].Hash, p2.Hash)
+}
