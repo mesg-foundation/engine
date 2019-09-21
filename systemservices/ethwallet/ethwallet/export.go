af60523683c2fe2ec32b85cc6f1ffd99ecbfb406
Nicolas Mahe <nicolas@mahe.me>
2019-09-21 15:07:01 +0700

    Simplify ethwallet export and import tasks by using the account in json encoded string

--- systemservices/ethwallet/ethwallet/export.go
+++ systemservices/ethwallet/ethwallet/export.go
@@ -1,8 +1,6 @@
 package ethwallet
 
 import (
-	"encoding/json"
-
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/mesg-foundation/engine/protobuf/types"
 	"github.com/mesg-foundation/engine/systemservices/ethwallet/x/xgo-ethereum/xaccounts"
@@ -10,39 +8,19 @@ import (
 
 func (s *Ethwallet) export(inputs *types.Struct) (*types.Struct, error) {
 	address := common.HexToAddress(inputs.GetStringValue("address"))
+	passphrase := inputs.GetStringValue("passphrase")
+
 	account, err := xaccounts.GetAccount(s.keystore, address)
 	if err != nil {
 		return nil, errAccountNotFound
 	}
 
-	passphrase := inputs.GetStringValue("passphrase")
-	keyJSON, err := s.keystore.Export(account, passphrase, passphrase)
+	accountJSON, err := s.keystore.Export(account, passphrase, passphrase)
 	if err != nil {
 		return nil, err
 	}
 
-	var accountJSON encryptedKeyJSONV3
-	if err = json.Unmarshal(keyJSON, &accountJSON); err != nil {
-		return nil, err
-	}
-
 	return types.NewStruct(map[string]*types.Value{
-		"address": types.NewValueFrom(accountJSON.Address),
-		"version": types.NewValueFrom(accountJSON.Version),
-		"id":      types.NewValueFrom(accountJSON.ID),
-		"crypto": types.NewValueFrom(types.NewStruct(
-			map[string]*types.Value{
-				"cipher":     types.NewValueFrom(accountJSON.Crypto.Cipher),
-				"ciphertext": types.NewValueFrom(accountJSON.Crypto.CipherText),
-				"cipherparams": types.NewValueFrom(types.NewStruct(
-					map[string]*types.Value{
-						"iv": types.NewValueFrom(accountJSON.Crypto.CipherParams.IV),
-					},
-				)),
-				"kdfparams": types.NewValueFrom(accountJSON.Crypto.KDFParams),
-				"kdf":       types.NewValueFrom(accountJSON.Crypto.KDF),
-				"mac":       types.NewValueFrom(accountJSON.Crypto.MAC),
-			},
-		)),
+		"account": types.NewValueFrom(string(accountJSON)),
 	}), nil
 }
--- systemservices/ethwallet/ethwallet/import.go
+++ systemservices/ethwallet/ethwallet/import.go
@@ -1,21 +1,14 @@
 package ethwallet
 
 import (
-	"encoding/json"
-
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/mesg-foundation/engine/protobuf/types"
 )
 
 func (s *Ethwallet) importA(inputs *types.Struct) (*types.Struct, error) {
-	account := common.HexToAddress(inputs.GetStringValue("account"))
-	accountJSON, err := json.Marshal(account)
-	if err != nil {
-		return nil, err
-	}
-
+	accountJSON := inputs.GetStringValue("account")
 	passphrase := inputs.GetStringValue("passphrase")
-	importedAccount, err := s.keystore.Import(accountJSON, passphrase, passphrase)
+
+	importedAccount, err := s.keystore.Import([]byte(accountJSON), passphrase, passphrase)
 	if err != nil {
 		return nil, err
 	}
