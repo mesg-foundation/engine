f033cba993abfaeca0de5e2689c1c7a82801070e
krhubert <krhubert@gmail.com>
2019-10-25 11:17:24 +0200

    wip

--- e2e/instance_test.go
+++ e2e/instance_test.go
@@ -4,25 +4,23 @@ import (
 	"context"
 	"testing"
 
-	"github.com/mesg-foundation/engine/hash"
 	pb "github.com/mesg-foundation/engine/protobuf/api"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/metadata"
 )
 
 func testInstance(t *testing.T) {
-	var instanceHash hash.Hash
 	t.Run("create", func(t *testing.T) {
 		ctx := metadata.NewOutgoingContext(context.Background(), passmd)
 		resp, err := client.InstanceClient.Create(ctx, &pb.CreateInstanceRequest{ServiceHash: testServiceHash})
 		require.NoError(t, err)
-		instanceHash = resp.Hash
+		testInstanceHash = resp.Hash
 	})
 
 	t.Run("get", func(t *testing.T) {
-		resp, err := client.InstanceClient.Get(context.Background(), &pb.GetInstanceRequest{Hash: instanceHash})
+		resp, err := client.InstanceClient.Get(context.Background(), &pb.GetInstanceRequest{Hash: testInstanceHash})
 		require.NoError(t, err)
-		require.Equal(t, instanceHash, resp.Hash)
+		require.Equal(t, testInstanceHash, resp.Hash)
 		require.Equal(t, testServiceHash, resp.ServiceHash)
 	})
 
