a536e8efa5f573b563fdf2bb3950c0942c1e54fb
krhubert <krhubert@gmail.com>
2019-11-13 10:32:07 +0100

    Remove unnecessary RegisterConcrete

--- sdk/service/codec.go
+++ sdk/service/codec.go
@@ -2,10 +2,8 @@ package servicesdk
 
 import (
 	"github.com/mesg-foundation/engine/codec"
-	"github.com/mesg-foundation/engine/service"
 )
 
 func init() {
 	codec.RegisterConcrete(msgCreateService{}, "service/create", nil)
-	codec.RegisterConcrete(&service.Service{}, "mesg.types.Service", nil)
 }
