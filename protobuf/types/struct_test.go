8fbd9720a7bd910ad032eab44427461b7a47a53d
Nicolas Mahe <nicolas@mahe.me>
2019-11-15 14:42:04 +0700

    Apply suggestion from krhubert

--- protobuf/types/struct_test.go
+++ protobuf/types/struct_test.go
@@ -30,38 +30,38 @@ func TestStructMarshal(t *testing.T) {
 		structSort1 = &Struct{
 			Fields: map[string]*Value{
 				"10000":  {Kind: &Value_NumberValue{NumberValue: 10}},
-				"1":      {Kind: &Value_StringValue{StringValue: "valuea"}},
+				"1":      {Kind: &Value_StringValue{StringValue: "value"}},
 				"null":   {Kind: &Value_NullValue{NullValue: NullValue_NULL_VALUE}},
 				"number": {Kind: &Value_NumberValue{NumberValue: 10}},
-				"string": {Kind: &Value_StringValue{StringValue: "valuea"}},
+				"string": {Kind: &Value_StringValue{StringValue: "value"}},
 				"bool":   {Kind: &Value_BoolValue{BoolValue: true}},
 				"struct": {Kind: &Value_StructValue{StructValue: &Struct{Fields: map[string]*Value{
 					"10000":  {Kind: &Value_NumberValue{NumberValue: 10}},
-					"1":      {Kind: &Value_StringValue{StringValue: "valuea"}},
+					"1":      {Kind: &Value_StringValue{StringValue: "value"}},
 					"null":   {Kind: &Value_NullValue{NullValue: NullValue_NULL_VALUE}},
 					"number": {Kind: &Value_NumberValue{NumberValue: 10}},
 					"struct": {Kind: &Value_StructValue{StructValue: &Struct{Fields: map[string]*Value{
 						"10000":  {Kind: &Value_NumberValue{NumberValue: 10}},
-						"1":      {Kind: &Value_StringValue{StringValue: "valuea"}},
+						"1":      {Kind: &Value_StringValue{StringValue: "value"}},
 						"null":   {Kind: &Value_NullValue{NullValue: NullValue_NULL_VALUE}},
 						"number": {Kind: &Value_NumberValue{NumberValue: 10}},
 					}}}},
 				}}}},
 				"list": {Kind: &Value_ListValue{ListValue: &ListValue{Values: []*Value{
-					{Kind: &Value_StringValue{StringValue: "dvaluea"}},
+					{Kind: &Value_StringValue{StringValue: "value"}},
 				}}}},
 			},
 		}
 		structSort2 = &Struct{
 			Fields: map[string]*Value{
-				"1": {Kind: &Value_StringValue{StringValue: "valuea"}},
+				"1": {Kind: &Value_StringValue{StringValue: "value"}},
 				"struct": {Kind: &Value_StructValue{StructValue: &Struct{Fields: map[string]*Value{
-					"1":    {Kind: &Value_StringValue{StringValue: "valuea"}},
+					"1":    {Kind: &Value_StringValue{StringValue: "value"}},
 					"null": {Kind: &Value_NullValue{NullValue: NullValue_NULL_VALUE}},
 					"struct": {Kind: &Value_StructValue{StructValue: &Struct{Fields: map[string]*Value{
 						"number": {Kind: &Value_NumberValue{NumberValue: 10}},
 						"10000":  {Kind: &Value_NumberValue{NumberValue: 10}},
-						"1":      {Kind: &Value_StringValue{StringValue: "valuea"}},
+						"1":      {Kind: &Value_StringValue{StringValue: "value"}},
 						"null":   {Kind: &Value_NullValue{NullValue: NullValue_NULL_VALUE}},
 					}}}},
 					"number": {Kind: &Value_NumberValue{NumberValue: 10}},
@@ -71,9 +71,9 @@ func TestStructMarshal(t *testing.T) {
 				"number": {Kind: &Value_NumberValue{NumberValue: 10}},
 				"null":   {Kind: &Value_NullValue{NullValue: NullValue_NULL_VALUE}},
 				"bool":   {Kind: &Value_BoolValue{BoolValue: true}},
-				"string": {Kind: &Value_StringValue{StringValue: "valuea"}},
+				"string": {Kind: &Value_StringValue{StringValue: "value"}},
 				"list": {Kind: &Value_ListValue{ListValue: &ListValue{Values: []*Value{
-					{Kind: &Value_StringValue{StringValue: "dvaluea"}},
+					{Kind: &Value_StringValue{StringValue: "value"}},
 				}}}},
 			},
 		}
@@ -88,13 +88,13 @@ func TestStructMarshal(t *testing.T) {
 		require.NoError(t, err)
 		structValueSort2, err = codec.MarshalBinaryBare(structSort2)
 		require.NoError(t, err)
-		require.Equal(t, hash.Dump(structValueSort1), hash.Dump(structValueSort2))
+		require.True(t, hash.Dump(structValueSort1).Equal(hash.Dump(structValueSort2)))
 	})
 	t.Run("Unmarshal", func(t *testing.T) {
 		require.NoError(t, codec.UnmarshalBinaryBare(structValueSort1, &structUnm1))
-		require.Equal(t, structSort1, structUnm1)
+		require.True(t, structSort1.Equal(structUnm1))
 		require.NoError(t, codec.UnmarshalBinaryBare(structValueSort2, &structUnm2))
-		require.Equal(t, structSort2, structUnm2)
-		require.Equal(t, structUnm1, structUnm2)
+		require.True(t, structSort2.Equal(structUnm2))
+		require.True(t, structUnm1.Equal(structUnm2))
 	})
 }
