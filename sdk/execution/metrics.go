2f824609426db99bfb6c06d09ff322944dc087e6
Anthony ESTEBE <anthony.estebe@gmail.com>
2020-01-03 12:47:10 +0700

    add execution stats

---
+++ sdk/execution/metrics.go
@@ -0,0 +1,49 @@
+package executionsdk
+
+import (
+	"github.com/go-kit/kit/metrics"
+	prometheus "github.com/go-kit/kit/metrics/prometheus"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+)
+
+var m *metric
+
+type metric struct {
+	Created    metrics.Counter
+	Signed     metrics.Counter
+	InProgress metrics.Counter
+	Completed  metrics.Counter
+}
+
+func newMetric() *metric {
+	return &metric{
+		Created: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "mesg",
+			Subsystem: "execution",
+			Name:      "created",
+			Help:      "executions created",
+		}, []string{}),
+		Signed: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "mesg",
+			Subsystem: "execution",
+			Name:      "signed",
+			Help:      "executions signed",
+		}, []string{}),
+		InProgress: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "mesg",
+			Subsystem: "execution",
+			Name:      "in_progress",
+			Help:      "executions in progress",
+		}, []string{}),
+		Completed: prometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "mesg",
+			Subsystem: "execution",
+			Name:      "completed",
+			Help:      "executions completed",
+		}, []string{}),
+	}
+}
+
+func init() {
+	m = newMetric()
+}
