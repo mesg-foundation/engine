b0b2ec3a1b23a54fcfa8b0aee6ed4c17fae6b267
Nicolas Mahe <nicolas@mahe.me>
2020-01-29 11:18:47 +0700

    fix typo

--- cosmos/client.go
+++ cosmos/client.go
@@ -146,7 +146,7 @@ func (c *Client) Stream(ctx context.Context, query string) (chan hash.Hash, chan
 			select {
 			case msg := <-msgStream.Out():
 				attrs := msg.Events()[EventHashType]
-				// The following error might be too much as MAYBE if one transaction contains many messages, the events will be merged accross the whole transaction
+				// The following error might be too much as MAYBE if one transaction contains many messages, the events will be merged across the whole transaction
 				if len(attrs) != 1 {
 					errC <- fmt.Errorf("event %s has %d tag(s), but only 1 is expected", EventHashType, len(attrs))
 				}
