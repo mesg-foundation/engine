5d41033794f94eb182d905e6660288d71ce71046
antho1404 <anthony@mesg.com>
2020-02-28 09:56:39 +0700

    make sure to call end block for each modules

--- x/service/module.go
+++ x/service/module.go
@@ -136,6 +136,7 @@ func (am AppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {
 
 // EndBlock returns the end blocker for the service module. It returns no validator
 // updates.
-func (AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+	EndBlocker(ctx, am.keeper)
 	return []abci.ValidatorUpdate{}
 }
