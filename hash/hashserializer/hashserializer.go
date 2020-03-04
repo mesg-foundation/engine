6280a240420a73fc080728c604d1ee7da0587290
Nicolas Mahe <nicolas@mahe.me>
2020-03-04 18:13:02 +0700

    Improve a lot the doc of hashserializer

--- hash/hashserializer/hashserializer.go
+++ hash/hashserializer/hashserializer.go
@@ -1,3 +1,9 @@
+// Package hashserializer creates a string representing arbitrary value that can then be used to generate a deterministic hash.
+//
+// Each value can be added to the HashSerializer using one of the AddXXX function alongside a prefix.
+// The prefix should be unique and the AddXXX function should always be called in the same order in order to create a deterministic string.
+//
+// Empty value are discarded and its prefix will not be part of the generated string.
 package hashserializer
 
 import (
@@ -5,71 +11,78 @@ import (
 	"strconv"
 )
 
-// HashSerializable represents a serializable struct
+// HashSerializable represents a serializable struct.
 type HashSerializable interface {
 	HashSerialize() string
 }
 
-// HashSerializer helps to create hash-serialized string of a struct
+// HashSerializer helps to create hash-serialized string of a struct.
 type HashSerializer struct {
 	buf bytes.Buffer
 }
 
-// New returns a new HashSerializer
+// New returns a new HashSerializer.
 func New() *HashSerializer {
 	return &HashSerializer{}
 }
 
-// HashSerialize returns the hash-serialized constructed string
+// HashSerialize returns the hash-serialized constructed string.
 func (s *HashSerializer) HashSerialize() string {
 	return s.buf.String()
 }
 
-// AddString adds a string to the serializer
-func (s *HashSerializer) AddString(number string, value string) {
+// AddString adds a string to the serializer.
+// If value is empty, nothing is added to the HashSerializer.
+func (s *HashSerializer) AddString(prefix string, value string) {
 	if value != "" {
-		s.buf.WriteString(number)
+		s.buf.WriteString(prefix)
 		s.buf.WriteString(":")
 		s.buf.WriteString(value)
 		s.buf.WriteString(";")
 	}
 }
 
-// AddBool adds a boolean to the serializer
-func (s *HashSerializer) AddBool(number string, value bool) {
+// AddBool adds a boolean to the serializer.
+// If value is false, nothing is added to the HashSerializer.
+func (s *HashSerializer) AddBool(prefix string, value bool) {
 	if value {
-		s.AddString(number, "true")
+		s.AddString(prefix, "true")
 	}
 }
 
-// AddFloat adds a float to the serializer
-func (s *HashSerializer) AddFloat(number string, value float64) {
+// AddFloat adds a float to the serializer.
+// If value is 0, nothing is added to the HashSerializer.
+func (s *HashSerializer) AddFloat(prefix string, value float64) {
 	if value != 0 {
-		s.AddString(number, strconv.FormatFloat(value, 'f', -1, 64))
+		s.AddString(prefix, strconv.FormatFloat(value, 'f', -1, 64))
 	}
 }
 
-// AddInt adds a int to the serializer
-func (s *HashSerializer) AddInt(number string, value int) {
+// AddInt adds a int to the serializer.
+// If value is 0, nothing is added to the HashSerializer.
+func (s *HashSerializer) AddInt(prefix string, value int) {
 	if value != 0 {
-		s.AddString(number, strconv.Itoa(value))
+		s.AddString(prefix, strconv.Itoa(value))
 	}
 }
 
-// Add adds a hashserializable struct to the serializer
-func (s *HashSerializer) Add(number string, value HashSerializable) {
-	s.AddString(number, value.HashSerialize())
+// Add adds a struct implementing HashSerializable to the serializer.
+// The function HashSerialize will be called on value and its result injected as a string in HashSerializer.
+// If value.HashSerialize() returns an empty string, nothing is added to the HashSerializer.
+func (s *HashSerializer) Add(prefix string, value HashSerializable) {
+	s.AddString(prefix, value.HashSerialize())
 }
 
-// AddStringSlice adds a slice of string to the serializer
-func (s *HashSerializer) AddStringSlice(number string, value []string) {
-	s.Add(number, StringSlice(value))
+// AddStringSlice adds a slice of string to the serializer.
+// If value is an empty slice or each element is empty, nothing is added to the HashSerializer.
+func (s *HashSerializer) AddStringSlice(prefix string, value []string) {
+	s.Add(prefix, StringSlice(value))
 }
 
-// StringSlice is an helper type to easily hashserialized slice of string
+// StringSlice is an helper type to easily hash-serialized slice of string.
 type StringSlice []string
 
-// HashSerialize returns the hashserialized string of this type
+// HashSerialize returns the hash-serialized string of this type.
 func (s StringSlice) HashSerialize() string {
 	ser := New()
 	for i, value := range s {
--- hash/hashserializer/hashserializer_test.go
+++ hash/hashserializer/hashserializer_test.go
@@ -1,9 +1,7 @@
 package hashserializer
 
 import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+	"fmt"
 )
 
 type testSerializable struct {
@@ -20,31 +18,145 @@ func (data testSerializable) HashSerialize() string {
 	return ser.HashSerialize()
 }
 
-func TestHashSerializer(t *testing.T) {
-	tests := []struct {
-		f func(*HashSerializer)
-		s string
-	}{
-		{func(ser *HashSerializer) { ser.AddBool("1", true) }, "1:true;"},
-		{func(ser *HashSerializer) { ser.AddBool("2", false) }, ""},
-		{func(ser *HashSerializer) { ser.AddFloat("3", 3.14159265359) }, "3:3.14159265359;"},
-		{func(ser *HashSerializer) { ser.AddFloat("4", 0.0) }, ""},
-		{func(ser *HashSerializer) { ser.AddFloat("5", -42.14159265359) }, "5:-42.14159265359;"},
-		{func(ser *HashSerializer) { ser.AddInt("6", 42) }, "6:42;"},
-		{func(ser *HashSerializer) { ser.AddInt("7", 0.0) }, ""},
-		{func(ser *HashSerializer) { ser.AddInt("8", -42) }, "8:-42;"},
-		{func(ser *HashSerializer) { ser.AddString("9", "") }, ""},
-		{func(ser *HashSerializer) { ser.AddString("10", "hello") }, "10:hello;"},
-		{func(ser *HashSerializer) { ser.AddStringSlice("11", []string{"c", "b", "a"}) }, "11:0:c;1:b;2:a;;"},
-		{func(ser *HashSerializer) { ser.AddStringSlice("12", []string{}) }, ""},
-		{func(ser *HashSerializer) { ser.AddStringSlice("13", []string{"c", "", "a"}) }, "13:0:c;2:a;;"},
-		{func(ser *HashSerializer) { ser.Add("14", testSerializable{42, "hello", []string{"c", "b", "a"}}) }, "14:1:42;2:hello;3:0:c;1:b;2:a;;;"},
-		{func(ser *HashSerializer) { ser.Add("15", testSerializable{}) }, ""},
+func ExampleHashSerializer() {
+	ser := New()
+	ser.AddBool("1", true)
+	ser.AddBool("2", false)
+	ser.AddFloat("3", 3.14159265359)
+	ser.AddFloat("4", 0.0)
+	ser.AddFloat("5", -42.14159265359)
+	ser.AddInt("6", 42)
+	ser.AddInt("7", 0.0)
+	ser.AddInt("8", -42)
+	ser.AddString("9", "")
+	ser.AddString("10", "hello")
+	ser.AddStringSlice("11", []string{"c", "b", "a"})
+	ser.AddStringSlice("12", []string{})
+	ser.AddStringSlice("13", []string{"c", "", "a"})
+	ser.Add("14", testSerializable{42, "hello", []string{"c", "b", "a"}})
+	ser.Add("15", testSerializable{})
+	fmt.Println(ser.HashSerialize())
+	// Output: 1:true;3:3.14159265359;5:-42.14159265359;6:42;8:-42;10:hello;11:0:c;1:b;2:a;;13:0:c;2:a;;14:1:42;2:hello;3:0:c;1:b;2:a;;;
+}
+
+func ExampleHashSerializer_AddBool_true() {
+	ser := New()
+	ser.AddBool("1", true)
+	fmt.Println(ser.HashSerialize())
+	// Output: 1:true;
+}
+
+func ExampleHashSerializer_AddBool_false() {
+	ser := New()
+	ser.AddBool("2", false)
+	fmt.Println(ser.HashSerialize())
+	// Output:
+}
+
+func ExampleHashSerializer_AddFloat() {
+	ser := New()
+	ser.AddFloat("3", 3.14159265359)
+	fmt.Println(ser.HashSerialize())
+	// Output: 3:3.14159265359;
+}
+
+func ExampleHashSerializer_AddFloat_zero() {
+	ser := New()
+	ser.AddFloat("4", 0.0)
+	fmt.Println(ser.HashSerialize())
+	// Output:
+}
+
+func ExampleHashSerializer_AddFloat_negative() {
+	ser := New()
+	ser.AddFloat("5", -42.14159265359)
+	fmt.Println(ser.HashSerialize())
+	// Output: 5:-42.14159265359;
+}
+
+func ExampleHashSerializer_AddInt() {
+	ser := New()
+	ser.AddInt("6", 42)
+	fmt.Println(ser.HashSerialize())
+	// Output: 6:42;
+}
+
+func ExampleHashSerializer_AddInt_zero() {
+	ser := New()
+	ser.AddInt("7", 0.0)
+	fmt.Println(ser.HashSerialize())
+	// Output:
+}
+
+func ExampleHashSerializer_AddInt_negative() {
+	ser := New()
+	ser.AddInt("8", -42)
+	fmt.Println(ser.HashSerialize())
+	// Output: 8:-42;
+}
+
+func ExampleHashSerializer_AddString_empty() {
+	ser := New()
+	ser.AddString("9", "")
+	fmt.Println(ser.HashSerialize())
+	// Output:
+}
+
+func ExampleHashSerializer_AddString() {
+	ser := New()
+	ser.AddString("10", "hello")
+	fmt.Println(ser.HashSerialize())
+	// Output: 10:hello;
+}
+
+func ExampleHashSerializer_AddStringSlice() {
+	ser := New()
+	ser.AddStringSlice("11", []string{"c", "b", "a"})
+	fmt.Println(ser.HashSerialize())
+	// Output: 11:0:c;1:b;2:a;;
+}
+
+func ExampleHashSerializer_AddStringSlice_empty() {
+	ser := New()
+	ser.AddStringSlice("12", []string{})
+	fmt.Println(ser.HashSerialize())
+	// Output:
+}
+
+func ExampleHashSerializer_AddStringSlice_empty_value() {
+	ser := New()
+	ser.AddStringSlice("13", []string{"c", "", "a"})
+	fmt.Println(ser.HashSerialize())
+	// Output: 13:0:c;2:a;;
+}
+
+func ExampleHashSerializer_Add() {
+	ser := New()
+	ser.Add("14", testSerializable{42, "hello", []string{"c", "b", "a"}})
+	fmt.Println(ser.HashSerialize())
+	// Output: 14:1:42;2:hello;3:0:c;1:b;2:a;;;
+}
+
+func ExampleHashSerializer_Add_empty() {
+	ser := New()
+	ser.Add("15", testSerializable{})
+	fmt.Println(ser.HashSerialize())
+	// Output:
+}
+
+func ExampleStringSlice() {
+	ser := New()
+	ser.Add("1", StringSlice([]string{"a", "b", "c"}))
+	fmt.Println(ser.HashSerialize())
+	// Output: 1:0:a;1:b;2:c;;
+}
+
+func ExampleHashSerializable_HashSerialize() {
+	s := testSerializable{
+		a: 42,
+		b: "hello",
+		c: []string{"c", "b", "a"},
 	}
-	// individual test
-	for _, tt := range tests {
-		ser := New()
-		tt.f(ser)
-		assert.Equal(t, tt.s, ser.HashSerialize())
-	}
+	fmt.Println(s.HashSerialize())
+	//Output: 1:42;2:hello;3:0:c;1:b;2:a;;
 }
