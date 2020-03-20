797dc26ce75adfb877dd33f5ea4a662ad4e8fefe
Nicolas Mahe <nicolas@mahe.me>
2020-03-20 17:03:08 +0700

    add test that should work when ref to result is working

--- e2e/orchestrator_test.go
+++ e2e/orchestrator_test.go
@@ -20,6 +20,7 @@ func testOrchestrator(t *testing.T) {
 	t.Run("result task", testOrchestratorResultTask(executionStream, testRunnerHash, testInstanceHash))
 	t.Run("map const", testOrchestratorMapConst(executionStream, testInstanceHash))
 	t.Run("ref grand parent task", testOrchestratorRefGrandParentTask(executionStream, testInstanceHash))
+	t.Run("ref grand parent result", testOrchestratorRefGrandParentResult(executionStream, testInstanceHash, testRunnerHash))
 	t.Run("nested data", testOrchestratorNestedData(executionStream, testInstanceHash))
 	t.Run("nested map", testOrchestratorNestedMap(executionStream, testInstanceHash))
 	t.Run("ref path nested", testOrchestratorRefPathNested(executionStream, testInstanceHash))
