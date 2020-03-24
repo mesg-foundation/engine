3d6949ed9a559b1d891ee8457d60906564b266ce
Nicolas Mahe <nicolas@mahe.me>
2020-03-24 16:40:44 +0700

    update build-proto script

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# generate modules' msg
+protoc --gogo_out=paths=source_relative:. x/service/internal/types/msg.proto
+protoc --gogo_out=paths=source_relative:. x/process/internal/types/msg.proto
+protoc --gogo_out=paths=source_relative:. x/runner/internal/types/msg.proto
+protoc --gogo_out=paths=source_relative:. x/execution/internal/types/msg.proto
+protoc --gogo_out=paths=source_relative:. x/ownership/internal/types/msg.proto
+
 TYPES_PATH=protobuf/types
 APIS_PATH=protobuf/api
 
@@ -8,7 +15,6 @@ for file in "${TYPES_PATH}"/{event,execution,instance,service,process,ownership,
 do
   file=$(basename ${file})
   dir="${file%.*}"
-
   protoc --gogo_out=paths=source_relative:"${dir}" --proto_path . --proto_path "${TYPES_PATH}" "${file}"
 done
 
@@ -18,5 +24,5 @@ protoc --gogo_out=paths=source_relative:. protobuf/types/struct.proto
 # generate services
 for file in "${APIS_PATH}"/{event,execution,instance,service,process,ownership,runner}.proto
 do
-  protoc --gogo_out=plugins=grpc:. --proto_path . "${file}"
+  protoc --gogo_out=plugins=grpc:. "${file}"
 done
