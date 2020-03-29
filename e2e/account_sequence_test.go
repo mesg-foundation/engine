a37a985a6a9db44897b08db4392a4bc254a6f444
Nicolas Mahe <nicolas@mahe.me>
2020-03-29 20:07:46 +0700

    fix and simply e2e account sequence test

--- e2e/account_sequence_test.go
+++ e2e/account_sequence_test.go
@@ -3,28 +3,32 @@ package main
 import (
 	"testing"
 
-	servicemodule "github.com/mesg-foundation/engine/x/service"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto"
 )
 
 func testAccountSequence(t *testing.T) {
+	var (
+		randomAddress = sdk.AccAddress(crypto.AddressHash([]byte("hello")))
+		err           error
+	)
 	t.Run("wrong msg", func(t *testing.T) {
-		_, err = lcd.BroadcastMsg(servicemodule.MsgCreate{
-			// Owner:  engineAddress,
-			Sid:    "test-account-seq",
-			Name:   "test-account-seq",
-			Source: "QmXcPDajWN55n1UPV5VNJDEKE96xJFJMe3X7GwN3qx8p7r",
-		})
-		require.EqualError(t, err, "transaction returned with invalid code 18: invalid request: Owner is a required field")
+		_, err = lcd.BroadcastMsg(bank.NewMsgSend(
+			engineAddress,
+			randomAddress,
+			sdk.NewCoins(sdk.NewInt64Coin("wrong", 1000)),
+		))
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "transaction returned with invalid code")
 	})
 	t.Run("good msg", func(t *testing.T) {
-		_, err = lcd.BroadcastMsg(servicemodule.MsgCreate{
-			Owner:  engineAddress,
-			Sid:    "test-account-seq",
-			Name:   "test-account-seq",
-			Source: "QmXcPDajWN55n1UPV5VNJDEKE96xJFJMe3X7GwN3qx8p7r",
-		})
+		_, err = lcd.BroadcastMsg(bank.NewMsgSend(
+			engineAddress,
+			randomAddress,
+			sdk.NewCoins(sdk.NewInt64Coin("atto", 1000)),
+		))
 		require.NoError(t, err)
 	})
-	t.FailNow()
 }
