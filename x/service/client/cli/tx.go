f60d843b95bb56eac4ddc4a17796393112f4a775
Nicolas Mahe <nicolas@mahe.me>
2020-05-01 15:54:04 +0700

    Add tx commands for modules execution, instance, ownership, process, runner and service

--- x/service/client/cli/tx.go
+++ x/service/client/cli/tx.go
@@ -1,11 +1,17 @@
 package cli
 
 import (
+	"bufio"
 	"fmt"
+	"strings"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
 	"github.com/mesg-foundation/engine/x/service/internal/types"
 	"github.com/spf13/cobra"
 )
@@ -14,13 +20,45 @@ import (
 func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	serviceTxCmd := &cobra.Command{
 		Use:                        types.ModuleName,
-		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		Short:                      fmt.Sprintf("%s transactions subcommands", strings.Title(types.ModuleName)),
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
 
-	serviceTxCmd.AddCommand(flags.PostCommands()...)
+	serviceTxCmd.AddCommand(flags.PostCommands(
+		GetCmdCreate(cdc),
+	)...)
 
 	return serviceTxCmd
+}
+
+// GetCmdCreate is the CLI command for creating a service.
+func GetCmdCreate(cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "create [definition]",
+		Short: "create a service from its definition",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			inBuf := bufio.NewReader(cmd.InOrStdin())
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(utils.GetTxEncoder(cdc))
+
+			rawMsg := fmt.Sprintf(`{"type":"service/create","value":%s}`, args[0])
+			var msg types.MsgCreate
+			if err := cdc.UnmarshalJSON([]byte(rawMsg), &msg); err != nil {
+				return err
+			}
+			if !msg.Owner.Empty() && !msg.Owner.Equals(cliCtx.FromAddress) {
+				return fmt.Errorf("the owner set in the definition is not the same as the from flag")
+			}
+			msg.Owner = cliCtx.FromAddress
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
 }
