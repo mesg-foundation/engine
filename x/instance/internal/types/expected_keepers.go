d5c50915b5e97d2eee375017de7b396d9c310b30
Nicolas Mahe <nicolas@mahe.me>
2020-06-03 15:27:49 +0700

    remove not used expected keeper

--- x/instance/internal/types/expected_keepers.go
+++ x/instance/internal/types/expected_keepers.go
@@ -2,18 +2,9 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/params"
 	"github.com/mesg-foundation/engine/hash"
 	servicepb "github.com/mesg-foundation/engine/service"
 )
-
-// ParamSubspace defines the expected Subspace interfacace
-type ParamSubspace interface {
-	WithKeyTable(table params.KeyTable) params.Subspace
-	Get(ctx sdk.Context, key []byte, ptr interface{})
-	GetParamSet(ctx sdk.Context, ps params.ParamSet)
-	SetParamSet(ctx sdk.Context, ps params.ParamSet)
-}
 
 // ServiceKeeper module interface.
 type ServiceKeeper interface {
--- x/process/internal/types/expected_keepers.go
+++ x/process/internal/types/expected_keepers.go
@@ -2,19 +2,10 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/params"
 	"github.com/mesg-foundation/engine/hash"
 	instancepb "github.com/mesg-foundation/engine/instance"
 	ownershippb "github.com/mesg-foundation/engine/ownership"
 )
-
-// ParamSubspace defines the expected Subspace interfacace
-type ParamSubspace interface {
-	WithKeyTable(table params.KeyTable) params.Subspace
-	Get(ctx sdk.Context, key []byte, ptr interface{})
-	GetParamSet(ctx sdk.Context, ps params.ParamSet)
-	SetParamSet(ctx sdk.Context, ps params.ParamSet)
-}
 
 // InstanceKeeper module interface.
 type InstanceKeeper interface {
@@ -25,9 +16,4 @@ type InstanceKeeper interface {
 type OwnershipKeeper interface {
 	Delete(ctx sdk.Context, owner sdk.AccAddress, resourceHash hash.Hash) error
 	Set(ctx sdk.Context, owner sdk.AccAddress, resourceHash hash.Hash, resource ownershippb.Ownership_Resource, resourceAddress sdk.AccAddress) (*ownershippb.Ownership, error)
-}
-
-// BankKeeper module interface.
-type BankKeeper interface {
-	SendCoins(ctx sdk.Context, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error
 }
